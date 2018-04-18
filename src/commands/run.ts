6629d6bf5bb0a6448052eee35e8246a1ea47b59c
guinii <guinartarnau@gmail.com>
2018-04-18 12:01:06 +0200

    feat: allow new botonic message types

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -52,47 +52,55 @@ Your bot is ready, start talking:
   }
 
   parseOutput(output: string) {
-    let html = load(output);
-      let outputs = html('[type=text], [type=carrousel]')
+    let soruceData = '[type=image], [type=video], [type=audio], [type=document]'
+    let html = load(output)
+      let outputs = html('[type=text], [type=carrousel], [type=image], [type=video], [type=audio],\
+        [type=document], [type=location]')
         .map((i, elem) => {
-          let el = html(elem);
-          let out = '';
+          let el = html(elem)
+          let out = ''
           if(el.is('[type=text]')) {
-            out = el.contents().filter(e => el.contents()[e].type === 'text').text().trim();
+            out = el.contents().filter(e => el.contents()[e].type === 'text').text().trim()
           } else if(el.is('[type=carrousel]')) {
             const c = new Table({
               style: { head: [], border: [] },
-              wordWrap: false }) as Table.HorizontalTable;
-            let cards: any[] = [];
+              wordWrap: false }) as Table.HorizontalTable
+            let cards: any[] = []
             el.find('element').slice(0, 3).each((j, e) => {
-              let te = new Table({style: { head: [], border: [] }}) as Table.HorizontalTable;
-              let el = html(e);
+              let te = new Table({style: { head: [], border: [] }}) as Table.HorizontalTable
+              let el = html(e)
               let buttons = el.find('button')
                 .map((k, b) => html(b).text() + '\n(' + html(b).attr('href') + ')')
-                .get();
-              te.push([el.find('title').text() + '\n\n' + el.find('desc').text()], buttons);
-              cards.push(te.toString());
+                .get()
+              te.push([el.find('title').text() + '\n\n' + el.find('desc').text()], buttons)
+              cards.push(te.toString())
             })
             if(el.find('element').length > 3)
               cards.push({
                 content: '...\n(' + (el.find('element').length - 3) + ' more elements)',
                 vAlign: 'center',
-                hAlign: 'center'});
-            c.push(cards);
-            out = '\n' + c.toString();
+                hAlign: 'center'})
+            c.push(cards)
+            out = '\n' + c.toString()
+          } else if(el.is(soruceData)) {
+            out = `${el.attr('type')}: ${el.attr('src')}`;
+          } else if(el.is('[type=location]')) {
+            const lat = el.find('lat')[0].children[0].data
+            const long = el.find('long')[0].children[0].data
+            out = `${el.attr('type')}: https://www.google.com/maps?q=${lat},${long}`;
           }
-          let keyboard = '';
+          let keyboard = ''
           if(el.find('reply').length > 0) {
-            let kt = new Table({style: { head: [], border: [] }}) as Table.HorizontalTable;
+            let kt = new Table({style: { head: [], border: [] }}) as Table.HorizontalTable
             let keys = el.find('reply')
               .map((i, e) => html(e).text() + '\n(' + html(e).attr('payload') + ')')
-              .get();
-            kt.push(keys);
-            keyboard = '\n' + kt.toString();
+              .get()
+            kt.push(keys)
+            keyboard = '\n' + kt.toString()
           }
-          if(out) return '  [bot]> ' + out + keyboard;
+          if(out) return '  [bot]> ' + out + keyboard
         })
-        .get();
-      console.log(colors.magenta(outputs.join('\n')));
+        .get()
+      console.log(colors.magenta(outputs.join('\n')))
   }
 }
