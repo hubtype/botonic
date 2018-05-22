37e0bdf27cfddc99f1b7fdbbce10d2d1ca6346f0
guinii <guinartarnau@gmail.com>
2018-05-22 13:01:28 +0200

    feat: allow humand handover

--- src/react/component.ts
+++ src/react/component.ts
@@ -2,12 +2,15 @@ import * as React from "react"
 
 export class Component extends React.Component {
 
-  static async getInitialProps(args: any) {
-    await this.botonicInit(args)
-    return { context: args.req.context, params: args.params }
-  }
+	static async getInitialProps(args: any) {
+		await this.botonicInit(args)
+		return { context: args.req.context, params: args.params }
+	}
 
-  static async botonicInit(args: any) {
+	static async botonicInit(args: any) {
+	}
 
-  }
+	static async humanHandOff(req: any) {
+		req.context['_botonic_action'] = 'create_case'
+	}
 }
