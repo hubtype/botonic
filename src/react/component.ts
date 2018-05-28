f263dd866413ca71e01aa4b61638a6e2785ac977
Marc Català <basto17@MacBook-Pro-Marc.local>
2018-05-28 10:04:00 +0200

    feat: improved literals to set language

--- src/react/component.ts
+++ src/react/component.ts
@@ -2,34 +2,24 @@ import * as React from "react"
 import * as fs from 'fs'
 import { join, resolve } from 'path'
 
-
 export class Component extends React.Component {
 
   static async getInitialProps(args: any) {
-
     await this.botonicInit(args)
     return { context: args.req.context, params: args.params }
   }
 
-  static async getLiteral(id: string) {
+  public lang: string = 'en_EN';
+
+  setLanguage(l: string){
+    this.lang = l
+  }
+
+  getLiteral(id: string) {
 
     var literals: any = {}
-    var lang: string = 'en_EN'
-
-    let path = join(process.cwd(), '/pages/literals')
-
-    var en_EN = JSON.parse(fs.readFileSync(join(path,'en_EN.json')).toString())
-    var es_ES = JSON.parse(fs.readFileSync(join(path,'es_ES.json')).toString())
-    var ca_CA = JSON.parse(fs.readFileSync(join(path,'ca_CA.json')).toString())
-
-    literals = {en_EN, es_ES, ca_CA}
-
-    try {
-      return literals[lang][id]
-
-    } catch(e) {
-      return ''
-    }
+    var literals = require(process.cwd() + '/.next/dist/bundles/pages/literals/' + this.lang)
+    return literals.literals[id]
   }
 
   static async botonicInit(args: any) {
