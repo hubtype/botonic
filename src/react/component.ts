d9c3504b6181a34aa3f8b3a2db40c9f148cd34bd
guinii <guinartarnau@gmail.com>
2018-06-01 11:32:55 +0200

    feat: add user input in react component

--- src/react/component.ts
+++ src/react/component.ts
@@ -6,7 +6,11 @@ export class Component extends React.Component {
 
   static async getInitialProps(args: any) {
     await this.botonicInit(args)
-    return { input: args.input, context: args.req.context, params: args.params }
+    return {
+      input: args.input,
+      context: args.req.context,
+      params: args.params
+    }
   }
 
   public lang: string = 'en_EN';
