332065e04c72736bd3e91ad829c8725da4126b5c
guinii <guinartarnau@gmail.com>
2018-06-21 17:29:21 +0200

    WIP: encapsulate webview component

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 export { run } from '@oclif/command'
 import { Component }  from './react/component'
+//import { Webview } from './react/webview'
+//import BotonicWebview from '../lib/react/dist/bundles/pages/webview'
+
+const BotonicWebview = require('../lib/react/dist/bundles/pages/webview').default
 
 export const Botonic: object = {
-  React: {Component}
+  React: {Component},
+  Webview: {BotonicWebview}
 }
 
 export { default as i18n , lang} from './i18n'
+//var webview = require('../lib/react/dist/bundles/pages/webview')
+//const BotonicWebview = require('../lib/react/dist/bundles/pages/webview').default
+//export const BotonicWebview
