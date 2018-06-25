ecd78129ec19edad0f2a4cb93e96e4b762b0d612
ericmarcos <ericmarcos.p@gmail.com>
2018-06-25 12:37:08 +0200

    styles: tutorial copy and code indent

--- src/react/component.ts
+++ src/react/component.ts
@@ -26,10 +26,10 @@ export class Component extends React.Component {
     return literals.literals[id]
   }
 
-	static async botonicInit(args: any) {
-	}
+  static async botonicInit(args: any) {
+  }
 
-	static async humanHandOff(req: any) {
-		req.context['_botonic_action'] = 'create_case'
-	}
+  static async humanHandOff(req: any) {
+    req.context['_botonic_action'] = 'create_case'
+  }
 }
