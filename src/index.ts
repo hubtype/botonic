9323a11fc41c2acb808823994f0d5bf23cd3f2b8
guinii <guinartarnau@gmail.com>
2018-07-27 15:04:28 +0200

    add messageTemplate and shareButton in Botonic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 export { run } from '@oclif/command'
 import { Component }  from './react/component'
 
+var MessageTemplate = require('../lib/react/server/bundles/pages/messageTemplate').default
+var ShareButton = require('../lib/react/server/bundles/pages/shareButton').default
+
 export const Botonic: object = {
-  React: {Component}
+  React: {Component},
+  MessageTemplate,
+  ShareButton
 }
 
 export { default as i18n , lang} from './i18n'
