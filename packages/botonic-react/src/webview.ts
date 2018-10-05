73f6d23962b468e26275cfc305aafc5008f4ecbe
ericmarcos <ericmarcos.p@gmail.com>
2018-10-05 09:49:32 +0200

    fix(react): add MessengerExtensions declaration to make typescript compile

--- packages/botonic-react/src/webview.ts
+++ packages/botonic-react/src/webview.ts
@@ -4,17 +4,23 @@ import axios from 'axios'
 
 const BOTONIC_URL = process.env.BOTONIC_URL || 'https://api.hubtype.com'
 
+var MessengerExtensions: any;
+
 export class BotonicWebview {
 
-	static async close (context, payload) {
-      if(payload) {
-	      const contextUrl = BOTONIC_URL + '/v1/bots/'+ context.bot.id +'/send_postback/'
-	      let resp = await axios({
-	        method: 'post',
-	        url: contextUrl,
-	        data: {payload: payload, chat_id: context.user.id}
-	      })
-	  }
-      //MessengerExtensions.requestCloseBrowser(() => {}, (err) => {})
+	static async close(context, payload) {
+		if (payload) {
+			const contextUrl = BOTONIC_URL + '/v1/bots/' + context.bot.id + '/send_postback/'
+			try {
+				let resp = await axios({
+					method: 'post',
+					url: contextUrl,
+					data: { payload: payload, chat_id: context.user.id }
+				})
+			} catch (e) {
+				console.log(e)
+			}
+		}
+		MessengerExtensions.requestCloseBrowser(() => { }, (err) => console.log(err))
 	}
 }
