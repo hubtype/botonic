dcba21ffb53e55d933e4a05251dbd7a696810db2
guinii <guinartarnau@gmail.com>
2019-03-12 16:35:16 +0100

    fix: dynamic customHeder url depending script

--- packages/botonic-cli/templates/custom_webchat/src/webchat/myHeader.js
+++ packages/botonic-cli/templates/custom_webchat/src/webchat/myHeader.js
@@ -1,30 +1,39 @@
 import Logo from '../assets/bot_vader.jpg'
 import React from 'react'
 
-export const MyHeader = props => (
-	<div
-		style={{
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'flex-end',
-			backgroundColor: 'pink',
-			color: 'saddlebrown'
-		}}
-	>
-		<img
+export const MyHeader = props => {
+	console.log('HEADERRRR')
+	var scriptBaseUrl = document
+		.querySelector('script[src$="webchat.botonic.js"]')
+		.getAttribute('src')
+	var scriptName = scriptBaseUrl.split('/').pop()
+	scriptBaseUrl = scriptBaseUrl.replace('/' + scriptName, '/')
+	console.log('ueuuee', scriptBaseUrl)
+	return (
+		<div
 			style={{
-				height: 24,
-				margin: '0px 12px'
-			}}
-			src={Logo}
-		/>
-		<h2
-			style={{
-				margin: 0,
-				fontFamily: 'Arial, Helvetica, sans-serif'
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'flex-end',
+				backgroundColor: 'pink',
+				color: 'saddlebrown'
 			}}
 		>
-			Custom Webchat Title
-		</h2>
-	</div>
-)
+			<img
+				style={{
+					height: 24,
+					margin: '0px 12px'
+				}}
+				src={scriptBaseUrl + Logo}
+			/>
+			<h2
+				style={{
+					margin: 0,
+					fontFamily: 'Arial, Helvetica, sans-serif'
+				}}
+			>
+				Custom Webchat Title
+			</h2>
+		</div>
+	)
+}
