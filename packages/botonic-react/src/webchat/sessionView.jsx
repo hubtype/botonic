f633e44ba264cf750999d91fbf84c332aa044142
guinii <guinartarnau@gmail.com>
2019-03-28 15:46:44 +0100

    fix(react): remove onAdd from JsonView

--- packages/botonic-react/src/webchat/sessionView.jsx
+++ packages/botonic-react/src/webchat/sessionView.jsx
@@ -9,7 +9,6 @@ export const SessionView = props => {
     <ReactJson
       style={{ marginLeft: '20px' }}
       onEdit={e => updateSession(e.updated_src)}
-      onAdd={e => updateSession(e.updated_src)}
       onDelete={e => updateSession(e.updated_src)}
       theme='apathy:inverted'
       iconStyle='triangle'
