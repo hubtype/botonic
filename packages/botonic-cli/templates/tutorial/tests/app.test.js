e88acfff4d336d210e1f4a8ea6ecbffc58baeb7a
Dani Pinyol <dpinol@gmail.com>
2019-04-12 15:34:58 +0200

    tests in botonic-cli. probably discard it

--- packages/botonic-cli/templates/tutorial/tests/app.test.js
+++ packages/botonic-cli/templates/tutorial/tests/app.test.js
@@ -1,4 +1,6 @@
 import { BotonicInputTester, BotonicOutputTester } from '@botonic/react'
+import * as contentful from 'botonic-plugin-contentful'
+// var contentful = require('botonic-plugin-contentful')
 
 import App from '../src/app'
 
@@ -6,6 +8,10 @@ let i = new BotonicInputTester(App)
 let o = new BotonicOutputTester(App)
 
 test('TEST: (404) NOT FOUND', async () => {
+  // let p = new plugin();
+  console.log("kkkkkkkk2");
+  let r = new contentful.Renderer();
+  r.richMessage();
   await expect(i.text('whatever')).resolves.toBe(
     o.text('Please, type "start" to start the tutorial.')
   )
