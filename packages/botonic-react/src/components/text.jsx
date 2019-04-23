534ce00bbb8126ff80d3c14878d3c9589b1a4288
guinii <guinartarnau@gmail.com>
2019-04-23 14:34:01 +0200

    fix(react): minor errors when getting data from backend, add some comments in code

--- packages/botonic-react/src/components/text.jsx
+++ packages/botonic-react/src/components/text.jsx
@@ -3,11 +3,14 @@ import { Message } from './message'
 
 const serialize = textProps => {
   let text = textProps.children
-  if (
-    typeof textProps.children == 'object' &&
-    typeof textProps.children[1] != 'string'
-  ) {
-    text = textProps.children[0]
+  /* As text message can have multiple children ( a text with buttons or quickreplies )
+    we only want to store the text, we don't want to store the buttons or quickreplies.
+    If we have an array of string, we want to join all the string as a single string, for 
+    future render actions
+  */
+  if (typeof textProps.children == 'object') {
+    text = textProps.children.join(' ')
+    if (typeof textProps.children[1] != 'string') text = textProps.children[0]
   }
   return { text }
 }
