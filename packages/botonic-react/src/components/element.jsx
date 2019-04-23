514b11b67fc5d7b537c2d496302d1fbab31dbae6
guinii <guinartarnau@gmail.com>
2019-04-23 14:34:01 +0200

    fix(react): minor errors when getting data from backend, add some comments in code

--- packages/botonic-react/src/components/element.jsx
+++ packages/botonic-react/src/components/element.jsx
@@ -28,10 +28,27 @@ Element.serialize = elementProps => {
       .filter(c => c.type && c.type.name != 'Button')
       .map(c => c.type.serialize && c.type.serialize(c.props))
   )
+  // When we are serializer buttons from backend, we are receiving the data
+  // as an array of buttons, so we have to keep robust with serve and deal with arrays
   element['buttons'] = [
     ...elementProps.children
-      .filter(c => c.type && c.type.name == 'Button')
-      .map(b => b.type.serialize && b.type.serialize(b.props).button)
+      .filter(c => {
+        if (c instanceof Array) return true
+        return c.type && c.type.name == 'Button'
+      })
+      .map(b => {
+        if (b instanceof Array) {
+          return b.map(
+            bb => bb.type.serialize && bb.type.serialize(bb.props).button
+          )
+        }
+        return b.type.serialize && b.type.serialize(b.props).button
+      })
   ]
+  // When we have the buttons from backend, we have all buttons inside an array on the first position
+  // of another array in element['buttons'] we want that element['buttons'] to be an array of buttons,
+  // not an array of another array of buttons
+  if (element['buttons'][0] instanceof Array)
+    element['buttons'] = element['buttons'][0]
   return element
 }
