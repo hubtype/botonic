b9632495c807ead8934af61e4156d09879288031
Dani Pinyol <dpinol@gmail.com>
2019-05-13 17:18:02 +0200

    chore(core/react): eslint & prettier configuration

---
+++ packages/botonic-react/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  // parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    // "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  plugins: [
+    "no-null"
+  ],
+  parserOptions: {
+    ecmaVersion: 2017, // async is from ecma2017. Supported in node >=7.10
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+// npm run lint runs eslint with --quiet --fix so that only errors are fixed
+  rules: {
+    // style. Soon a precommit githook will fix prettier errors
+    "prettier/prettier": "error",
+
+    "dot-notation": "warn", // in typescript we must use obj.field when we have the types, and obj['field'] when we don't
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "node/no-unsupported-features/es-syntax": "off", //babel will take care of ES compatibility
+
+    // compatibility with botonic style
+    "no-null/no-null": "off"
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  },
+  env: {
+    jest: true
+  }
+};
