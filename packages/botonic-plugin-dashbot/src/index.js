ec9abc2f02a901318986152eb2dd9781bfbd29b8
sergi <sergi.sorigue@gmail.com>
2019-05-14 12:10:12 +0200

    feat(plugin): New Dashbot Plugin

---
+++ packages/botonic-plugin-dashbot/src/index.js
@@ -0,0 +1,24 @@
+var dashbot = require('dashbot')
+
+export default class BotonicPluginDashbot {
+  constructor(options) {
+    this.options = options
+    dashbot = dashbot(this.options.apiKey).webchat
+  }
+
+  async pre({ input, session, lastRoutePath }) {}
+
+  async post({ input, session, lastRoutePath, response }) {
+    const messageInDashbot = {
+      text: input.data,
+      userId: session.user.id
+    }
+    await dashbot.logIncoming(messageInDashbot)
+
+    const messageOutDashbot = {
+      text: response,
+      userId: session.user.id
+    }
+    await dashbot.logOutgoing(messageOutDashbot)
+  }
+}
