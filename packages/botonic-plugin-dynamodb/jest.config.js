96ffc799149367ef1492315a6576243aee46e600
Dani Pinyol <dpinol@gmail.com>
2019-05-14 10:06:18 +0200

    chore(dynamodb): project setup

---
+++ packages/botonic-plugin-dynamodb/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['tests/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  // regex works fine with "find tests | egrep ..." but incorrectly gets files with names without 'test' or 'spec
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(ts|tsx)$',
+  testPathIgnorePatterns: ['lib', '.*\.d\.ts', 'tests/helpers'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  snapshotSerializers: [],
+  setupFilesAfterEnv: [],
+  modulePaths: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
+};
