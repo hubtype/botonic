c3257c5628af91d8a0de04ca802023d10c44b9b4
Dani Pinyol <dpinol@gmail.com>
2019-05-15 16:01:51 +0200

    feature(core): call to store a case rating

--- packages/botonic-core/index.d.ts
+++ packages/botonic-core/index.d.ts
@@ -1,10 +1,19 @@
+type StringMatcher = RegExp| string | ((data: string) => boolean);
+
 export interface Route {
-  path: string;
-  payload?: RegExp| string | ((input: string) => boolean);
-  text?: RegExp | string | ((input: string) => boolean);
+  path: StringMatcher;
+  payload?: StringMatcher;
+  text?: StringMatcher;
+  type?: StringMatcher;
+  intent?: StringMatcher;
+  session?: (session: any) => boolean;
   action: React.ReactNode;
 }
 
+// handoff
 export declare function humanHandOff(session: any, queue_name: string, on_finish: {payload?: any, path?: any}): Promise<void>;
-
 export declare function getOpenQueues(session: any): Promise<{queues: string[]}>;
+export declare function storeCaseRating(session: any, rating: number): Promise<any>;
+
+
+export declare function storeCaseRating(session: any, rating: number): Promise<void>;
