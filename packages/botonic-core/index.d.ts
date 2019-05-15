b0ae77944ea851a9aeadefe7ba05b73ddd1707d1
Dani Pinyol <dpinol@gmail.com>
2019-05-15 07:09:34 +0200

    chore(core): types for humanHandOff & getOpenQueues

--- packages/botonic-core/index.d.ts
+++ packages/botonic-core/index.d.ts
@@ -4,3 +4,7 @@ export interface Route {
   text?: RegExp | string | ((input: string) => boolean);
   action: React.ReactNode;
 }
+
+export declare function humanHandOff(session: any, queue_name: string, on_finish: {payload?: any, path?: any}): Promise<void>;
+
+export declare function getOpenQueues(session: any): Promise<{queues: string[]}>;
