ed2900b0a3a2e2b92170b1decfd829e0ee908717
Dani Pinyol <dpinol@gmail.com>
2019-05-15 16:01:51 +0200

    feature(core): call to store a case rating

--- packages/botonic-core/src/index.js
+++ packages/botonic-core/src/index.js
@@ -1,5 +1,5 @@
 export { Router } from './router'
 export { getString } from './i18n'
-export { getOpenQueues, humanHandOff } from './handoff'
+export { getOpenQueues, humanHandOff, storeCaseRating } from './handoff'
 export { getNLU } from './nlu'
 export { isBrowser, isNode, params2queryString } from './utils'
