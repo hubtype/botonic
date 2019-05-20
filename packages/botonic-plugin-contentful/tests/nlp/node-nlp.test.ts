34cf23263e5cb81fac8a5ebbf5887d625d9070ed
Dani Pinyol <dpinol@gmail.com>
2019-05-20 10:18:37 +0200

    feature(nlp): remove stopwords

--- packages/botonic-plugin-contentful/tests/nlp/node-nlp.test.ts
+++ packages/botonic-plugin-contentful/tests/nlp/node-nlp.test.ts
@@ -2,6 +2,6 @@ import { normalize } from '../../src/nlp';
 
 test('TEST: normalize', () => {
   expect(normalize('áá')).toEqual(['aa']);
-  expect(normalize(',./ áé  íó(úÑ)  ;')).toEqual(['ae', 'io', 'un']);
-  expect(normalize('realizar pedido')).toEqual(['realiz', 'ped']);
+  expect(normalize(',./ áé  íó(óÑ)  ;')).toEqual(['ae', 'io', 'on']);
+  expect(normalize('pero realizar mi la un una de ya del pedido')).toEqual(['realiz', 'ped']);
 });
