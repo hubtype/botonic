a411cea06ee04ed1beb8d69bc5d436034a730f7f
Dani Pinyol <dpinol@gmail.com>
2019-05-23 15:59:44 +0200

    feature(contentful): allow hiding a button of a Text
    Text builder for tests

---
+++ packages/botonic-plugin-contentful/tests/helpers/builders.ts
@@ -0,0 +1,70 @@
+import {
+  Button,
+  ButtonStyle,
+  Callback,
+  Model,
+  ModelType,
+  Text
+} from '../../src/cms';
+
+function rndStr(): string {
+  return Math.random().toString();
+}
+
+function rndBool(): boolean {
+  return Math.random() >= 0.5;
+}
+
+abstract class ModelBuilder {
+  name: string = rndStr();
+
+  withName(name: string): ModelBuilder {
+    this.name = name;
+    return this;
+  }
+  abstract build(): Model;
+}
+
+export class ButtonsBuilder {
+  name = rndStr();
+  buttons: Button[] = [];
+
+  build(): Button[] {
+    return this.buttons;
+  }
+
+  withButton(): ButtonsBuilder {
+    this.buttons.push(
+      new Button(rndStr(), rndStr(), Callback.ofModel(ModelType.TEXT, rndStr()))
+    );
+    return this;
+  }
+}
+
+export class KeywordsBuilder {
+  keywords = [rndStr(), rndStr()];
+
+  build(): string[] {
+    return this.keywords;
+  }
+}
+
+export class TextBuilder extends ModelBuilder {
+  text = rndStr();
+  readonly buttons = new ButtonsBuilder();
+  shortText = rndStr();
+  readonly keywords = new KeywordsBuilder();
+
+  build(): Text {
+    return new Text(
+      this.name,
+      this.text,
+      this.buttons.build(),
+      this.shortText,
+      this.keywords.build(),
+      // TODO add follow up but avoid too many recursive texts
+      undefined,
+      rndBool() ? ButtonStyle.BUTTON : ButtonStyle.QUICK_REPLY
+    );
+  }
+}
