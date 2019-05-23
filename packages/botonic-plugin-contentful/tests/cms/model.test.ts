34a66ea46116f4c28d12ca8eaab02bb017df1d89
Dani Pinyol <dpinol@gmail.com>
2019-05-23 15:59:44 +0200

    feature(contentful): allow hiding a button of a Text
    Text builder for tests

---
+++ packages/botonic-plugin-contentful/tests/cms/model.test.ts
@@ -0,0 +1,17 @@
+import { TextBuilder } from '../helpers/builders';
+import { expectEqualExceptOneField } from '../helpers/expect';
+
+test('TEST: cloneWithoutButtonsThat copies all fields except buttons', () => {
+  const builder = new TextBuilder();
+  builder.buttons.withButton().withButton();
+  const t1 = builder.build();
+
+  const clone = t1.cloneWithoutButtonsThat(b => b === t1.buttons[1]);
+
+  expect(clone).not.toEqual(t1);
+  expect(clone.buttons).toHaveLength(1);
+  expect(clone.buttons[0]).toBe(t1.buttons[1]);
+
+  expectEqualExceptOneField(t1, clone, 'buttons');
+});
+
---
+++ packages/botonic-plugin-contentful/tests/helpers/expect.ts
@@ -0,0 +1,6 @@
+export function expectEqualExceptOneField(o1: any, o2: any, fieldName: string): void {
+  for (let f of Object.keys(o1)) {
+    if (f == fieldName) continue;
+    expect(o2[f]).toEqual(o1[f]);
+  }
+}
