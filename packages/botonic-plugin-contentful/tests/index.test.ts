b2bab65e0c2119adba7e75239bdab29857d0abcf
Dani Pinyol <dpinol@gmail.com>
2019-05-24 13:16:10 +0200

    chore(contentful): chores
    defined interfaces for Contentful plugin config (reused in tests)
    Now Keywords.suggestTextsForInput requires previous tokenization
    rename cloneWithoutButtonsThat->cloneWithFilteredButtons

--- packages/botonic-plugin-contentful/tests/index.test.ts
+++ packages/botonic-plugin-contentful/tests/index.test.ts
@@ -2,33 +2,37 @@ import { testContentful } from './contentful/contentful.helper';
 import * as plugin from '../src/';
 import 'jest-extended';
 
+// TODO move to keywords folder
+
 test('TEST: suggestTextsForInput keywords found', async () => {
   let cmsPlugin = new plugin.default({ cms: testContentful() });
 
   // act
-  let text = await cmsPlugin.suggestTextsForInput(
-    ' DevoluciON fuera de  plazo?',
+
+  let text = await cmsPlugin.keywords.suggestTextsForInput(
+    cmsPlugin.keywords.tokenize(' DevoluciON fuera de  plazo?'),
     'GbIpKJu8kW6PqMGAUYkoS',
     '4C2ghzuNPXIl0KqLaq1Qqm'
   );
 
   // assert
+  expect(text.name).toEqual('KEYWORDS_OK');
   expect(text.buttons).toHaveLength(2);
   let textNames = text.buttons.map(b => b.name);
   expect(textNames).toIncludeSameMembers(['POST_FAQ3', 'POST_FAQ5']);
 });
 
-test('TEST: suggestTextsForInput', async () => {
+test('TEST: suggestTextsForInput no keywords found', async () => {
   let cmsPlugin = new plugin.default({ cms: testContentful() });
 
   // act
-  let text = await cmsPlugin.suggestTextsForInput(
-    ' donde esta  Mi pedido?',
+  let text = await cmsPlugin.keywords.suggestTextsForInput(
+    cmsPlugin.keywords.tokenize('willnotbefound'),
     'GbIpKJu8kW6PqMGAUYkoS',
     '4C2ghzuNPXIl0KqLaq1Qqm'
   );
 
   // assert
-  expect(text.buttons).toHaveLength(1);
-  expect(text.buttons[0].name).toEqual('POST_FAQ1');
+  expect(text.name).toEqual('KEYWORDS_KO_RETRY');
+  expect(text.buttons).toHaveLength(0);
 });
