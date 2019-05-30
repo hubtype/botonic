327570f532687873707a2158d5282ac1ea2918d6
Dani Pinyol <dpinol@gmail.com>
2019-05-30 13:18:39 +0200

    chore(contentful): allow using functions & classes before declaring them

--- packages/botonic-plugin-contentful/tests/nlp/node-nlp.test.ts
+++ packages/botonic-plugin-contentful/tests/nlp/node-nlp.test.ts
@@ -3,5 +3,7 @@ import { tokenizeAndStem } from '../../src/nlp';
 test('TEST: normalize', () => {
   expect(tokenizeAndStem('áá')).toEqual(['aa']);
   expect(tokenizeAndStem(',./ áé  íó(óÑ)  ;')).toEqual(['ae', 'io', 'on']);
-  expect(tokenizeAndStem('pero realizar mi la un una de ya del pedido')).toEqual(['realiz', 'ped']);
+  expect(
+    tokenizeAndStem('pero realizar mi la un una de ya del pedido')
+  ).toEqual(['realiz', 'ped']);
 });
