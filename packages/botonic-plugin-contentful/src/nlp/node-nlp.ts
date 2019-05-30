886efad1eea229c772bd8bed4517396fec1b0d10
Dani Pinyol <dpinol@gmail.com>
2019-05-30 13:18:39 +0200

    chore(contentful): allow using functions & classes before declaring them

--- packages/botonic-plugin-contentful/src/nlp/node-nlp.ts
+++ packages/botonic-plugin-contentful/src/nlp/node-nlp.ts
@@ -1,14 +1,22 @@
-//import NlpUtil from 'node-nlp/lib/nlp/nlp-util';
 import stemmer from 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer-es';
 
 // TODO should be filtered before stemmer, because stemmer removes some final letters
 const defaultStopWords = [
+  // "cuando","quien","donde",,
+  //   "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "_",,"sí"
+  // eslint-disable-next-line prettier/prettier
   "a","un","el","ella","y","sobre","de","la","que","en",
+  // eslint-disable-next-line prettier/prettier
   "los","del","se","las","por","un","para","con","no",
+  // eslint-disable-next-line prettier/prettier
   "una","su","al","lo","como","más","per"/*pero*/,"sus","le",
+  // eslint-disable-next-line prettier/prettier
   "ya","o","porque","muy","sin","sobre","también",
+  // eslint-disable-next-line prettier/prettier
   "me","hasta","desde","nos","durante","uno",
+  // eslint-disable-next-line prettier/prettier
   "ni","contra","ese","eso","mí","qué","otro","él","cual",
+  // eslint-disable-next-line prettier/prettier
   "poco","mi","tú","te","ti"
 ];
 
@@ -28,5 +36,3 @@ export function tokenizeAndStem(
   return tokens.filter(t => !stopWords!.includes(t));
 }
 
-// "cuando","quien","donde",,
-//   "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "_",,"sí"
