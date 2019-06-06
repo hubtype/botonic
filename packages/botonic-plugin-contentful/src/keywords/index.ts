05588d0e91ae4927e5791be4540876ca831027e7
Dani Pinyol <dpinol@gmail.com>
2019-06-06 14:37:40 +0200

    feature(contentful): regexForModel more strict & limit number of buttons

--- packages/botonic-plugin-contentful/src/keywords/index.ts
+++ packages/botonic-plugin-contentful/src/keywords/index.ts
@@ -4,7 +4,10 @@ import * as cms from '../cms';
 import { KeywordsParser } from '../nlp/keywords';
 
 export class Keywords {
-  constructor(readonly cms: cms.CMS) {}
+  /**
+   * @param maxButtons Some providers only support 3 buttons
+   */
+  constructor(readonly cms: cms.CMS, readonly maxButtons = 3) {}
 
   tokenize(inputText: string): string[] {
     return tokenizeAndStem(inputText);
@@ -33,30 +36,48 @@ export class Keywords {
     return new cms.Text(foundText.name, foundText.text, buttons);
   }
 
+  /**
+   * Given a {@link cms.Text} returned by {@link suggestContentsForInput}, it analyses the buttons (which refer to
+   * normal keywords or to chitchat keywords) and returns the {@link cms.Text} to display.
+   * @return undefined when only chitchat keywords were detected but we estimate the sentence contained a question
+   * which was not recognized
+   */
   public async treatChitChat(
     tokens: string[],
     text: cms.Text,
     chitchatShortTexts: string[]
   ): Promise<cms.Text | undefined> {
-    const onlyChitChatsFunc = (b: cms.Button) =>
-      chitchatShortTexts.includes(b.text);
+    const isChitChat = (b: cms.Button) => chitchatShortTexts.includes(b.text);
 
-    const onlyChitChats = text.buttons.filter(onlyChitChatsFunc);
+    const onlyChitChats = text.buttons.filter(isChitChat);
     if (onlyChitChats.length == 0) {
-      // no chitchats
       return text;
     }
-    if (onlyChitChats.length == text.buttons.length) {
-      // all chitchats, no normal keywords
-      const estimatedNoChitchatWords = tokens.length - onlyChitChats.length * 2;
-      if (estimatedNoChitchatWords > 2) {
-        // avoid that a sentence with chitchat and a question without recognized keywords is answered as chitchat
-        return undefined;
-      }
-      let anyCallback = onlyChitChats[0].callback as ContentCallback;
-      return (await anyCallback.deliverPayloadContent(this.cms)) as cms.Text;
+    if (onlyChitChats.length < text.buttons.length) {
+      let textWithoutChitChats = text.cloneWithFilteredButtons(b => !isChitChat(b));
+      return this.limitNumButtons(textWithoutChitChats);
     }
-    // remove chitchats if input text matches with some keywords
-    return text.cloneWithFilteredButtons(b => !onlyChitChatsFunc(b));
+    return await this.treatOnlyChitchats(tokens, onlyChitChats);
+  }
+
+  private limitNumButtons(text: cms.Text): cms.Text {
+    if (text.buttons.length <= this.maxButtons) {
+      return text;
+    }
+    let counter = 0;
+    return text.cloneWithFilteredButtons(b => ++counter <= this.maxButtons);
+  }
+
+  private async treatOnlyChitchats(
+    tokens: string[],
+    onlyChitChats: cms.Button[]
+  ): Promise<cms.Text | undefined> {
+    const estimatedNoChitchatWords = tokens.length - onlyChitChats.length * 2;
+    if (estimatedNoChitchatWords > 2) {
+      // avoid that a sentence with chitchat and a question without recognized keywords is answered as chitchat
+      return undefined;
+    }
+    let anyCallback = onlyChitChats[0].callback as ContentCallback;
+    return (await anyCallback.deliverPayloadContent(this.cms)) as cms.Text;
   }
 }
