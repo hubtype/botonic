6bf030bd811ef3c864a87165d581700839812ae9
Dani Pinyol <dpinol@gmail.com>
2019-06-12 08:56:35 +0200

    chore(contentful): eslint config
    upgrade typescript eslint to avoid warning with typescript 3.5.2

--- packages/botonic-plugin-contentful/.eslintrc.js
+++ packages/botonic-plugin-contentful/.eslintrc.js
@@ -44,7 +44,8 @@ module.exports = {
     "unicorn/prevent-abbreviations" : "off", // the plugin removes removes type annotations from typescript code :-(
     "unicorn/filename-case" : "off", // React convention is in CamelCase
     "@typescript-eslint/no-non-null-assertion" : "warn", // specially useful in tests, and "when you know what you're doing"
-    "@typescript-eslint/no-object-literal-type-assertion" : {allowAsParameter: false} //useful to pass options to plugins
+    "@typescript-eslint/no-object-literal-type-assertion" : {allowAsParameter: false}, //useful to pass options to plugins
+    "@typescript-eslint/no-namespace": { allowDeclarations: true }, // to encapsulate types in namespace with same name as Class
   },
   settings: {
     react: {
