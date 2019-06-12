95a363b2592ad73caea68498dc4ece4d092707c7
Dani Pinyol <dpinol@gmail.com>
2019-06-12 08:53:47 +0200

    feature(contentful): KeywordParser option to require only keywords

--- packages/botonic-plugin-contentful/src/nlp/keywords.ts
+++ packages/botonic-plugin-contentful/src/nlp/keywords.ts
@@ -5,26 +5,45 @@ class CandidateWithKeywords<M> {
   constructor(readonly owner: M, readonly keywords: string[]) {}
 }
 
+export enum MatchType {
+  /** After removing stop words, spaces and word endings, the input text must only contain the keywords*/
+  ONLY_KEYWORDS_FOUND,
+  /** The keyword may be preceded and followed by other words */
+  KEYWORDS_AND_OTHERS_FOUND
+}
+
 export class KeywordsParser<M> {
   private readonly candidates = [] as CandidateWithKeywords<M>[];
 
-  addCandidate(candidate: M, keywords: string[]): void {
-    keywords = keywords.map(kw => {
+  constructor(readonly matchType: MatchType) {}
+
+  addCandidate(candidate: M, rawKeywords: string[]): void {
+    let stemmedKeywords = rawKeywords.map(kw => {
       return tokenizeAndStem(kw).join(' ');
     });
-    this.candidates.push(new CandidateWithKeywords(candidate, keywords));
+    this.candidates.push(new CandidateWithKeywords(candidate, stemmedKeywords));
   }
 
-  findCandidatesWithKeywordsAt(tokens: string[]): M[] {
+  findCandidatesWithKeywordsAt(stemmedTokens: string[]): M[] {
     let matches = [] as M[];
+    let joinedTokens = stemmedTokens.join(' ');
     for (let candidate of this.candidates) {
       for (let keyword of candidate.keywords) {
-        if (substringIsBlankSeparated(tokens.join(' '), keyword)) {
+        if (this.keywordMatches(joinedTokens, keyword)) {
           matches = matches.concat(candidate.owner);
           break;
         }
       }
     }
     return matches;
+  }
+
+  private keywordMatches(joinedTokens: string, keyword: string): boolean {
+    switch (this.matchType) {
+      case MatchType.KEYWORDS_AND_OTHERS_FOUND:
+        return substringIsBlankSeparated(joinedTokens, keyword);
+      case MatchType.ONLY_KEYWORDS_FOUND:
+        return joinedTokens == keyword;
+    }
   }
 }
