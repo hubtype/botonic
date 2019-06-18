6dde607382114b8a93d1240f4c89439f631c54b4
Dani Pinyol <dpinol@gmail.com>
2019-06-18 15:08:23 +0200

    chores(core): rename storeCaseRating local var

--- packages/botonic-core/src/handoff.js
+++ packages/botonic-core/src/handoff.js
@@ -14,7 +14,7 @@ export async function getOpenQueues(session) {
   })
   return resp.data
 }
-//TODO test in backend
+
 export async function humanHandOff(session, queue_name, on_finish) {
   let params = `create_case:${queue_name}`
   if (on_finish) {
@@ -27,14 +27,13 @@ export async function humanHandOff(session, queue_name, on_finish) {
 export async function storeCaseRating(session, rating) {
   let base_url = session._hubtype_api || 'https://api.hubtype.com'
   let chat_id = session.user.id
-  const queues_url = `${base_url}/v1/chats/${chat_id}/store_case_rating/`
 
   let resp = await axios({
     headers: {
       Authorization: `Bearer ${session._access_token}`
     },
     method: 'post',
-    url: queues_url,
+    url: `${base_url}/v1/chats/${chat_id}/store_case_rating/`,
     data: { chat_id, rating }
   })
   return resp.data
