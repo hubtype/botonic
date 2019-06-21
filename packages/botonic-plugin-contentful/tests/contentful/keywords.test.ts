22b0a711207e84ab497952781327bf477be17622
Dani Pinyol <dpinol@gmail.com>
2019-06-21 10:21:45 +0200

    feature(contentful): support SearchableByKeywords

---
+++ packages/botonic-plugin-contentful/tests/contentful/keywords.test.ts
@@ -0,0 +1,44 @@
+import { ContentCallback, ModelType, SearchResult } from '../../src/cms';
+import { testContentful } from './contentful.helper';
+import { TEST_POST_FAQ1_ID } from './text.test';
+import 'jest-extended';
+
+test('TEST: contentful contentsWithKeywords', async () => {
+  let results = await testContentful().contentsWithKeywords();
+
+  let queues: SearchResult[] = [];
+  let contentsWithKeywords: SearchResult[] = [];
+  results
+    .filter(result => result.callback instanceof ContentCallback)
+    .map(result => {
+      switch ((result.callback as ContentCallback).model) {
+        case ModelType.QUEUE:
+          queues.push(result);
+          break;
+        default:
+          contentsWithKeywords.push(result);
+      }
+    });
+  expect(queues).toHaveLength(2);
+  let keywordsByPrio: { [priority: number]: string[] } = {};
+  for (let queue of queues) {
+    expect(queue.name).toEqual('TEST_QUEUE');
+    expect(queue.shortText).toEqual('Short Text');
+    keywordsByPrio[queue.priority] = queue.keywords;
+  }
+  expect(keywordsByPrio[10]).toEqual(['low1', 'low2']);
+  expect(keywordsByPrio[99]).toEqual(['high1', 'high2']);
+
+  expect(contentsWithKeywords).toHaveLength(16);
+
+  let postFaq1 = contentsWithKeywords.find(
+    content => (content.callback as ContentCallback).id == TEST_POST_FAQ1_ID
+  );
+  expect(postFaq1!.name).toEqual('POST_FAQ1');
+  expect(postFaq1!.shortText).toEqual('Encontrar mi pedido');
+  expect(postFaq1!.priority).toEqual(100);
+  expect(postFaq1!.keywords).toIncludeSameMembers([
+    'no encuentro mi pedido',
+    'donde esta mi pedido'
+  ]);
+});
