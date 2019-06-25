f2981a20cb9e0afa25eab55ec0f5366acf227c12
Dani Pinyol <dpinol@gmail.com>
2019-06-25 09:06:46 +0200

    chores(dynamodb): copy jest & TS config from contentful

--- packages/botonic-plugin-dynamodb/jest.config.js
+++ packages/botonic-plugin-dynamodb/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json'
+      tsConfig: '<rootDir>/tests/tsconfig.json'
     }
   },
   preset: 'ts-jest',
@@ -15,7 +15,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   snapshotSerializers: [],
-  setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   modulePaths: ['node_modules', 'src'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
