dfe3514136e8d697d1d54c195579906fd99b9df8
Dani Pinyol <dpinol@gmail.com>
2019-06-27 11:57:00 +0200

    feature(contentful): Contentful delivery of DateRange type

---
+++ packages/botonic-plugin-contentful/src/contentful/date-range.ts
@@ -0,0 +1,30 @@
+import { ContentWithKeywordsFields, DeliveryApi } from './delivery-api';
+import * as contentful from 'contentful/index';
+import * as time from '../time';
+
+export class DateRangeDelivery {
+  constructor(protected delivery: DeliveryApi) {}
+
+  async dateRange(id: string): Promise<time.DateRange> {
+    let entry: contentful.Entry<DateRangeFields> = await this.delivery.getEntry(
+      id
+    );
+    return DateRangeDelivery.dateRangeFromEntry(entry);
+  }
+
+  static dateRangeFromEntry(
+    entry: contentful.Entry<DateRangeFields>
+  ): time.DateRange {
+    return new time.DateRange(
+      entry.fields.name,
+      new Date(Date.parse(entry.fields.from)),
+      new Date(Date.parse(entry.fields.to))
+    );
+  }
+}
+
+export interface DateRangeFields extends ContentWithKeywordsFields {
+  name: string;
+  from: string;
+  to: string;
+}
