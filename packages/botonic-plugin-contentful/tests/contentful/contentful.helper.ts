9a8ab006594626eb68e87f431d7219a51d22e7a0
Dani Pinyol <dpinol@gmail.com>
2019-07-01 19:04:19 +0200

    chores(contentful): restore contentful credentials

--- packages/botonic-plugin-contentful/tests/contentful/contentful.helper.ts
+++ packages/botonic-plugin-contentful/tests/contentful/contentful.helper.ts
@@ -1,13 +1,13 @@
 import Contentful from '../../src/contentful';
 
 export function testSpaceId(): string {
-  return 's1zkp12rqz4o'; //process.env.CONTENTFUL_TEST_SPACE_ID!;
+  return process.env.CONTENTFUL_TEST_SPACE_ID!;
 }
 
 export function testContentful(): Contentful {
   return new Contentful(
     testSpaceId(),
-    'Q7K3ly6oEr7k-ER9b68U2gFuhdZCDC9ZNP68oWMXf6Y', //process.env.CONTENTFUL_TEST_TOKEN!,
+    process.env.CONTENTFUL_TEST_TOKEN!,
     2000
   );
 }
