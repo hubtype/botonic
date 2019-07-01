e67dd62ca56ff34922c39050d17d515e59235a78
Dani Pinyol <dpinol@gmail.com>
2019-07-01 19:01:11 +0200

    feature(contentful): console.error when missing text in button
    when button has no text, replace with name field

--- packages/botonic-plugin-contentful/src/cms/cms-error.ts
+++ packages/botonic-plugin-contentful/src/cms/cms-error.ts
@@ -16,34 +16,54 @@ import * as time from '../time';
 export class ErrorReportingCMS implements CMS {
   constructor(readonly cms: CMS) {}
 
+  private validate<C extends Content>(content: C): C {
+    let v = content.validate();
+    if (v) {
+      console.error(v);
+    }
+    return content;
+  }
+
   carousel(id: string, callbacks?: CallbackMap): Promise<Carousel> {
     return this.cms
       .carousel(id, callbacks)
-      .catch(this.handleError(ModelType.CAROUSEL, id));
+      .catch(this.handleError(ModelType.CAROUSEL, id))
+      .then(this.validate);
   }
 
   text(id: string, callbacks?: CallbackMap): Promise<Text> {
     return this.cms
       .text(id, callbacks)
-      .catch(this.handleError(ModelType.TEXT, id));
+      .catch(this.handleError(ModelType.TEXT, id))
+      .then(this.validate);
   }
 
   chitchat(id: string, callbacks?: CallbackMap): Promise<Chitchat> {
     return this.cms
       .text(id, callbacks)
-      .catch(this.handleError(ModelType.CHITCHAT, id));
+      .catch(this.handleError(ModelType.CHITCHAT, id))
+      .then(this.validate);
   }
 
   url(id: string): Promise<Url> {
-    return this.cms.url(id).catch(this.handleError(ModelType.URL, id));
+    return this.cms
+      .url(id)
+      .catch(this.handleError(ModelType.URL, id))
+      .then(this.validate);
   }
 
   image(id: string): Promise<Image> {
-    return this.cms.image(id).catch(this.handleError(ModelType.IMAGE, id));
+    return this.cms
+      .image(id)
+      .catch(this.handleError(ModelType.IMAGE, id))
+      .then(this.validate);
   }
 
   queue(id: string): Promise<Queue> {
-    return this.cms.queue(id).catch(this.handleError(ModelType.QUEUE, id));
+    return this.cms
+      .queue(id)
+      .catch(this.handleError(ModelType.QUEUE, id))
+      .then(this.validate);
   }
 
   contentsWithKeywords(): Promise<SearchResult[]> {
