c2a759c9b41469b05e73c5a6def7b7318714acfd
Dani Pinyol <dpinol@gmail.com>
2019-07-11 12:26:51 +0200

    chores(dynamo): add more eslint plugins and rules

--- packages/botonic-plugin-dynamodb/.eslintrc.js
+++ packages/botonic-plugin-dynamodb/.eslintrc.js
@@ -2,19 +2,23 @@ module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/eslint-recommended",
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "eslint:recommended",
+    "plugin:jest/recommended"
   ],
   plugins: [
+    "jest",
     "no-null",
-    "filenames"
+    "filenames",
+    "promise",
+    "@typescript-eslint"
   ],
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaVersion: 2017, // async is from ecma2017. Supported in node >=7.10
-    sourceType: "module", // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
+    sourceType: "module" // Allows for the use of imports
   },
 // npm run lint runs eslint with --quiet --fix so that only errors are fixed
   rules: {
@@ -34,22 +38,34 @@ module.exports = {
     // allow public functions/classes to call private functions/classes declared below.
     // otoh, variables (typically constants) should be declared at the top
     "@typescript-eslint/no-use-before-define": ["error", { "variables": true, "functions": false, "classes": false }],
+    "no-console" :"off",
 
     // special for TYPESCRIPT
     "no-null/no-null": "warn", // fields declared with ? are undefined, not null (be aware that React uses null)
     "@typescript-eslint/explicit-member-accessibility": "off", //we think defaulting to public is a good default
     "@typescript-eslint/explicit-function-return-type": "off", // annoying for tests
     "@typescript-eslint/no-useless-constructor": "warn",
+    "@typescript-eslint/no-floating-promises": "error", // see https://github.com/xjamundx/eslint-plugin-promise/issues/151
     "no-useless-constructor": "off", //makes no sense for TS (https://github.com/typescript-eslint/typescript-eslint/issues/426)
     "@typescript-eslint/no-parameter-properties": "off", // opinionated: parameter properties make data classes shorter
     "valid-jsdoc": "off", // function comments hide code complexity (and typescript already have type specifications),
     "unicorn/prevent-abbreviations" : "off", // the plugin removes removes type annotations from typescript code :-(
     "unicorn/filename-case" : "off", // React convention is in CamelCase
     "@typescript-eslint/no-non-null-assertion" : "warn", // specially useful in tests, and "when you know what you're doing"
-    "@typescript-eslint/no-object-literal-type-assertion" : {allowAsParameter: false}, //useful to pass options to plugins
-    "@typescript-eslint/no-namespace": { allowDeclarations: true }, // to encapsulate types in namespace with same name as Class
+    "@typescript-eslint/no-object-literal-type-assertion" : [ "error", {allowAsParameter: false}], //useful to pass options to plugins
+    "@typescript-eslint/no-namespace": ["error", { allowDeclarations: true }], // to encapsulate types in namespace with same name as Class
+    "no-empty-pattern" : "off",
+    "require-await": "error"
+
+    // nice rules which are on development
+    // https://github.com/typescript-eslint/typescript-eslint/pull/612
   },
   env: {
-    jest: true
+    jest: true,
+    "jest/globals": true,
+    es6: true,
+    // browser/node to prevent "'console' is not defined  no-undef"
+    browser: true,
+    node: true
   }
 };
