0969146b46f8e63b6641ccdb740708805595bf9c
Dani Pinyol <dpinol@gmail.com>
2019-07-18 07:22:36 +0200

    feature(contentful): support en, ca & pt tokenizer/stemmers
    polish not supported by node-nlp

---
+++ packages/botonic-plugin-contentful/src/nlp/stemmer.ts
@@ -0,0 +1,25 @@
+import { BaseStemmer } from 'node-nlp/lib/nlp/stemmers';
+import { Locale } from './index';
+// porter stemmers are simpler than the other ones, but they are the default in node-nlp. Why?
+import CatalanStemmer from 'node-nlp/lib/nlp/stemmers/catalan-stemmer';
+import stemmerEn from 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer';
+import stemmerEs from 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer-es';
+import stemmerPt from 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer-pt';
+import NlpUtil = require('node-nlp/lib/nlp/nlp-util');
+
+const catalanStemmer = new CatalanStemmer(NlpUtil.getTokenizer('ca'));
+export const stemmers: { [key: string]: BaseStemmer } = {
+  ca: catalanStemmer,
+  en: stemmerEn,
+  es: stemmerEs,
+  pr: stemmerPt
+  //node-nlp does not support polish
+};
+
+export function stemmerFor(locale: Locale): BaseStemmer {
+  let stem = stemmers[locale];
+  if (!stem) {
+    throw new Error(`No stemmer configured for locale '${locale}'`);
+  }
+  return stem;
+}
--- packages/botonic-plugin-contentful/src/typings.d.ts
+++ packages/botonic-plugin-contentful/src/typings.d.ts
@@ -1,3 +1,8 @@
+declare module 'node-nlp/lib/nlp/tokenizers' {
+  export class Tokenizer {
+    tokenize(text: string): string[];
+  }
+}
 declare module 'node-nlp/lib/nlp/stemmers' {
   export class BaseStemmer {
     tokenizeAndStem(str: string, keepStops: boolean): string[];
@@ -5,16 +10,45 @@ declare module 'node-nlp/lib/nlp/stemmers' {
 }
 
 declare module 'node-nlp/lib/nlp/nlp-util' {
+  import { Tokenizer } from 'node-nlp/lib/nlp/tokenizers';
   import { BaseStemmer } from 'node-nlp/lib/nlp/stemmers';
   class NlpUtil {
     static getStemmer(locale: string): BaseStemmer;
+    static getTokenizer(locale: string): Tokenizer;
   }
+
   export = NlpUtil;
 }
 
 // We use directly the ES Stemmer instead of using NlpUtil to avoid loading dependencies of so many languages
 // (there are evn errors with 'fs' library and with Japanese dependencies)
 declare module 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer-es' {
+  class PorterStemmer {
+    tokenizeAndStem(str: string, keepStops: boolean): string[];
+  }
+  const stemmer: PorterStemmer;
+  export = stemmer;
+}
+
+declare module 'node-nlp/lib/nlp/stemmers/catalan-stemmer' {
+  import { Tokenizer } from 'node-nlp/lib/nlp/tokenizers';
+
+  class CatalanStemmer {
+    constructor(tokenizer: Tokenizer);
+    tokenizeAndStem(str: string, keepStops: boolean): string[];
+  }
+  export = CatalanStemmer;
+}
+
+declare module 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer' {
+  class PorterStemmer {
+    tokenizeAndStem(str: string, keepStops: boolean): string[];
+  }
+  const stemmer: PorterStemmer;
+  export = stemmer;
+}
+
+declare module 'node-nlp/lib/nlp/stemmers/natural/porter-stemmer-pt' {
   class PorterStemmer {
     tokenizeAndStem(str: string, keepStops: boolean): string[];
   }
