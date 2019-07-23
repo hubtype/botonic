671f359c782aca19cb14b26962d8207b14ccd926
Eric Marcos <ericmarcos.p@gmail.com>
2019-07-23 12:08:57 +0200

    chore(react): add deprecation notice to App, throw link to migration guide

---
+++ packages/botonic-react/src/app.jsx
@@ -0,0 +1,6 @@
+
+export class App {
+    constructor() {
+        throw "This setup is deprecated in Botonic versions >=0.9.0. Please read the migration guide at: https://github.com/hubtype/botonic/tree/master/packages/botonic-react/src/MIGRATION_GUIDE.md";
+    }
+}
