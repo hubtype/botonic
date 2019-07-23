04fb51cc495e49970d48fba4296437f02273926d
Eric Marcos <ericmarcos.p@gmail.com>
2019-07-23 14:05:39 +0200

    feat(core): add end-to-end encryption for webchat

--- packages/botonic-core/src/hubtype-service.js
+++ packages/botonic-core/src/hubtype-service.js
@@ -1,7 +1,9 @@
 import Pusher from 'pusher-js'
 import axios from 'axios'
+import { secretbox } from 'tweetnacl'
+import { encodeUTF8, decodeBase64 } from 'tweetnacl-util'
 
-const PUSHER_KEY = 'da85029877df0c827e44'
+const PUSHER_KEY = process.env.WEBCHAT_PUSHER_KEY || '434ca667c8e6cb3f641c'
 const HUBTYPE_API_URL = process.env.HUBTYPE_API_URL || 'https://api.hubtype.com'
 
 export class HubtypeService {
@@ -14,10 +16,30 @@ export class HubtypeService {
   initPusher(user) {
     if (user) this.user = user
     if (this.pusher || !this.user.id || !this.appId) return
-
-    this.pusher = new Pusher(PUSHER_KEY)
+    this.pusher = new Pusher(PUSHER_KEY, {
+      cluster: 'eu',
+      authEndpoint: `${HUBTYPE_API_URL}/v1/provider_accounts/webhooks/webchat/${this.appId}/auth/`,
+      auth: {
+        headers: {
+          'X-BOTONIC-USER-ID': this.user.id
+        }
+      }
+    })
     this.pusher.subscribe(this.pusherChannel)
-    this.pusher.bind('botonic_response', event => this.onEvent(event))
+    this.pusher.bind('botonic_response', data => {
+      try {
+        let cipherText = decodeBase64(data.ciphertext)
+        let nonce = decodeBase64(data.nonce)
+        let bytes = secretbox.open(
+          cipherText,
+          nonce,
+          this.pusher.allChannels()[0].key
+        )
+        this.onEvent(JSON.parse(encodeUTF8(bytes)))
+      } catch (e) {
+        console.log(e)
+      }
+    })
   }
 
   onPusherEvent(event) {
@@ -25,10 +47,10 @@ export class HubtypeService {
   }
 
   get pusherChannel() {
-    return `public-${this.appId}-${this.user.id}`
+    return `private-encrypted-${this.appId}-${this.user.id}`
   }
 
-  async postMessage(user, message) {
+  postMessage(user, message) {
     this.initPusher(user)
     return axios.post(
       `${HUBTYPE_API_URL}/v1/provider_accounts/webhooks/webchat/${this.appId}/`,
