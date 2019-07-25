09e4660cd0e0de98cbec0dc684d49cef9965b593
Dani Pinyol <dpinol@gmail.com>
2019-07-25 12:26:15 +0200

    chore(contentful) moved plugin to its own file

---
+++ packages/botonic-plugin-contentful/src/plugin.ts
@@ -0,0 +1,49 @@
+import * as cms from './cms';
+import Contentful from './contentful';
+import { Search } from './search';
+import { BotonicMsgConverter } from './render';
+
+interface OptionsBase {
+  renderer?: BotonicMsgConverter;
+  search?: Search;
+}
+
+export interface CmsOptions extends OptionsBase {
+  cms?: cms.CMS;
+}
+
+export interface ContentfulOptions extends OptionsBase {
+  spaceId: string;
+  accessToken: string;
+}
+
+export default class BotonicPluginContentful {
+  readonly cms: cms.CMS;
+
+  readonly renderer: BotonicMsgConverter;
+
+  readonly search: Search;
+
+  constructor(options: CmsOptions | ContentfulOptions) {
+    let optionsAny = options as any;
+    if (optionsAny.cms) {
+      this.cms = optionsAny.cms;
+    } else {
+      let contOptions = options as ContentfulOptions;
+      this.cms = new Contentful(contOptions.spaceId, contOptions.accessToken);
+    }
+    this.cms = new cms.ErrorReportingCMS(this.cms);
+    this.renderer = options.renderer || new BotonicMsgConverter();
+    this.search = options.search || new Search(this.cms);
+  }
+
+  // @ts-ignore
+  pre({ input, session, lastRoutePath }) {
+    return { input, session, lastRoutePath };
+  }
+
+  // @ts-ignore
+  post({ input, session, lastRoutePath, response }) {
+    return { input, session, lastRoutePath, response };
+  }
+}
