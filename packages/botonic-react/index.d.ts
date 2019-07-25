11d862bbd024bdc660442cb723edade67961c611
Marc Rabat <marcrabat@gmail.com>
2019-07-25 20:35:40 +0200

    fix: fix index.d.ts for iberdrola bot

--- packages/botonic-react/index.d.ts
+++ packages/botonic-react/index.d.ts
@@ -1,43 +1,43 @@
-import * as React from 'react';
-import * as core from '@botonic/core';
+import * as React from 'react'
+import * as core from '@botonic/core'
 
-export interface MessageProps{
-  type? : string;
-  from? : string;
-  delay? : number;
-  typing? : number;
-  children : any;
-  json: any;
-  style: any;
+export interface MessageProps {
+  type?: string
+  from?: string
+  delay?: number
+  typing?: number
+  children: any
+  json: any
+  style: any
 }
 export class Message extends React.Component<MessageProps, any> {}
 
-
-export interface TextProps{
-  delay?: number;
-  typing?: number;
+export interface TextProps {
+  delay?: number
+  typing?: number
 }
 export class Text extends React.Component<TextProps, any> {}
 
 export interface Webview {
-  name: string;
+  name: string
 }
 
-export interface ButtonProps{
-  payload?: string;
-  url?: string;
-  path?: string;
-  webview?: Webview;
+export interface ButtonProps {
+  payload?: string
+  url?: string
+  path?: string
+  webview?: Webview
+  params?: any
 }
 
 export class Button extends React.Component<ButtonProps, any> {}
 
-export type ReplyProps = ButtonProps;
+export type ReplyProps = ButtonProps
 
 export class Reply extends React.Component<ReplyProps, any> {}
 
-export interface PicProps{
-  src: string;
+export interface PicProps {
+  src: string
 }
 export class Pic extends React.Component<PicProps, any> {}
 export class Image extends React.Component<PicProps, any> {}
@@ -47,73 +47,80 @@ export class Title extends React.Component<any, any> {}
 export class Subtitle extends React.Component<any, any> {}
 export class Element extends React.Component<any, any> {}
 
-export type Locales = {[id: string]: string|string[]|Locales };
+export type Locales = { [id: string]: string | string[] | Locales }
 
 export class App {
   routes: core.Route[]
-  locales: Locales;
-  integrations?: {[id: string]: any};
-  theme?: string;
-  plugins: { [id: string]: any};
-  defaultTyping : number;
-  defaultDelay : number;
+  locales: Locales
+  integrations?: { [id: string]: any }
+  theme?: string
+  plugins: { [id: string]: any }
+  defaultTyping: number
+  defaultDelay: number
 
   constructor(app: {
-    routes: core.Route[],
-    locales: Locales,
-    integrations?: {[id: string]: any};
-    theme?: string,
-    plugins?: { [id: string]: any},
-    defaultTyping?: number,
+    routes: core.Route[]
+    locales: Locales
+    integrations?: { [id: string]: any }
+    theme?: string
+    plugins?: { [id: string]: any }
+    defaultTyping?: number
     defaultDelay?: number
-  });
+  })
 }
 
 export interface Input {
-  type: string; // text, postback...
-  payload?: string;
-  data?: string;
+  type: string // text, postback...
+  payload?: string
+  data?: string
 }
 
 // Parameters of the actions' botonicInit method
 export interface ActionInitInput {
-  input : Input;
-  session: core.Session;
-  params: any;
-  lastRoutePath: any;
-  plugins: any;
+  input: Input
+  session: core.Session
+  params: any
+  lastRoutePath: any
+  plugins: any
 }
 
 export class BotonicInputTester {
-  constructor(app: App);
+  constructor(app: App)
 
-  text(inp: string, session?: core.Session, lastRoutePath?: string): Promise<string>;
+  text(
+    inp: string,
+    session?: core.Session,
+    lastRoutePath?: string
+  ): Promise<string>
 
-  payload(inp: string, session?: core.Session, lastRoutePath?: string): Promise<string>;
+  payload(
+    inp: string,
+    session?: core.Session,
+    lastRoutePath?: string
+  ): Promise<string>
 }
 
 export class BotonicOutputTester {
-  constructor(app: any);
+  constructor(app: any)
 
-  text(out: string, replies?: any): Promise<string>;
+  text(out: string, replies?: any): Promise<string>
 }
 
-export const RequestContext: React.Context<any>;
-
+export const RequestContext: React.Context<any>
 
 // plugins
 
 // Arguments of the plugin pre() method
 export interface PluginPreInput {
-  input: Input;
-  session: core.Session;
-  lastRoutePath: string;
+  input: Input
+  session: core.Session
+  lastRoutePath: string
 }
 
 // Arguments of the plugin post() method
 export interface PluginPostInput {
-  input: Input;
-  session: core.Session;
-  lastRoutePath: string;
-  response: string;
+  input: Input
+  session: core.Session
+  lastRoutePath: string
+  response: string
 }
