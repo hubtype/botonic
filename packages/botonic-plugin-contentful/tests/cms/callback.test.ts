584a5198d70f1dc3000ce86f8959fcf299cbb2d4
Dani Pinyol <dpinol@gmail.com>
2019-07-25 09:05:40 +0200

    chore(contentful): remove async from functions that don't need it

--- packages/botonic-plugin-contentful/tests/cms/callback.test.ts
+++ packages/botonic-plugin-contentful/tests/cms/callback.test.ts
@@ -2,7 +2,7 @@ import { instance, mock } from 'ts-mockito';
 import { ContentCallback, ModelType } from '../../src';
 import * as cms from '../../src';
 
-test('TEST: ofPayload', async () => {
+test('TEST: ofPayload', () => {
   let callback = cms.Callback.ofPayload('text$text1');
   expect(callback).toBeInstanceOf(ContentCallback);
   expect((callback as ContentCallback).id).toEqual('text1');
@@ -25,7 +25,7 @@ test('TEST: callbackMap fixed callback', () => {
   expect(sut.getCallback(Math.random().toString())).toBe(callback);
 });
 
-test('TEST: regexForModel', async () => {
+test('TEST: regexForModel', () => {
   let callback = new cms.ContentCallback(ModelType.CAROUSEL, 'id1');
   expect(
     cms.ContentCallback.regexForModel(ModelType.CAROUSEL).test(
