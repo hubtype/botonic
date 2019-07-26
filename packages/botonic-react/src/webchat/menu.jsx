52db05353174669995ae9b47bd5b77ef804e3e90
imOscarCrespo <oscarcrespo5@gmail.com>
2019-07-26 11:16:16 +0200

    default components

--- packages/botonic-react/src/webchat/menu.jsx
+++ packages/botonic-react/src/webchat/menu.jsx
@@ -1,71 +1,42 @@
 import React, { useContext, useState } from 'react'
 import { WebchatContext } from '../contexts'
 import { Flex } from '@rebass/grid'
-import { Button } from '@botonic/react/src/components/button'
+import { Button } from '../components/button'
 import { WebchatMessageList } from './messageList'
+import Textarea from 'react-textarea-autosize'
 
 const DefaultMenu = () => {
-  //const [reset, setReset] = useState(false)
-  //const { webchatState } = useContext(WebchatContext)
-  /*const resetBot = () => {
-    if (!reset) {
-      webchatState.messagesComponents = []
-      webchatState.replies = []
-      webchatState.messagesJSON = []
-      setReset(true)
-    }
-  }*/
+  const { webchatState } = useContext(WebchatContext)
   return (
-    <Flex
-      flexDirection="column"
+    <Textarea
+      name="text"
+      minRows={2}
+      maxRows={4}
+      wrap="soft"
+      maxLength="1000"
+      placeholder={webchatState.theme.textPlaceholder}
+      autoFocus={location.hostname === 'localhost'}
+      inputRef={textArea}
+      onKeyDown={e => onKeyDown(e)}
       style={{
-        position: 'absolute',
-        top: 284,
-        left: 14
+        display: 'flex',
+        padding: '8px 10px',
+        fontSize: 14,
+        border: 'none',
+        borderTop: '1px solid rgba(0, 0, 0, 0.4)',
+        resize: 'none',
+        overflow: 'auto',
+        outline: 'none'
       }}
-    >
-      <Flex width={200} flexDirection="column">
-        <Flex>
-          <Button onClick={() => resetBot()} top={`8px`}>
-            Start Over
-          </Button>
-        </Flex>
-        <Flex>
-          <Button>Help</Button>
-        </Flex>
-        <Flex>
-          <Button bottom={`8px`}>Rate this bot</Button>
-        </Flex>
-      </Flex>
-    </Flex>
+    />
   )
 }
 
-const CustomMenu = ({ options }) => {
-  return (
-    <Flex
-      flexDirection="column"
-      style={{
-        position: 'absolute',
-        top: 284,
-        left: 14
-      }}
-    >
-      <Flex width={200} flexDirection="column">
-        {options.map((e, i) => (
-          <Flex key={i}>
-            <Button payload={e.payload}>{e.label}</Button>
-          </Flex>
-        ))}
-      </Flex>
-    </Flex>
-  )
-}
-
-export const Menu = props => {
-  if (props.options) {
-    return <CustomMenu options={props.options} />
-  } else {
-    return <DefaultMenu />
+export const WebchatMenu = props => {
+  const { webchatState } = useContext(WebchatContext)
+  if (webchatState.theme.customMenu) {
+    let CustomMenu = webchatState.theme.customMenu
+    return <CustomMenu />
   }
+  return <DefaultMenu />
 }
