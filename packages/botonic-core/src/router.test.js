c3d119ce00ddafb0051be246b12a9cae8faf7ed3
Eric Marcos <ericmarcos.p@gmail.com>
2019-08-06 12:30:10 +0200

    test(core): add router unit tests

---
+++ packages/botonic-core/src/router.test.js
@@ -0,0 +1,226 @@
+const { Router } = require('./router')
+
+const textInput = { type: 'text', data: 'hi' }
+const textInputComplex = { type: 'text', data: 'Cömplêx input &% 🚀' }
+const textPayloadInput = { type: 'text', data: 'hi', payload: 'foo' }
+const postbackInput = { type: 'postback', payload: 'foo' }
+
+describe('Bad router initialization', () => {
+  test('empty routes throw TypeError', () => {
+    let router = new Router([])
+    expect(() => router.processInput2(textInput)).toThrow(TypeError)
+  })
+  test('null routes throw TypeError', () => {
+    let router = new Router()
+    expect(() => router.processInput2(textInput)).toThrow(TypeError)
+  })
+})
+
+test('Router returns 404', () => {
+  let router = new Router([{ path: '404', action: '404Action' }])
+  let { action } = router.processInput2(textInput)
+  expect(action).toBe('404Action')
+})
+
+describe('Match route by MATCHER <> INPUT', () => {
+  let router = new Router()
+  test('text <> text', () => {
+    expect(router.matchRoute('text', 'hi', textInput)).toBeTruthy()
+    expect(router.matchRoute('text', 'hii', textInput)).toBeFalsy()
+    expect(router.matchRoute('text', 'bye', textInput)).toBeFalsy()
+    expect(router.matchRoute('text', '', textInput)).toBeFalsy()
+    expect(router.matchRoute('text', null, textInput)).toBeFalsy()
+    expect(
+      router.matchRoute('text', 'Cömplêx input &% 🚀', textInputComplex)
+    ).toBeTruthy()
+    expect(
+      router.matchRoute('text', ' Cömplêx input &% 🚀', textInputComplex)
+    ).toBeFalsy() // has a space at the beginning
+  })
+  test('regex <> text', () => {
+    expect(router.matchRoute('text', /hi/, textInput)).toBeTruthy()
+    expect(router.matchRoute('text', /bye/, textInput)).toBeFalsy()
+    expect(router.matchRoute('text', /🚀/, textInputComplex)).toBeTruthy()
+    expect(router.matchRoute('text', /complex/, textInputComplex)).toBeFalsy()
+  })
+  test('function <> text', () => {
+    expect(
+      router.matchRoute('text', v => v.startsWith('hi'), textInput)
+    ).toBeTruthy()
+    expect(
+      router.matchRoute('text', v => !v.startsWith('hi'), textInput)
+    ).toBeFalsy()
+  })
+  test('input <> text', () => {
+    expect(
+      router.matchRoute('input', i => i.data.startsWith('hi'), textInput)
+    ).toBeTruthy()
+    expect(
+      router.matchRoute('input', i => !i.data.startsWith('hi'), textInput)
+    ).toBeFalsy()
+  })
+  test('text <> text payload', () => {
+    expect(router.matchRoute('payload', 'foo', textPayloadInput)).toBeTruthy()
+    expect(router.matchRoute('payload', 'fooo', textPayloadInput)).toBeFalsy()
+    expect(router.matchRoute('payload', 'bar', textPayloadInput)).toBeFalsy()
+    expect(router.matchRoute('payload', '', textPayloadInput)).toBeFalsy()
+    expect(router.matchRoute('payload', null, textPayloadInput)).toBeFalsy()
+  })
+  test('regex <> text payload', () => {
+    expect(router.matchRoute('payload', /foo/, textPayloadInput)).toBeTruthy()
+    expect(router.matchRoute('payload', /bar/, textPayloadInput)).toBeFalsy()
+  })
+  test('function <> text payload', () => {
+    expect(
+      router.matchRoute('payload', v => v.startsWith('fo'), textPayloadInput)
+    ).toBeTruthy()
+    expect(
+      router.matchRoute('payload', v => !v.startsWith('fo'), textPayloadInput)
+    ).toBeFalsy()
+  })
+  test('text <> postback', () => {
+    expect(router.matchRoute('payload', 'foo', postbackInput)).toBeTruthy()
+    expect(router.matchRoute('payload', 'fooo', postbackInput)).toBeFalsy()
+    expect(router.matchRoute('payload', 'bar', postbackInput)).toBeFalsy()
+    expect(router.matchRoute('payload', '', postbackInput)).toBeFalsy()
+    expect(router.matchRoute('payload', null, postbackInput)).toBeFalsy()
+  })
+  test('regex <> postback', () => {
+    expect(router.matchRoute('payload', /foo/, postbackInput)).toBeTruthy()
+    expect(router.matchRoute('payload', /bar/, postbackInput)).toBeFalsy()
+  })
+  test('function <> postback', () => {
+    expect(
+      router.matchRoute('payload', v => v.startsWith('fo'), postbackInput)
+    ).toBeTruthy()
+    expect(
+      router.matchRoute('payload', v => !v.startsWith('fo'), postbackInput)
+    ).toBeFalsy()
+  })
+})
+
+describe('Get route by path', () => {
+  const externalRoutes = [
+    { path: '', action: 'DefaultAction' },
+    { path: 'child', action: 'ChildAction' }
+  ]
+  const router = new Router([
+    { path: 'initial', action: 'Initial' },
+    {
+      path: 'flow-1',
+      action: 'Flow1',
+      childRoutes: [
+        {
+          path: '1',
+          action: 'Flow1.1',
+          childRoutes: [
+            { path: '1', action: 'Flow1.1.1' },
+            { path: '2', action: 'Flow1.1.2' },
+            { path: '3', action: 'Flow1.1.3' }
+          ]
+        },
+        { path: '2', action: 'Flow1.2', childRoutes: externalRoutes },
+        {
+          path: '3',
+          action: 'Flow1.3',
+          childRoutes: [
+            { path: '1', action: 'Flow1.3.1' },
+            { path: '2', action: 'Flow1.3.2' },
+            { path: '3', action: 'Flow1.3.3' }
+          ]
+        }
+      ]
+    },
+    { path: '404', action: '404Action' }
+  ])
+  test('path exists', () => {
+    expect(router.getRouteByPath('initial').action).toBe('Initial')
+    expect(router.getRouteByPath('flow-1/1').action).toBe('Flow1.1')
+    expect(router.getRouteByPath('flow-1/3/2').action).toBe('Flow1.3.2')
+  })
+  test('path exists in composed child routes', () => {
+    expect(router.getRouteByPath('flow-1/2').action).toBe('Flow1.2')
+    expect(router.getRouteByPath('flow-1/2/child').action).toBe('ChildAction')
+  })
+  test('path does not exist', () => {
+    expect(router.getRouteByPath('')).toBeNull()
+    expect(router.getRouteByPath('foo')).toBeNull()
+    expect(router.getRouteByPath('flow-1/3/2/6')).toBeNull()
+  })
+})
+
+describe('Process input (v<0.9)', () => {
+  const externalRoutes = [
+    { path: '', action: 'DefaultAction' },
+    { path: 'child', action: 'ChildAction' }
+  ]
+  const router = new Router([
+    { path: 'help', payload: 'help', action: 'Help' },
+    {
+      path: 'initial',
+      intent: /greeting/,
+      action: 'Flow1',
+      childRoutes: [
+        {
+          path: '1',
+          payload: '1',
+          action: 'Flow1.1',
+          childRoutes: [
+            { path: '1', payload: '1', action: 'Flow1.1.1' },
+            { path: '2', payload: '2', action: 'Flow1.1.2' },
+            { path: '3', payload: '3', action: 'Flow1.1.3' }
+          ]
+        },
+        {
+          path: '2',
+          payload: '2',
+          action: 'Flow1.2',
+          childRoutes: externalRoutes
+        },
+        {
+          path: '3',
+          payload: '3',
+          action: 'Flow1.3',
+          childRoutes: [
+            { path: '1', payload: '1', action: 'Flow1.3.1' },
+            { path: '2', payload: '2', action: 'Flow1.3.2' },
+            { path: '3', payload: '3', action: 'Flow1.3.3' }
+          ]
+        }
+      ]
+    },
+    { path: '404', action: '404Action' }
+  ])
+  test('text input, root level route', () => {
+    let input = { type: 'text', data: 'hi', intent: 'greeting' }
+    let session = {}
+    let lastRoutePath = null
+    expect(router.processInput2(input, session, lastRoutePath).action).toBe(
+      'Flow1'
+    )
+  })
+  test('payload input, 2nd level route', () => {
+    let input = { type: 'postback', payload: '2' }
+    let session = {}
+    let lastRoutePath = 'initial'
+    expect(router.processInput2(input, session, lastRoutePath).action).toBe(
+      'Flow1.2'
+    )
+  })
+  test('path payload input, root level route (should be ignored)', () => {
+    let input = { type: 'postback', payload: '__PATH_PAYLOAD__initial/2' }
+    let session = {}
+    let lastRoutePath = ''
+    expect(router.processInput2(input, session, lastRoutePath).action).toBe(
+      'Flow1.2'
+    )
+  })
+  test('path payload input, 2nd level route (should be ignored)', () => {
+    let input = { type: 'postback', payload: '__PATH_PAYLOAD__initial/2' }
+    let session = {}
+    let lastRoutePath = 'initial'
+    expect(router.processInput2(input, session, lastRoutePath).action).toBe(
+      'Flow1.2'
+    )
+  })
+})
--- packages/botonic-core/src/utils.js
+++ packages/botonic-core/src/utils.js
@@ -14,3 +14,34 @@ export const params2queryString = params =>
   Object.entries(params)
     .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
     .join('&')
+
+// Joins path segments.  Preserves initial "/" and resolves ".." and "."
+// Does not support using ".." to go above/outside the root.
+// This means that join("foo", "../../bar") will not resolve to "../bar"
+export const join = (/* path segments */) => {
+  // Split the inputs into a list of path commands.
+  var parts = []
+  for (var i = 0, l = arguments.length; i < l; i++) {
+    parts = parts.concat(arguments[i].split('/'))
+  }
+  // Interpret the path commands to get the new resolved path.
+  var newParts = []
+  for (i = 0, l = parts.length; i < l; i++) {
+    var part = parts[i]
+    // Remove leading and trailing slashes
+    // Also remove "." segments
+    if (!part || part === '.') continue
+    // Interpret ".." to pop the last segment
+    if (part === '..') newParts.pop()
+    // Push new path segments.
+    else newParts.push(part)
+  }
+  // Preserve the initial slash if there was one.
+  if (parts[0] === '') newParts.unshift('')
+  // Turn back into a single string path.
+  return newParts.join('/') || (newParts.length ? '/' : '.')
+}
+
+// A simple function to get the dirname of a path
+// Trailing slashes are ignored. Leading slash is preserved.
+export const dirname = path => join(path, '..')
