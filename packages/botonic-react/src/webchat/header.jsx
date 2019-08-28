71894e5205d2ae361d38d0a5b84e18239cece89a
imOscarCrespo <oscarcrespo5@gmail.com>
2019-08-28 15:51:41 +0200

    add: fixing some issues

--- packages/botonic-react/src/webchat/header.jsx
+++ packages/botonic-react/src/webchat/header.jsx
@@ -1,62 +1,3 @@
-// import React, { useContext } from 'react'
-// import { WebchatContext } from '../contexts'
-// import Logo from './botonic_react_logo100x100.png'
-
-// export const WebchatHeader = props => {
-//   const { webchatState, staticAssetsUrl, toggleWebchat } = useContext(
-//     WebchatContext
-//   )
-
-//   if (webchatState.theme.customHeader) {
-//     let CustomHeader = webchatState.theme.customHeader
-//     return <CustomHeader />
-//   }
-//   return (
-//     <div
-//       style={{
-//         ...(props.style || {}),
-//         display: 'flex',
-//         alignItems: 'center',
-//         justifyContent: 'flex-start',
-//         backgroundColor: '#b0c4de',
-//         color: '#295179'
-//       }}
-//     >
-//       <img
-//         style={{
-//           height: 24,
-//           margin: '0px 12px'
-//         }}
-//         src={staticAssetsUrl + (webchatState.theme.brandIconUrl || Logo)}
-//       />
-//       <h4
-//         style={{
-//           margin: 0,
-//           fontFamily: 'Arial, Helvetica, sans-serif'
-//         }}
-//       >
-//         {webchatState.theme.title || 'Botonic'}
-//       </h4>
-//       <div
-//         style={{
-//           cursor: 'pointer',
-//           fontSize: '16px',
-//           color: 'black',
-//           position: 'absolute',
-//           right: '10px',
-//           top: '9px'
-//         }}
-//         onClick={event => {
-//           toggleWebchat(false)
-//           event.preventDefault()
-//         }}
-//       >
-//         ✕
-//       </div>
-//     </div>
-//   )
-// }
-
 import React, { useContext } from 'react'
 import { WebchatContext } from '../contexts'
 import styled from 'styled-components'
@@ -80,7 +21,7 @@ const Subtitle = styled.h1`
 `
 const Diffuse = styled(Flex)`
   background: linear-gradient(90deg, #2e203b 0%, ${props => props.color} 100%);
-  width: 1;
+  width: 100%;
   height: 55px;
   border-radius: 6px 6px 0px 0px;
 `
@@ -97,7 +38,7 @@ export const DefaultHeader = props => (
         src={Logo}
       />
     </Flex>
-    <Flex width={1} flexDirection="column">
+    <Flex width={1} flexDirection='column'>
       <HeaderTitle>Botonic</HeaderTitle>
       <Subtitle>Online</Subtitle>
     </Flex>
