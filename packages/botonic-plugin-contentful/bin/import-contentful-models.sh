df2f8ec61897f91f64c797b15630d415a993b483
Dani Pinyol <dpinol@gmail.com>
2019-09-04 11:40:34 +0200

    doc(contentful): plugin doc and script to import Contentful model

---
+++ packages/botonic-plugin-contentful/bin/import-contentful-models.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+BIN_DIR=${0:a:h}
+
+if [[ "$CONTENTFUL_SPACEID" == "" ]]; then
+  echo "Assign your contentful account spaceid to environment variable CONTENTFUL_SPACEID"
+  exit 1
+fi
+
+if [[ "$CONTENTFUL_TOKEN" == "" ]]; then
+  echo "Assign your Contentful Content management token to environment variable CONTENTFUL_TOKEN"
+  exit 1
+fi
+
+contentful space import --space-id=$CONTENTFUL_SPACEID \
+    --management-token=$CONTENTFUL_TOKEN \
+    --content-file "$BIN_DIR/../exports/contentful-export-0.9.16.json"
