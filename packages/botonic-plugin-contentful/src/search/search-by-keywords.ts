3fcaf74eab002a9d75f9a3ef98dac816fc6663ee
Dani Pinyol <dpinol@gmail.com>
2019-09-12 14:13:32 +0200

    chore(contentful): expose KeywordsOptions in plugins options

--- packages/botonic-plugin-contentful/src/search/search-by-keywords.ts
+++ packages/botonic-plugin-contentful/src/search/search-by-keywords.ts
@@ -1,24 +1,16 @@
 import * as cms from '../cms';
 import { Context } from '../cms';
-import { Locale, tokenizeAndStem } from '../nlp';
-import {
-  KeywordsOptions,
-  KeywordsParser,
-  MatchType,
-  SortType
-} from '../nlp/keywords';
+import { KeywordsOptions, KeywordsParser, MatchType } from '../nlp/keywords';
 import { checkLocale } from '../nlp/locales';
-import { StemmingEscaper } from '../nlp/node-nlp';
+import { Tokenizer } from '../nlp/tokens';
 import { SearchResult } from './search-result';
 
 export class SearchByKeywords {
-  constructor(readonly cms: cms.CMS, readonly stemEscaper: StemmingEscaper) {}
-
-  tokenize(locale: Locale, inputText: string): string[] {
-    const escaped = this.stemEscaper.escape(inputText);
-    const stems = tokenizeAndStem(locale, escaped);
-    return stems.map(stem => this.stemEscaper.unescape(stem));
-  }
+  constructor(
+    readonly cms: cms.CMS,
+    readonly tokenizer: Tokenizer,
+    readonly keywordsOptions = new KeywordsOptions()
+  ) {}
 
   async searchContentsFromInput(
     inputTextTokens: string[],
@@ -30,7 +22,8 @@ export class SearchByKeywords {
     const kws = new KeywordsParser<SearchResult>(
       locale,
       matchType,
-      new KeywordsOptions(1, (l, i) => this.tokenize(l, i), SortType.LENGTH)
+      this.tokenizer,
+      this.keywordsOptions
     );
     contentsWithKeywords.forEach(content =>
       kws.addCandidate(content, content.keywords!)
