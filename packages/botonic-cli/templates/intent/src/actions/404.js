b22ba849239eceac831228fe9ffa09e993ecb683
Marc Rabat <marcrabat@gmail.com>
2019-10-03 23:13:31 +0200

    fix(cli): update intent template to work with dialogflow-V2 for release 0.9.0

--- packages/botonic-cli/templates/intent/src/actions/404.js
+++ packages/botonic-cli/templates/intent/src/actions/404.js
@@ -5,8 +5,8 @@ export default class extends React.Component {
   render() {
     return (
       <Text>
-        Enter the dialogflow token in integrations.js to test the bot. Try
-        typing "hello" to start the bot.
+        Enter the generated JSON key for dialogflowV2 in plugins.js to test the
+        bot. Try typing "hello" to start the bot.
       </Text>
     )
   }
