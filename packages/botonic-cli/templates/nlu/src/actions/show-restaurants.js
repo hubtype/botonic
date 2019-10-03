31e159da4e91af076ae6d716913a2ae5aef21617
Marc Rabat <marcrabat@gmail.com>
2019-10-03 23:22:49 +0200

    fix(cli): update nlu template with entities example for release 0.9.0

--- packages/botonic-cli/templates/nlu/src/actions/show-restaurants.js
+++ packages/botonic-cli/templates/nlu/src/actions/show-restaurants.js
@@ -1,53 +1,65 @@
 import React from 'react'
-import {
-  RequestContext,
-  Text,
-  Button,
-  Carousel,
-  Pic,
-  Element,
-  Title,
-  Subtitle
-} from '@botonic/react'
-
-import Rest1 from '../assets/Rest1.jpg'
-import Rest2 from '../assets/Rest2.jpg'
-import Rest3 from '../assets/Rest3.jpg'
+import { RequestContext, Text } from '@botonic/react'
 
 export default class extends React.Component {
   static contextType = RequestContext
+  static async botonicInit({ input, session, lastRoutePath }) {
+    const getNotUndefinedCustomEntities = (entities, tag) =>
+      entities.tags.length
+        ? entities.tags.filter(e => e.tags.includes(tag)).map(res => res.value)
+        : undefined
+
+    let namedEntitiesPlace = input.entities.places.length
+      ? input.entities.places
+      : undefined
+
+    let customEntitiesRestaurants = getNotUndefinedCustomEntities(
+      input.entities,
+      'Restaurant'
+    )
+    return { namedEntitiesPlace, customEntitiesRestaurants }
+  }
 
   render() {
-    return (
-      <>
-        <Text>For sure! These are some of the best places I've found!</Text>
-        <Carousel>
-          <Element>
-            <Pic src={Rest1} />
-            <Title>The Salty Yardt</Title>
-            <Subtitle>Who said that salads were boring?</Subtitle>
-            <Button payload='selected_restaurant-The Salty Yardt'>
-              This one!
-            </Button>
-          </Element>
-          <Element>
-            <Pic src={Rest2} />
-            <Title>The Chocolate Leaf</Title>
-            <Subtitle>The coffee of your life with the best dishes</Subtitle>
-            <Button payload='selected_restaurant-The Chocolate Leaf'>
-              This one!
-            </Button>
-          </Element>
-          <Element>
-            <Pic src={Rest3} />
-            <Title>The Vineyard</Title>
-            <Subtitle>The best dishes and wines in the earth!</Subtitle>
-            <Button payload='selected_restaurant-The Vineyard'>
-              This one!
-            </Button>
-          </Element>
-        </Carousel>
-      </>
-    )
+    if (
+      this.props.customEntitiesRestaurants &&
+      this.props.customEntitiesRestaurants.length
+    ) {
+      return (
+        <>
+          <Text>
+            For sure! So you want to make a reservation at{' '}
+            {this.props.customEntitiesRestaurants}
+          </Text>
+          <Text>Now you can ask me how to reach it!</Text>
+        </>
+      )
+    } else if (
+      this.props.namedEntitiesPlace &&
+      this.props.namedEntitiesPlace.length
+    ) {
+      return (
+        <>
+          <Text>
+            Okay! So you want to make a reservation close to{' '}
+            {this.props.namedEntitiesPlace[0]}
+          </Text>
+          <Text>Now you can ask me how to reach it!</Text>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Text>Sorry, I have not found any restaurant!</Text>
+          <Text>
+            You can try to include some of these restaurants in your query:
+            "Eggy's", "Delicatessen", "Joan's", "Seven-Hills"
+          </Text>
+          <Text>
+            Additionally, you can ask me for a restaurant near "Times Square"
+          </Text>
+        </>
+      )
+    }
   }
 }
