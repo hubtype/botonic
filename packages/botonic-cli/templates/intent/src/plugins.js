259fd0e22afed27415b6cfb688afd54909465a59
Marc Rabat <marcrabat@gmail.com>
2019-10-03 23:13:31 +0200

    fix(cli): update intent template to work with dialogflow-V2 for release 0.9.0

--- packages/botonic-cli/templates/intent/src/plugins.js
+++ packages/botonic-cli/templates/intent/src/plugins.js
@@ -1,1 +1,7 @@
-export const plugins = []
+export const plugins = [
+  {
+    id: 'dialogflow',
+    resolve: require('@botonic/plugin-dialogflow'),
+    options: 'YOUR_DIALOGFLOW_V2_GENERATED_JSON_KEY'
+  }
+]
