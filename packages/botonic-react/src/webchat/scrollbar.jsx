47954d61825651b72455bacc8f495df8bab9226b
Marc Rabat <marcrabat@gmail.com>
2019-10-17 17:19:30 +0200

    refactor(react): scrollbar

--- packages/botonic-react/src/webchat/scrollbar.jsx
+++ packages/botonic-react/src/webchat/scrollbar.jsx
@@ -4,47 +4,45 @@ import 'simplebar/dist/simplebar.css'
 
 export const StyledSimpleBar = styled(SimpleBar)`
   & .simplebar-scrollbar::before {
-    background-color: ${props =>
-      props.scrollbar && props.scrollbar.thumb && props.scrollbar.thumb.color
-        ? props.scrollbar.thumb.color
+    background-color: ${({ scrollbar }) =>
+      scrollbar && scrollbar.thumb && scrollbar.thumb.color
+        ? scrollbar.thumb.color
         : '#818181'};
-    background-image: ${props =>
-      props.scrollbar && props.scrollbar.thumb && props.scrollbar.thumb.bgcolor
-        ? props.scrollbar.thumb.bgcolor
+    background-image: ${({ scrollbar }) =>
+      scrollbar && scrollbar.thumb && scrollbar.thumb.bgcolor
+        ? scrollbar.thumb.bgcolor
         : '#818181'};
-    border-radius: ${props =>
-      props.scrollbar && props.scrollbar.thumb && props.scrollbar.thumb.border
-        ? props.scrollbar.thumb.border
+    border-radius: ${({ scrollbar }) =>
+      scrollbar && scrollbar.thumb && scrollbar.thumb.border
+        ? scrollbar.thumb.border
         : '20px'};
   }
   & .simplebar-track .simplebar-scrollbar.simplebar-visible::before {
-    opacity: ${props =>
-      props.scrollbar && props.scrollbar.disabled
+    opacity: ${({ scrollbar }) =>
+      scrollbar && scrollbar.disabled
         ? '0'
-        : props.scrollbar &&
-          props.scrollbar.thumb &&
-          props.scrollbar.thumb.opacity
-        ? props.scrollbar.thumb.opacity
+        : scrollbar && scrollbar.thumb && scrollbar.thumb.opacity
+        ? scrollbar.thumb.opacity
         : '0.75'};
   }
   & .simplebar-track {
-    background-color: ${props =>
-      props.scrollbar &&
-      props.scrollbar.track &&
-      props.scrollbar.track.color &&
-      !(props.scrollbar && props.scrollbar.disabled)
-        ? props.scrollbar.track.color
+    background-color: ${({ scrollbar }) =>
+      scrollbar &&
+      scrollbar.track &&
+      scrollbar.track.color &&
+      !(scrollbar && scrollbar.disabled)
+        ? scrollbar.track.color
         : 'transparent'};
-    background-image: ${props =>
-      props.scrollbar &&
-      props.scrollbar.track &&
-      props.scrollbar.track.bgcolor &&
-      !(props.scrollbar && props.scrollbar.disabled)
-        ? props.scrollbar.track.bgcolor
+    background-image: ${({ scrollbar }) =>
+      scrollbar &&
+      scrollbar.track &&
+      scrollbar.track.bgcolor &&
+      !(scrollbar && scrollbar.disabled)
+        ? scrollbar.track.bgcolor
         : 'transparent'};
-    border-radius: ${props =>
-      props.scrollbar && props.scrollbar.track && props.scrollbar.track.border
-        ? props.scrollbar.track.border
+    border-radius: ${({ scrollbar }) =>
+      scrollbar && scrollbar.track && scrollbar.track.border
+        ? scrollbar.track.border
         : '20px'};
   }
 `
