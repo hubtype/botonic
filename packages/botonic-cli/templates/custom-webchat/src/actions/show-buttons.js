656270ff5c8106ea053d3f85cd927afe0becfd57
Marc Rabat <marcrabat@gmail.com>
2019-10-21 17:45:02 +0200

    chore(cli): update custom-webchat template with missing options: scrollbar, attachments and webviewStyle

--- packages/botonic-cli/templates/custom-webchat/src/actions/show-buttons.js
+++ packages/botonic-cli/templates/custom-webchat/src/actions/show-buttons.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, Button, RequestContext } from '@botonic/react'
+import { MyWebview } from '../webviews/my-webview'
 
 export default class extends React.Component {
   constructor(props) {
@@ -13,8 +14,9 @@ export default class extends React.Component {
         <Text>
           What about these buttons?
           <Button url={'https://docs.botonic.io/main-concepts/webviews'}>
-            You can use me with webviews
+            You can use me to open links
           </Button>
+          <Button webview={MyWebview}>To open a webview</Button>
           <Button payload={'customized-payload'}>Or to send a payload</Button>
         </Text>
       </>
