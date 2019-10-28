20ed248b8a754ae49c0b4d9dad7e4692af30caf5
Dani Pinyol <dpinol@gmail.com>
2019-10-28 10:08:25 +0100

    feat(contentful): new SearchRegression class to evaluate keyword matching

---
+++ packages/botonic-plugin-contentful/src/tools/regression-tools.ts
@@ -0,0 +1,42 @@
+import { Locale, MatchType } from '../nlp';
+import BotonicPluginContentful from '../plugin';
+import { ContentCallback } from '../cms';
+
+export type SearchEvaluator = (
+  /** Starting with 0. Undefined if not found */
+  matchPosition: number | undefined,
+  numResults: number
+) => number;
+
+export class GroundTruth {
+  constructor(readonly utterance: string, readonly contentId: string) {}
+}
+
+export class SearchRegression {
+  constructor(
+    readonly plugin: BotonicPluginContentful,
+    readonly evaluator: SearchEvaluator
+  ) {}
+
+  async run(
+    matchType: MatchType,
+    groundTruths: Iterable<GroundTruth>,
+    locale: Locale
+  ): Promise<number> {
+    let sumEvals = 0;
+    let count = 0;
+    for (const gt of groundTruths) {
+      count++;
+      const res = await this.plugin.search.searchByKeywords(
+        gt.utterance,
+        matchType,
+        { locale }
+      );
+      const pos = res.findIndex(
+        res => (res.callback as ContentCallback).id == gt.contentId
+      );
+      sumEvals += this.evaluator(pos >= 0 ? pos : undefined, res.length);
+    }
+    return sumEvals / count;
+  }
+}
