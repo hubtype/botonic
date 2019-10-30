2b6f1396da062fc03b05e01f9d4688349e0a9306
Dani Pinyol <dpinol@gmail.com>
2019-10-30 15:00:51 +0100

    feature(contentful): add Polish to Normalizer

--- packages/botonic-plugin-contentful/src/nlp/stemmer.ts
+++ packages/botonic-plugin-contentful/src/nlp/stemmer.ts
@@ -5,6 +5,7 @@ import EnglishStemmer from 'node-nlp/lib/nlp/stemmers/english-stemmer'
 import SpanishStemmer from 'node-nlp/lib/nlp/stemmers/spanish-stemmer'
 import PortugueseStemmer from 'node-nlp/lib/nlp/stemmers/portuguese-stemmer'
 import { tokenizerPerLocale } from './tokens'
+import { PolishStemmer } from './stemmers/polish-stemmer'
 
 // see https://github.com/axa-group/nlp.js/blob/HEAD/docs/language-support.md
 // and https://stackoverflow.com/a/11210358/145289
@@ -15,6 +16,7 @@ export const stemmers: { [key: string]: BaseStemmer } = {
   ca: new CatalanStemmer(tokenizerPerLocale('ca')),
   en: new EnglishStemmer(tokenizerPerLocale('en')),
   es: new SpanishStemmer(tokenizerPerLocale('es')),
+  pl: new PolishStemmer(tokenizerPerLocale('pl')),
   pt: new PortugueseStemmer(tokenizerPerLocale('pt')),
   //node-nlp does not support polish
 }
