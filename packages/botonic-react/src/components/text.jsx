59192f8b4746f9df57f0303b0b9824b22466fb74
Eric Marcos <ericmarcos.p@gmail.com>
2019-11-15 09:40:01 +0100

    fix(react): check Text children is array using Array.isArray

--- packages/botonic-react/src/components/text.jsx
+++ packages/botonic-react/src/components/text.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Message } from './message'
 
 const serialize = textProps => {
-  let text = textProps.children
+  let text = String(textProps.children)
   /* As text message can have multiple children ( a text with buttons or quickreplies )
     we only want to store the text, we don't want to store the buttons or quickreplies.
     If we have an array of string, we want to join all the string as a single string, for
     future render actions
   */
-  if (typeof textProps.children == 'object') {
+  if (Array.isArray(textProps.children)) {
     text = textProps.children.join(' ')
     if (typeof textProps.children[1] != 'string') text = textProps.children[0]
   }
