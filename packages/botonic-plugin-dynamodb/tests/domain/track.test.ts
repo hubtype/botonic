c21f8f9eda6a7c5e60760dd1e1314e038a20ec03
Dani Pinyol <github@dpinol.com>
2019-12-05 13:55:45 +0100

    test(dynamo): fix test

--- packages/botonic-plugin-dynamodb/tests/domain/track.test.ts
+++ packages/botonic-plugin-dynamodb/tests/domain/track.test.ts
@@ -2,6 +2,7 @@ import { instance, mock, when } from 'ts-mockito'
 import { DynamoTrackStorage } from '../../src/infrastructure/dynamo'
 import { ErrorReportingTrackStorage, Track } from '../../src/domain/track'
 import DoneCallback = jest.DoneCallback
+import { TrackException } from '../../src/domain/exceptions'
 
 // next line avoids refactor as per https://github.com/jest-community/eslint-plugin-jest/blob/master/docs/rules/no-test-callback.md
 // eslint-disable-next-line jest/no-test-callback
@@ -21,7 +22,9 @@ test('TEST: ErrorReportingCMS write rejected', async (done: DoneCallback) => {
       done.fail('should have thrown')
     })
     .catch(error2 => {
-      expect(error2).toBe(error)
+      expect(error2).toBeInstanceOf(TrackException)
+      const trackException = error2 as TrackException
+      expect(trackException.reason).toBe(error)
       done()
     })
 })
