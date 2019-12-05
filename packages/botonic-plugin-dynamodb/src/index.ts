5519939c514f4328aead320985ea1350404b6be6
Dani Pinyol <github@dpinol.com>
2019-12-05 11:25:26 +0100

    feat(dynamo): added configuration of timeout

--- packages/botonic-plugin-dynamodb/src/index.ts
+++ packages/botonic-plugin-dynamodb/src/index.ts
@@ -7,25 +7,39 @@ import {
   TrackStorage,
   UserEvent,
 } from './domain'
-import { DynamoTrackStorage } from './infrastructure/dynamo'
+import { DynamoTrackStorage, Env } from './infrastructure/dynamo'
 import time from './domain/time'
 
 export * from './domain/dummy'
 export * from './infrastructure'
 
+export interface TrackOptions {
+  storage: Storage
+}
+
+export interface DynamoDbOptions extends DynamoDB.ClientConfiguration {
+  env: Env
+  /**
+   * If not specified, failing operations will be configured according to
+   * DynamoDB.ClientConfiguration's httpOptions and retry fields. By default, it
+   * will automatically retry with a backoff algorithm.
+   * If specified, operations will fail if the first try takes longer than this
+   * amount of ms
+   */
+  timeout?: number
+}
+
 export default class BotonicPluginDynamoDB {
   readonly storage: TrackStorage
 
-  constructor(options: any) {
-    if (options.storage) {
-      this.storage = options.storage
+  constructor(opt: TrackOptions | DynamoDbOptions) {
+    const optionsAny = opt as any
+    if (optionsAny.storage) {
+      this.storage = optionsAny.storage
     } else {
-      const conf: DynamoDB.ClientConfiguration = {
-        accessKeyId: options['accessKeyId'],
-        secretAccessKey: options['secretAccessKey'],
-        region: options['region'],
-      }
-      this.storage = new DynamoTrackStorage(options['env'], conf)
+      const dynamoConf = opt as DynamoDbOptions
+      this.applyTimeout(dynamoConf)
+      this.storage = new DynamoTrackStorage(dynamoConf.env, dynamoConf)
     }
     this.storage = new ErrorReportingTrackStorage(this.storage)
   }
@@ -35,7 +49,7 @@ export default class BotonicPluginDynamoDB {
     user: string,
     event: string,
     args: any = undefined
-  ): Promise<undefined> {
+  ): Promise<void> {
     const userEvent = new UserEvent(user, event, args)
     const track = new Track(botId, time.now(), [userEvent])
     return this.storage.write(track)
@@ -49,5 +63,17 @@ export default class BotonicPluginDynamoDB {
   // @ts-ignore
   post({ input, session, lastRoutePath, response }) {
     return { input, session, lastRoutePath, response }
+  }
+
+  private applyTimeout(pluginDynamoOpts: DynamoDbOptions): void {
+    if (!pluginDynamoOpts.timeout) {
+      return
+    }
+    pluginDynamoOpts.httpOptions = {
+      timeout: pluginDynamoOpts.timeout,
+      connectTimeout: pluginDynamoOpts.timeout,
+    }
+    pluginDynamoOpts.retryDelayOptions = { base: pluginDynamoOpts.timeout }
+    pluginDynamoOpts.maxRetries = 1
   }
 }
--- packages/botonic-plugin-dynamodb/tests/helpers/dynamo.ts
+++ packages/botonic-plugin-dynamodb/tests/helpers/dynamo.ts
@@ -1,23 +1,21 @@
-import { ServiceConfigurationOptions } from 'aws-sdk/lib/service'
 import * as fs from 'fs'
 import * as os from 'os'
+import { DynamoDbOptions } from '../../src'
 
 /**
  * At least Dynamo gets the keys from ~/.aws/credentials, but it does not get the region from ~/.aws/config
+ * https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
  * @param conf
  */
-export function testConfig(
-  conf?: ServiceConfigurationOptions
-): ServiceConfigurationOptions {
+export function testConfig(conf: DynamoDbOptions): DynamoDbOptions {
   if (!conf || !conf.secretAccessKey || !conf.accessKeyId) {
     const credentials = os.homedir() + '/.aws/credentials'
     if (!fs.existsSync(credentials)) {
-      console.warn(
+      console.error(
         `Credentials file not found at ${credentials}. Are you authenticating some other way?`
       )
     }
   }
-  conf = conf || {}
   conf.region = conf.region || 'eu-west-1'
   return conf
 }
