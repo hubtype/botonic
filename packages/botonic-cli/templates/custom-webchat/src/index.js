a02d8f33b0e2b259d9335fa200e2a7e54f488420
Marc Rabat <marcrabat@gmail.com>
2019-12-19 12:20:00 +0100

    chore(templates): add default config for every template

--- packages/botonic-cli/templates/custom-webchat/src/index.js
+++ packages/botonic-cli/templates/custom-webchat/src/index.js
@@ -3,4 +3,4 @@ export { locales } from './locales'
 export { plugins } from './plugins'
 export { webchat } from './webchat'
 export { webviews } from './webviews'
-export const config = { defaultDelay: 1, defaultTyping: 1 }
+export const config = { defaultDelay: 0.4, defaultTyping: 0.6 }
