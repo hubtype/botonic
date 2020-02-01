2d684088a76170bcaedafbe5d8c0a9ff32f38478
Marc Rabat <marcrabat@gmail.com>
2020-02-01 11:47:43 +0100

    feat(core): add providers in a constants file

---
+++ packages/botonic-core/src/constants.js
@@ -0,0 +1,16 @@
+export const Providers = Object.freeze({
+  Messaging: {
+    TELEGRAM: 'telegram',
+    WECHAT: 'wechat',
+    FACEBOOK: 'facebook',
+    INTERCOM: 'intercom',
+    GENERIC: 'generic',
+    TWITTER: 'twitter',
+    WHATSAPP: 'whatsapp',
+    SMOOCH: 'smooch',
+    SMOOCH_WEB: 'smooch_web',
+    WHATSAPPNEW: 'whatsappnew',
+    IMBEE: 'imbee',
+    WEBCHAT: 'webchat',
+  },
+})
