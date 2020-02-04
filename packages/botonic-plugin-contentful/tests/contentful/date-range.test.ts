59ab8bcd376fe9f9b4fbc1f2a7283ecb2027b80e
Marcos Calafat <marccalafat@gmail.com>
2020-02-04 10:57:52 +0100

    fix/ fixed timezone test error

--- packages/botonic-plugin-contentful/tests/contentful/date-range.test.ts
+++ packages/botonic-plugin-contentful/tests/contentful/date-range.test.ts
@@ -2,10 +2,28 @@ import { testContentful } from './contentful.helper'
 
 const TEST_DATE_RANGE_HUBTYPE_ID = '46taK0TceR2OZ8issCMhB7'
 
+function europeDate(
+  year: number,
+  month: number,
+  day: number,
+  hour: number = 0,
+  minute: number = 0
+): Date {
+  let date = new Date(Date.UTC(year, month, day, hour, minute, 0));
+
+  let utcDate = new Date(date.toLocaleString('en-US', { timeZone: "UTC" }));
+  let tzDate = new Date(date.toLocaleString('en-US', { timeZone: "Europe/Madrid" }));
+  let offset = utcDate.getTime() - tzDate.getTime();
+
+  date.setTime( date.getTime() + offset );
+
+  return date;
+};
+
 test('TEST: contentful dateRange', async () => {
   const dateRange = await testContentful().dateRange(TEST_DATE_RANGE_HUBTYPE_ID)
 
   expect(dateRange.common.name).toEqual('Test DateRange name')
-  expect(dateRange.dateRange.from).toEqual(new Date(2019, 4, 26, 20, 30))
-  expect(dateRange.dateRange.to).toEqual(new Date(2019, 8, 1))
+  expect(dateRange.dateRange.from).toEqual(europeDate(2019, 4, 26, 20, 30))
+  expect(dateRange.dateRange.to).toEqual(europeDate(2019, 8, 1))
 })
