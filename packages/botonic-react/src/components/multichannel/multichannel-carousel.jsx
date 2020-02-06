6c116f2b8ed98c9f1893674b846cb8fb72198dd9
Marc Rabat <marcrabat@gmail.com>
2020-02-06 16:56:19 +0100

    wip: add buttons to carousels

--- packages/botonic-react/src/components/multichannel/multichannel-carousel.jsx
+++ packages/botonic-react/src/components/multichannel/multichannel-carousel.jsx
@@ -6,6 +6,22 @@ import { MultichannelText } from './multichannel-text'
 import { MultichannelButton } from './multichannel-button'
 import { Providers } from '@botonic/core'
 
+const carouselToCaption = (index, title, subtitle, imageSrc, buttonProps) => {
+  let caption = ''
+  let header = `${title ? `*${title}*` : ''}`
+  header += `${subtitle ? ` - _${subtitle}_` : ''}`
+  let buttons = ''
+  if (buttonProps.url) {
+    buttons += ` - ${buttonProps.children}: ${buttonProps.url}`
+    caption = `${header ? `${header}\n` : ''}${buttons}`
+  }
+  if (buttonProps.payload || buttonProps.path) {
+    buttons += `${index}. `
+    caption = `${buttons}${buttonProps.children}`
+  }
+  return caption
+}
+
 export class MultichannelCarousel extends React.Component {
   static contextType = RequestContext
   constructor(props) {
@@ -24,7 +40,7 @@ export class MultichannelCarousel extends React.Component {
           let imageSrc = undefined
           let title = undefined
           let subtitle = undefined
-          let button = {}
+          let buttonProps = {}
 
           for (let e of element) {
             if (e.type.name == 'Pic') {
@@ -37,17 +53,18 @@ export class MultichannelCarousel extends React.Component {
               subtitle = e.props.children
             }
             if (e.type.name == 'Button') {
-              button.text = e.props.children
-              button.url = e.props.url
+              buttonProps = e.props
             }
           }
           if (this.enableURL) {
+            let header = `${title ? `*${title}*` : ''}`
+            header += `${subtitle ? ` - _${subtitle}_` : ''}`
             return (
               <React.Fragment key={i}>
-                <MultichannelText>
-                  {`*${title}* - _${subtitle}_`}
-                  <MultichannelButton url={button.url}>
-                    {`${button.text}`}
+                <MultichannelText index={i}>
+                  {header}
+                  <MultichannelButton {...buttonProps}>
+                    {`${buttonProps.children}`}
                   </MultichannelButton>
                 </MultichannelText>
                 {this.props.enablePics && <Image src={imageSrc}></Image>}
@@ -58,7 +75,13 @@ export class MultichannelCarousel extends React.Component {
               <React.Fragment key={i}>
                 <Image
                   src={imageSrc}
-                  caption={`*${title}* - _${subtitle}_\n${button.text}: ${button.url}`}
+                  caption={carouselToCaption(
+                    i + 1,
+                    title,
+                    subtitle,
+                    imageSrc,
+                    buttonProps
+                  )}
                 ></Image>
               </React.Fragment>
             )
