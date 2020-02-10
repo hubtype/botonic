004414ed169e887de6e4131a9d7d3d6568f2d31c
Dani Pinyol <github@dpinol.com>
2020-02-10 12:58:31 +0100

    chore(react): improve TS typing and organize index imports

--- packages/botonic-react/src/components/multichannel/multichannel-utils.js
+++ packages/botonic-react/src/components/multichannel/multichannel-utils.js
@@ -1,7 +1,7 @@
 export function getButtons(node, buttonFilter) {
   const buttons = []
   buttonFilter = buttonFilter || (() => true)
-  let isFilteredButton = (but) => but.type.name == 'Button' && buttonFilter
+  let isFilteredButton = but => but.type.name == 'Button' && buttonFilter
 
   for (let n of node) {
     if (n instanceof Array) {
