57bfebf8d0d59ea4a16e76e58e97fbd6b431f61f
Dani Pinyol <github@dpinol.com>
2020-02-13 05:58:40 +0100

    chore: move lint dependencies and config to top folder
    using typescript parser as the default so that it can also
    validate the .d.ts files

--- packages/.eslintrc.js
+++ packages/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    // "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
@@ -26,7 +26,17 @@ module.exports = {
     "node/no-unsupported-features/es-syntax": "off", //babel will take care of ES compatibility
 
     // compatibility with botonic style
-    "no-null/no-null": "off"
+    "no-null/no-null": "off",
+
+    "prefer-const" : ["error", {"destructuring": "all"}],
+//    "@typescript-eslint/no-use-before-define": ["error", { "variables": true, "functions": false, "classes": false }],
+    "@typescript-eslint/no-use-before-define": "warn", //not an error due to our heavy use of const functions
+    "@typescript-eslint/camelcase": "warn", // TODO add comments to allow backend ids but forbid elsewhere
+    "@typescript-eslint/no-var-requires": "off", // TODO only enable for TS code
+    "@typescript-eslint/no-empty-function": "warn",
+
+    // for d.ts
+    "@typescript-eslint/explicit-member-accessibility": "off",
   },
   env: {
     jest: true
