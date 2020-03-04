5035189071293a38878b40acd6f60b27f1e4ecf5
Dani Pinyol <github@dpinol.com>
2020-03-04 21:11:07 +0100

    chore: content builders to return this
    to have covariant return

--- packages/botonic-plugin-contentful/src/cms/factories.ts
+++ packages/botonic-plugin-contentful/src/cms/factories.ts
@@ -10,12 +10,12 @@ import {
 abstract class ModelBuilder {
   protected constructor(public id: string, public name: string) {}
 
-  withId(id: string): ModelBuilder {
+  withId(id: string): this {
     this.id = id
     return this
   }
 
-  withName(name: string): ModelBuilder {
+  withName(name: string): this {
     this.name = name
     return this
   }
@@ -39,32 +39,32 @@ export class TextBuilder extends ModelBuilder {
     super(id, name)
   }
 
-  withText(text: string): TextBuilder {
+  withText(text: string): this {
     this.text = text
     return this
   }
 
-  withButtons(buttons: Button[]): TextBuilder {
+  withButtons(buttons: Button[]): this {
     this.buttons = buttons
     return this
   }
 
-  withShortText(shortText: string): TextBuilder {
+  withShortText(shortText: string): this {
     this.shortText = shortText
     return this
   }
 
-  withFollowUp(followUp: FollowUp): TextBuilder {
+  withFollowUp(followUp: FollowUp): this {
     this.followUp = followUp
     return this
   }
 
-  withKeywords(kw: string[]): TextBuilder {
+  withKeywords(kw: string[]): this {
     this.keywords = kw
     return this
   }
 
-  withButtonStyle(style: ButtonStyle): TextBuilder {
+  withButtonStyle(style: ButtonStyle): this {
     this.buttonsStyle = style
     return this
   }
