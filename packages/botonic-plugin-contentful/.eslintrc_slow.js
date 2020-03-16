4feecef3201c1df3dce97932b3639b7c276bea72
Dani Pinyol <github@dpinol.com>
2020-03-16 23:01:55 +0100

    chore(contentful/dynamo): eslint to also affect .eslintrc*js
    tsconfig.eslint.json required to avoid error
    Parsing error: "parserOptions.project" has been set for @typescript-eslint/parser.
    The file does not match your project config: .eslintrc.js
    The file does not match your project config: .eslintrc.json
    
    see https://github.com/typescript-eslint/typescript-eslint/issues/967#issuecomment-531817014

--- packages/botonic-plugin-contentful/.eslintrc_slow.js
+++ packages/botonic-plugin-contentful/.eslintrc_slow.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const base = require('./.eslintrc');
-base.parserOptions.project = "./tests/tsconfig.json";
+base.parserOptions.project = "./tsconfig.eslint.json";
 
 base.rules["@typescript-eslint/no-floating-promises"] =  "error"; // see https://github.com/xjamundx/eslint-plugin-promise/issues/151
 base.rules["@typescript-eslint/no-misused-promises"] = "error";
