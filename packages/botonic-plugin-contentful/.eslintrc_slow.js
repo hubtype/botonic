7858019304be0765ead53bf27925d1742b85578d
Dani Pinyol <github@dpinol.com>
2020-03-16 23:12:46 +0100

    chore(contentful/dynamo): apply eslint to .eslintrc*.js

--- packages/botonic-plugin-contentful/.eslintrc_slow.js
+++ packages/botonic-plugin-contentful/.eslintrc_slow.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const base = require('./.eslintrc');
-base.parserOptions.project = "./tsconfig.eslint.json";
+const base = require('./.eslintrc')
+base.parserOptions.project = './tsconfig.eslint.json'
 
-base.rules["@typescript-eslint/no-floating-promises"] =  "error"; // see https://github.com/xjamundx/eslint-plugin-promise/issues/151
-base.rules["@typescript-eslint/no-misused-promises"] = "error";
-base.rules["@typescript-eslint/require-await"] = "error";
+base.rules['@typescript-eslint/no-floating-promises'] = 'error' // see https://github.com/xjamundx/eslint-plugin-promise/issues/151
+base.rules['@typescript-eslint/no-misused-promises'] = 'error'
+base.rules['@typescript-eslint/require-await'] = 'error'
 
-module.exports = base;
+module.exports = base
