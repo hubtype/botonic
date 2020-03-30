0642ea053e6479f4946434ad7e464d776133d15e
Dani Pinyol <github@dpinol.com>
2020-03-30 16:23:34 +0200

    chore: pre-commit was not aborting when lint failed

--- scripts/qa/lint-package.sh
+++ scripts/qa/lint-package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd $1
+cd "$1" || exit
 npm run lint
+EXIT_CODE=$?
 # when lint can fix all errors, npm will return 0
 # In this case, we want to see which files were changed
 if [[ $? == 0 ]]; then
@@ -8,4 +9,6 @@ if [[ $? == 0 ]]; then
   echo "If lint fixed some files, run the following to verify the changes and commit again:"
   echo " git diff"
   echo " git commit -a"
+else
+  exit $EXIT_CODE
 fi
