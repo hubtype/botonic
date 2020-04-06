6ff9a4f075d02587804cd708938d68489743a369
Dani Pinyol <github@dpinol.com>
2020-04-06 17:05:12 +0200

    chore(contentful/dynamo): use unified eslintrc.js

--- packages/botonic-plugin-dynamodb/.eslintrc_slow.js
+++ packages/botonic-plugin-dynamodb/.eslintrc_slow.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const base = require('./.eslintrc')
+const base = require('../.eslintrc')
 base.parserOptions.project = './tsconfig.eslint.json'
 
 base.rules['@typescript-eslint/no-floating-promises'] = 'error' // see https://github.com/xjamundx/eslint-plugin-promise/issues/151
