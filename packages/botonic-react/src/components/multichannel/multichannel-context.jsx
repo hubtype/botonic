86047c5bdced84200dcedd8af195b358d1bbe91c
Dani Pinyol <github@dpinol.com>
2020-04-15 11:28:21 +0200

    feat(react): customize multichannel visualization

--- packages/botonic-react/src/components/multichannel/multichannel-context.jsx
+++ packages/botonic-react/src/components/multichannel/multichannel-context.jsx
@@ -2,4 +2,12 @@ import React from 'react'
 
 export const MultichannelContext = React.createContext({
   currentIndex: 1,
+  boldIndex: true,
+  carousel: {
+    indexMode: undefined,
+  },
+  text: {
+    indexMode: 'letter',
+  },
+  indexSeparator: '.',
 })
