bc249f9facbac748198bc26bf4c1de3f740423a9
Dani Pinyol <github@dpinol.com>
2020-04-15 22:36:13 +0200

    chore(contentful): backup of changed contentful contents

--- packages/botonic-plugin-contentful/bin/export_contentful_full.sh
+++ packages/botonic-plugin-contentful/bin/export_contentful_full.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 BIN_DIR=${0:a:h}
 
-contentful space export --download-assets \
+"$BIN_DIR"/../node_modules/.bin/contentful space export --download-assets \
   --space-id=$CONTENTFUL_TEST_SPACE_ID \
   --management-token=$CONTENTFUL_TEST_MANAGE_TOKEN
