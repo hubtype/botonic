756a5df374d957645ba58ab9df67c49817db0b4e
Marc Rabat <marcrabat@gmail.com>
2020-05-18 15:34:12 +0200

    refactor(react): renaming improvements

--- packages/botonic-react/src/message-utils.js
+++ packages/botonic-react/src/message-utils.js
@@ -1,20 +1,21 @@
 import { INPUT } from '@botonic/core'
-import { MIME_WHITELIST } from './constants'
+import { MIME_WHITELIST, MAX_ALLOWED_SIZE_MB } from './constants'
 
-export const isType = (msgType, type) => msgType === type
-export const isText = msg => isType(msg.type, INPUT.TEXT)
-export const isPostback = msg => isPostback(msg.type, INPUT.POSTBACK)
-export const isAudio = msg => isType(msg.type, INPUT.AUDIO)
-export const isImage = msg => isType(msg.type, INPUT.IMAGE)
-export const isVideo = msg => isType(msg.type, INPUT.VIDEO)
-export const isDocument = msg => isType(msg.type, INPUT.DOCUMENT)
-export const isLocation = msg => isType(msg.type, INPUT.LOCATION)
-export const isContact = msg => isType(msg.type, INPUT.CONTACT)
-export const isCarousel = msg => isType(msg.type, INPUT.CAROUSEL)
-export const isCustom = msg => isType(msg.type, INPUT.CUSTOM)
-export const isButtonMessage = msg => isType(msg.type, INPUT.BUTTON_MESSAGE)
+export const isOfType = (msgType, type) => msgType === type
 
-export const MEDIA_TYPES = [
+export const isText = msg => isOfType(msg.type, INPUT.TEXT)
+export const isPostback = msg => isOfType(msg.type, INPUT.POSTBACK)
+export const isAudio = msg => isOfType(msg.type, INPUT.AUDIO)
+export const isImage = msg => isOfType(msg.type, INPUT.IMAGE)
+export const isVideo = msg => isOfType(msg.type, INPUT.VIDEO)
+export const isDocument = msg => isOfType(msg.type, INPUT.DOCUMENT)
+export const isLocation = msg => isOfType(msg.type, INPUT.LOCATION)
+export const isContact = msg => isOfType(msg.type, INPUT.CONTACT)
+export const isCarousel = msg => isOfType(msg.type, INPUT.CAROUSEL)
+export const isCustom = msg => isOfType(msg.type, INPUT.CUSTOM)
+export const isButtonMessage = msg => isOfType(msg.type, INPUT.BUTTON_MESSAGE)
+
+export const INPUT_MEDIA_TYPES = [
   INPUT.AUDIO,
   INPUT.IMAGE,
   INPUT.VIDEO,
@@ -22,11 +23,11 @@ export const MEDIA_TYPES = [
 ]
 
 export const isMedia = message =>
-  MEDIA_TYPES.some(type => isType(message.type, type))
+  INPUT_MEDIA_TYPES.some(type => isOfType(message.type, type))
 
 // TODO: Centralize handling attachments in class
 
-export const toBase64 = file =>
+export const readDataURL = file =>
   new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.readAsDataURL(file)
@@ -35,19 +36,18 @@ export const toBase64 = file =>
   })
 
 export const isAllowedSize = fileSize => {
-  const MAX_MB_SIZE_ALLOWED = 10
-  const maxAllowedBytesSize = MAX_MB_SIZE_ALLOWED * 1024 * 1024
+  const maxAllowedBytesSize = MAX_ALLOWED_SIZE_MB * 1024 * 1024
   if (fileSize > maxAllowedBytesSize) return false
   return true
 }
 
-export const getAttachmentType = fileType => {
+export const getMediaType = fileType => {
   return Object.entries(MIME_WHITELIST)
     .filter(([_, formatsForType]) => formatsForType.includes(fileType))
     .map(([type, _]) => type)[0]
 }
 
-export const getAcceptedFormats = () =>
-  Object.values(MIME_WHITELIST)
-    .map(acceptedFormatsForType => acceptedFormatsForType.join(','))
-    .join(',')
+export const getFullMimeWhitelist = () =>
+  Object.values(MIME_WHITELIST).map(acceptedFormatsForType =>
+    acceptedFormatsForType.join(',')
+  )
