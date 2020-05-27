c9a9bc2d28906d00b28810b7c8cd52ba059ae1a3
Marc Rabat <marcrabat@gmail.com>
2020-05-27 12:13:51 +0200

    chore: update uuid deps manually

--- packages/botonic-plugin-dialogflow/src/index.js
+++ packages/botonic-plugin-dialogflow/src/index.js
@@ -1,11 +1,11 @@
-import uuid from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import axios from 'axios'
 import { KJUR } from 'jsrsasign'
 
 export default class BotonicPluginDialogflow {
   constructor(creds) {
     this.projectID = creds.project_id
-    this.sessionID = uuid.v4()
+    this.sessionID = uuidv4()
     this.creds = creds
     this.token = null
   }
