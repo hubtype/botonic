06801d48582b109a5f88fb186f4f1166004e23aa
Marc Rabat <marcrabat@gmail.com>
2020-06-04 13:49:58 +0200

    feat(react): make getVisibility accessible from Botonic api

--- packages/botonic-react/src/webchat-app.jsx
+++ packages/botonic-react/src/webchat-app.jsx
@@ -133,6 +133,13 @@ export class WebchatApp {
     this.webchatRef.current.clearMessages()
   }
 
+  async getVisibility() {
+    return this.resolveWebchatVisibility({
+      appId: this.appId,
+      visibility: this.visibility,
+    })
+  }
+
   getLastMessageUpdate() {
     return this.webchatRef.current.getLastMessageUpdate()
   }
