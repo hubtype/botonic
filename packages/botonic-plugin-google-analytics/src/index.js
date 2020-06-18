201c82aea49dbb5f479aa1bf7cd4ba1c1b78916a
Adrià Sastre <adria.sastre@gmail.com>
2020-06-18 09:31:33 +0200

    refactor(plugin-google-analytics): remove async/await and refactor the track method

--- packages/botonic-plugin-google-analytics/src/index.js
+++ packages/botonic-plugin-google-analytics/src/index.js
@@ -17,7 +17,24 @@ export default class BotonicPluginGoogleAnalytics {
 
   async post({ input, session, lastRoutePath, response }) {}
 
-  async track(eventName, eventFields, callback) {
-    await this.analytics.track(eventName, eventFields, callback)
+  /**
+   * Track event to Google Analytics
+   * @param {Object} eventFields - Event fields to send (see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#event_fields)
+   * @param {string} eventFields.category - Google Analytics eventCategory field
+   * @param {string} eventFields.action - Google Analytics eventAction field
+   * @param {string} [eventFields.label] - Google Analytics eventLabel field
+   * @param {number} [eventFields.value] - Google Analytics eventValue field
+   * @param  {Function} [callback]  - Callback to fire after tracking completes
+   * @returns {Promise<void>}
+   */
+  track(eventFields, callback) {
+    if (!eventFields.action || !eventFields.category)
+      throw 'The eventFields object must contain the fields: action and category'
+
+    this.analytics.track(eventFields.action, eventFields)
+      .then(callback)
+      .catch(error => {
+        throw `Error while tracking to Google Analytics: ${error}`
+      })
   }
 }
