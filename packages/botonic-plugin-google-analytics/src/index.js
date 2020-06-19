6e6ab02c8bad17edbb057d39725f61256db4347a
Adrià Sastre <adria.sastre@gmail.com>
2020-06-19 10:04:26 +0200

    feat(plugin-google-analytics): add user identification while tracking and add default tracking

--- packages/botonic-plugin-google-analytics/src/index.js
+++ packages/botonic-plugin-google-analytics/src/index.js
@@ -2,12 +2,21 @@ import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
 
 export default class BotonicPluginGoogleAnalytics {
-  constructor({ botName, trackingId }) {
+  /**
+   * @param {Object} options - Options for the plugin
+   * @param {string} options.trackingId - Tracking ID for Google Analytics.
+   * @param {function({session: object}): string} [options.userId] - Method that returns a unique user ID as string.
+   * @param {function({session: object}): object} [options.userTraits] - Method that returns the user traits as object.
+   * @param {boolean} [options.trackManually] - If set to true, no default tracking will be done in post method.
+   */
+  constructor(options) {
+    this.userId = options.userId ?? this.defaultUserId
+    this.userTraits = options.userTraits ?? this.defaultUserTraits
+    this.trackManually = options.trackManually
     this.analytics = Analytics({
-      app: botName,
       plugins: [
         googleAnalytics({
-          trackingId: trackingId,
+          trackingId: options.trackingId,
         }),
       ],
     })
@@ -15,26 +24,42 @@ export default class BotonicPluginGoogleAnalytics {
 
   async pre({ input, session, lastRoutePath }) {}
 
-  async post({ input, session, lastRoutePath, response }) {}
+  async post({ input, session, lastRoutePath, response }) {
+    if (!this.trackManually) {
+      const eventFields = {
+        category: session.bot.name,
+        action: lastRoutePath,
+        label: input.data,
+      }
+      await this.track({ session, eventFields })
+    }
+  }
+
+  defaultUserId = ({ session }) => session.user.id
+
+  defaultUserTraits = ({ session }) => ({
+    userName: session.user.username,
+    channel: session.user.provider,
+    channelId: session.user.provider_id,
+  })
 
   /**
    * Track event to Google Analytics
+   * @param {Object} session - Bot's session
    * @param {Object} eventFields - Event fields to send (see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#event_fields)
    * @param {string} eventFields.category - Google Analytics eventCategory field
    * @param {string} eventFields.action - Google Analytics eventAction field
    * @param {string} [eventFields.label] - Google Analytics eventLabel field
    * @param {number} [eventFields.value] - Google Analytics eventValue field
-   * @param  {Function} [callback]  - Callback to fire after tracking completes
-   * @returns {Promise<void>}
+   * @returns {Promise}
    */
-  track(eventFields, callback) {
+  async track({ session, eventFields }) {
     if (!eventFields.action || !eventFields.category)
-      throw 'The eventFields object must contain the fields: action and category'
+      throw new Error(
+        'The eventFields object must contain the fields: action and category'
+      )
 
-    this.analytics.track(eventFields.action, eventFields)
-      .then(callback)
-      .catch(error => {
-        throw `Error while tracking to Google Analytics: ${error}`
-      })
+    await this.analytics.identify(this.userId({ session }), this.userTraits({ session }))
+    return this.analytics.track(eventFields.action, eventFields)
   }
 }
