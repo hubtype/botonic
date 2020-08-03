aa73027531997cbe7f12cdf5e0907d16e4d617e2
Dani Pinyol <github@dpinol.com>
2020-08-03 14:46:29 +0200

    feat(contentful): import-csv to publish if locale was empty
    configure readCsvForTranslators depending on value of 3 available modes:
    * DRY: no actual writing
    * NO_OVERWRITE: to publish a locale for the first time
    * OVERWRITE: modifies previous value, but leaves it in UNPUBLISHED state

--- packages/botonic-plugin-contentful/src/tools/l10n/import-csv-for-translators.ts
+++ packages/botonic-plugin-contentful/src/tools/l10n/import-csv-for-translators.ts
@@ -2,6 +2,7 @@ import { CsvImport, StringFieldImporter } from './csv-import'
 import { ManageContentful } from '../../contentful/manage'
 import { ManageContext } from '../../manage-cms'
 import { ContentfulOptions } from '../../plugin'
+import { isOfType } from '../../util/enums'
 
 async function readCsvForTranslators(
   contentfulOptions: ContentfulOptions,
@@ -14,16 +15,27 @@ async function readCsvForTranslators(
   await importer.import(fname)
 }
 
+export enum ImportType {
+  DRY = 'DRY', //parse files but do write to CM
+  NO_OVERWRITE = 'NO_OVERWRITE', // publishes the content, but fails if fields for this locale already have value
+  OVERWRITE = 'OVERWRITE', // modifies previous value, but leaves it in UNPUBLISHED state
+}
+
 const spaceId = process.argv[2]
 const environment = process.argv[3]
 const accessToken = process.argv[4]
 const locale = process.argv[5]
 const fileName = process.argv[6]
-const dryRun = Boolean(process.argv[7])
+const importType = String(process.argv[7])
+if (!isOfType(importType, ImportType)) {
+  throw Error(`${importType} is not a valid value`)
+}
 
-if (process.argv.length < 7 || process.argv[2] == '--help') {
+if (process.argv.length < 8 || process.argv[2] == '--help') {
   console.error(
-    `Usage: space_id environment access_token locale filename [dry_run]`
+    `Usage: space_id environment access_token locale filename [${Object.values(
+      ImportType
+    ).join('|')}]`
   )
   // eslint-disable-next-line no-process-exit
   process.exit(1)
@@ -42,14 +54,20 @@ async function main() {
       },
       {
         locale,
-        preview: true,
-        dryRun: dryRun,
+        preview: importType == ImportType.OVERWRITE,
+        dryRun: importType == ImportType.DRY,
+        allowOverwrites: importType == ImportType.OVERWRITE,
       },
       fileName
     )
     console.log('done')
   } catch (e) {
     console.error(e)
+  }
+  if (importType == ImportType.OVERWRITE) {
+    console.log(
+      "Remember that you'll need to publish the changed contents from contentful.com"
+    )
   }
 }
 
