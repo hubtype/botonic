3d9b8d92d4c2c25f4c523e735e380b0610b25dbd
Dani Pinyol <github@dpinol.com>
2020-08-08 12:54:36 +0200

    chore(contentful): fix relative path args in l10n scripts
    since the script was doing "cd", relative paths were not working

--- packages/botonic-plugin-contentful/bin/l10n/import-from-translators.sh
+++ packages/botonic-plugin-contentful/bin/l10n/import-from-translators.sh
@@ -1,19 +1,23 @@
 #!/bin/zsh
-BIN_DIR=${0:a:h}
-cd "$BIN_DIR"/../.. || exit
-
-if [[ $# -lt 8 ]]; then
-	../../node_modules/.bin/ts-node --files	src/tools/l10n/import-csv-for-translators.ts --help
-	exit 1
-fi
-
-
+set -x
 SPACE_ID=$1
 ENVIRONMENT=$2
 DELIVER_TOKEN=$3
 CONTENTFUL_MANAGEMENT_TOKEN=$4
 LOCALE=$5
 CSV_FILENAME=$6
+CSV_FILENAME="$( cd "$( dirname "$CSV_FILENAME" )" && pwd )/$(basename $CSV_FILENAME)"
+
+
+BIN_DIR=${0:a:h}
+cd "$BIN_DIR"/../.. || exit
+
+if [[ $# -lt 8 ]]; then
+	../../node_modules/.bin/ts-node --files	src/tools/l10n/import-csv-from-translators.ts --help
+	exit 1
+fi
+
+
 
 WRITE_MODE=$7
 #  DRY: parse files but do write to CM
--- packages/botonic-plugin-contentful/src/tools/l10n/locale-migrate.ts
+++ packages/botonic-plugin-contentful/src/tools/l10n/locale-migrate.ts
@@ -17,20 +17,16 @@ const toLocale = process.argv[5]
 const removeLocales = process.argv.length > 6 ? process.argv[6] : ''
 
 function main() {
-  try {
-    const spaceExport = SpaceExport.fromJsonFile(fromFile)
-    const migrator = new LocaleMigrator(fromLocale, toLocale)
-    const remover = new LocaleRemover(removeLocales.split(','), toLocale)
-    console.log('Removing locales', remover.removeLocs)
+  const spaceExport = SpaceExport.fromJsonFile(fromFile)
+  const migrator = new LocaleMigrator(fromLocale, toLocale)
+  const remover = new LocaleRemover(removeLocales.split(','), toLocale)
+  console.log('Removing locales', remover.removeLocs)
 
-    migrator.migrate(spaceExport)
-    remover.remove(spaceExport)
+  migrator.migrate(spaceExport)
+  remover.remove(spaceExport)
 
-    spaceExport.write(toFile)
-    console.log('done')
-  } catch (e) {
-    console.error(e)
-  }
+  spaceExport.write(toFile)
+  console.log('done')
 }
 
 main()
