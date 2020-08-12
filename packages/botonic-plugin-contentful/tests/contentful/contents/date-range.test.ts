f5da7266ba162ef13eed7266b1873dd8a470d671
Dani Pinyol <github@dpinol.com>
2020-08-12 21:32:24 +0200

    fix(contentful): schedule was miscalculating around midnight
    it was not taking the tz offset into account to calculate the weekday

--- packages/botonic-plugin-contentful/tests/contentful/contents/date-range.test.ts
+++ packages/botonic-plugin-contentful/tests/contentful/contents/date-range.test.ts
@@ -2,7 +2,7 @@ import { testContentful } from '../contentful.helper'
 
 const TEST_DATE_RANGE_HUBTYPE_ID = '46taK0TceR2OZ8issCMhB7'
 
-function europeDate(
+export function cetDate(
   year: number,
   month: number,
   day: number,
@@ -26,6 +26,6 @@ test('TEST: contentful dateRange', async () => {
   const dateRange = await testContentful().dateRange(TEST_DATE_RANGE_HUBTYPE_ID)
 
   expect(dateRange.common.name).toEqual('Test DateRange name')
-  expect(dateRange.dateRange.from).toEqual(europeDate(2019, 4, 26, 20, 30))
-  expect(dateRange.dateRange.to).toEqual(europeDate(2019, 8, 1))
+  expect(dateRange.dateRange.from).toEqual(cetDate(2019, 4, 26, 20, 30))
+  expect(dateRange.dateRange.to).toEqual(cetDate(2019, 8, 1))
 })
---
+++ packages/botonic-plugin-contentful/tests/time/timezones.test.ts
@@ -0,0 +1,14 @@
+import momentTz from 'moment-timezone'
+import { cetDate } from '../contentful/contents/date-range.test'
+import { offsetWithTimeZone } from '../../src/time/timezones'
+
+test('TEST offsetWithTimeZone', () => {
+  const date = cetDate(2020, 1, 1, 0)
+
+  // act
+  const offset1 = offsetWithTimeZone(momentTz.tz.zone('Europe/London')!, date)
+  const offset2 = offsetWithTimeZone(momentTz.tz.zone('Europe/Madrid')!, date)
+
+  // assert
+  expect(offset2 - offset1).toEqual(60 * 60 * 1000)
+})
