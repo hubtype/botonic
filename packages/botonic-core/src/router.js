c39a5924ab8a72f39544c3c77f3432d0d1402393
Aina Vendrell <aina@MacBook-de-Aina.local>
2020-08-14 12:55:01 +0200

    fix(core): check childRoutes in getRoute

--- packages/botonic-core/src/router.js
+++ packages/botonic-core/src/router.js
@@ -144,24 +144,34 @@ export class Router {
     return undefined
   }
 
-  getRoute(input, routes, session) {
+  getRoute(input, routes, session, previousValue) {
     const computedRoutes = isFunction(routes)
       ? routes({ input, session })
       : routes
     /* Find the route that matches the given input, if it match with some of the entries,
       return the whole Route of the entry with optional params captured if matcher was a regex */
     let params = {}
-    const route = computedRoutes.find(r =>
+    let childRoute = undefined
+    let route = computedRoutes.find(r =>
       Object.entries(r)
         .filter(([key, {}]) => key != 'action' && key != 'childRoutes')
         .some(([key, value]) => {
-          const match = this.matchRoute(r, key, value, input, session)
-          try {
-            params = match.groups
-          } catch (e) {}
+          let currentValue = value
+          if (previousValue) currentValue = previousValue.concat('/', value)
+          const match = this.matchRoute(r, key, currentValue, input, session)
+          if (!match && r.childRoutes && !childRoute) {
+            childRoute = this.getRoute(
+              input,
+              r.childRoutes,
+              session,
+              currentValue
+            )
+          }
+          if (match) params = match.groups
           return Boolean(match)
         })
     )
+    if (childRoute) route = childRoute.route
     if (route) {
       return { route, params }
     }
