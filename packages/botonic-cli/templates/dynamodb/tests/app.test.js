e79da950c6e7f5f704f817408457274df19edd64
Dani Pinyol <github@dpinol.com>
2020-09-16 17:35:09 +0200

    feat(cli): template of js bot using dynamo plugin

---
+++ packages/botonic-cli/templates/dynamodb/tests/app.test.js
@@ -0,0 +1,17 @@
+import { routes, plugins, locales, config } from '../src/'
+import {
+  BotonicInputTester,
+  BotonicOutputTester,
+  NodeApp,
+} from '@botonic/react'
+
+const app = new NodeApp({ routes, locales, plugins, ...config })
+
+const i = new BotonicInputTester(app)
+const o = new BotonicOutputTester(app)
+
+test('TEST: (404) NOT FOUND', async () => {
+  await expect(i.text('whatever')).resolves.toBe(
+    o.text("I don't understand you")
+  )
+})
