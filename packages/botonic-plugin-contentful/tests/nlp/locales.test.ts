a6531542bce58630a2b19e8d0333ea1bafc7a1c9
Dani Pinyol <github@dpinol.com>
2020-10-01 12:19:04 +0200

    feat(contentful): buildLocale function
    fix locale separator from _ to -

--- packages/botonic-plugin-contentful/tests/nlp/locales.test.ts
+++ packages/botonic-plugin-contentful/tests/nlp/locales.test.ts
@@ -1,10 +1,15 @@
-import { preprocess, rootLocale } from '../../src/nlp/locales'
+import { buildLocale, preprocess, rootLocale } from '../../src/nlp/locales'
 
 test('TEST: preprocess', () => {
-  expect(preprocess('es_ES', ' ÑÇáü òL·l ')).toEqual('ncau ol·l')
+  expect(preprocess('es-ES', ' ÑÇáü òL·l ')).toEqual('ncau ol·l')
 })
 
 test('TEST: rootLocale', () => {
-  expect(rootLocale('es_ES')).toEqual('es')
+  expect(rootLocale('es-ES')).toEqual('es')
   expect(rootLocale('en')).toEqual('en')
+})
+
+test('TEST: buildLocale', () => {
+  expect(buildLocale('ES', 'es')).toEqual('es-ES')
+  expect(buildLocale('EN', undefined)).toEqual('en')
 })
