edf167e1233d18db0a4cc9e4d628068206cbbbb3
Dani Pinyol <github@dpinol.com>
2020-10-06 18:58:08 +0200

    feat(contentful): contentful validation improvements
    check button target is available and type of delivered content
    add test for contentful element delivery

--- packages/botonic-plugin-contentful/tests/contentful/contents/carousel.test.ts
+++ packages/botonic-plugin-contentful/tests/contentful/contents/carousel.test.ts
@@ -6,19 +6,6 @@ import { SPANISH } from '../../../src'
 export const TEST_CAROUSEL_MAIN_ID = '2yR9f3stNAEqdamUr8VtfD'
 export const TEST_POST_MENU_CRSL = '7ATP377r6Nb8VqNtRKAS4J'
 
-function assertElementDudasPrevias(element: cms.Element) {
-  expect(element.title).toBe('Dudas previas a la compra')
-  expect(element.subtitle).toBe(
-    'Le ayudaré con sus compras en la tienda online.'
-  )
-  expectImgUrlIs(element.imgUrl, 'blue.jpg')
-  expect(element.buttons).toHaveLength(1)
-  expect(element.buttons[0].callback.payload).toBe(
-    'text$79aRfznNCyN2VGdGDQZBf3'
-  )
-  expect(element.buttons[0].text).toBe('Ver opciones')
-}
-
 test('TEST: contentful carousel', async () => {
   const sut = testContentful()
 
@@ -35,3 +22,29 @@ test('TEST: contentful carousel', async () => {
   expect(carousel.common.shortText).toEqual('Menú de Inicio')
   assertElementDudasPrevias(carousel.elements[0])
 })
+
+test('TEST: contentful element', async () => {
+  const sut = testContentful()
+
+  // act
+  const element = await sut.element(
+    '714AB6c5NZoHLku5NoYVPE',
+    testContext([{ locale: SPANISH }])
+  )
+
+  //assert
+  assertElementDudasPrevias(element)
+})
+
+function assertElementDudasPrevias(element: cms.Element) {
+  expect(element.title).toBe('Dudas previas a la compra')
+  expect(element.subtitle).toBe(
+    'Le ayudaré con sus compras en la tienda online.'
+  )
+  expectImgUrlIs(element.imgUrl, 'blue.jpg')
+  expect(element.buttons).toHaveLength(1)
+  expect(element.buttons[0].callback.payload).toBe(
+    'text$79aRfznNCyN2VGdGDQZBf3'
+  )
+  expect(element.buttons[0].text).toBe('Ver opciones')
+}
