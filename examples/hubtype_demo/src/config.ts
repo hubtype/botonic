0d96d1b768d722a8d0697ef7799d90145beaa11b
Eric Marcos <ericmarcos.p@gmail.com>
2020-11-02 15:30:35 +0100

    docs: remove outdated example

--- examples/hubtype_demo/src/config.ts
+++
@@ -1,4 +0,0 @@
-export class ContentfulConfig {
-  spaceId = ''
-  accessToken = ''
-}
--- examples/hubtype_demo/src/index.js
+++
@@ -1,5 +0,0 @@
-export { routes } from './routes'
-export { locales } from './locales'
-export { plugins } from './plugins'
-export { webchat } from './webchat'
-export { webviews } from './webviews'
