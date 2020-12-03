2b08235c571685a488b78d0b74fcd14adea0d81b
Dani Pinyol <github@dpinol.com>
2020-12-03 17:18:48 +0100

    refactor(contentful): removed rootLocale
    languageFromLocale does the same

--- packages/botonic-plugin-contentful/tests/nlp/locales.test.ts
+++ packages/botonic-plugin-contentful/tests/nlp/locales.test.ts
@@ -1,15 +1,32 @@
-import { buildLocale, preprocess, rootLocale } from '../../src/nlp/locales'
+import {
+  buildLocale,
+  countryFromLocale,
+  fixLocale,
+  languageFromLocale,
+  preprocess,
+} from '../../src/nlp/locales'
 
 test('TEST: preprocess', () => {
   expect(preprocess('es-ES', ' ÑÇáü òL·l ')).toEqual('ncau ol·l')
 })
 
-test('TEST: rootLocale', () => {
-  expect(rootLocale('es-ES')).toEqual('es')
-  expect(rootLocale('en')).toEqual('en')
+test('TEST: languageFromLocale', () => {
+  expect(languageFromLocale('es-ES')).toEqual('es')
+  expect(languageFromLocale('en')).toEqual('en')
+})
+
+test('TEST: countryFromLocale', () => {
+  expect(countryFromLocale('es-AR')).toEqual('AR')
+  expect(countryFromLocale('es')).toEqual('')
 })
 
 test('TEST: buildLocale', () => {
   expect(buildLocale('ES', 'es')).toEqual('es-ES')
   expect(buildLocale('EN', undefined)).toEqual('en')
+})
+
+test('TEST: fixLocale', () => {
+  expect(fixLocale('ES')).toEqual('es')
+  expect(fixLocale('es-ES')).toEqual('es-ES')
+  expect(fixLocale('ES-es')).toEqual('es-ES')
 })
