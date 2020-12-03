62865079dbcc697a238ea96a02be93d7b6e39c89
Dani Pinyol <github@dpinol.com>
2020-12-03 19:04:51 +0100

    feat(contentful): only import stemmer for locales in use

--- packages/botonic-plugin-contentful/src/nlp/stemmer.ts
+++ packages/botonic-plugin-contentful/src/nlp/stemmer.ts
@@ -1,5 +1,6 @@
 import Stemmer from '@nlpjs/core/src/stemmer'
 
+import { SingletonMap } from '../util'
 import { languageFromLocale, Locale } from './locales'
 import { StemmerHr } from './stemmers/stemmer-hr'
 import { StemmerSk } from './stemmers/stemmer-sk'
@@ -9,27 +10,83 @@ import { StemmerSk } from './stemmers/stemmer-sk'
 // snowball algorithm inspired from https://github.com/MihaiValentin/lunr-languages, based on
 // https://github.com/fortnightlabs/snowball-js/blob/master/stemmer/src/ext/SpanishStemmer.js based on
 // java version at http://snowball.tartarus.org/download.html
-const stemmers: { [key: string]: Stemmer } = {
-  ca: new StemmerCa(),
-  en: new StemmerEn(),
-  es: new StemmerEs(),
-  pl: new StemmerPl(),
-  pt: new StemmerPt(),
-  ru: new StemmerRu(),
-  tr: new StemmerTr(),
-  it: new StemmerIt(),
-  fr: new StemmerFr(),
-  de: new StemmerDe(),
-  ro: new StemmerRo(),
-  el: new StemmerEl(),
-  cs: new StemmerCs(),
-  uk: new StemmerUk(),
-  hr: new StemmerHr(),
-  sk: new StemmerSk(),
-}
+const stemmers = new SingletonMap<Stemmer>({
+  ca: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerCa = require('@nlpjs/lang-ca/src/stemmer-ca')
+    return new StemmerCa()
+  },
+  en: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerEn = require('@nlpjs/lang-en-min/src/stemmer-en')
+    return new StemmerEn()
+  },
+  es: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerEs = require('@nlpjs/lang-es/src/stemmer-es')
+    return new StemmerEs()
+  },
+  pl: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerPl = require('@nlpjs/lang-pl/src/stemmer-pl')
+    return new StemmerPl()
+  },
+  pt: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerPt = require('@nlpjs/lang-pt/src/stemmer-pt')
+    return new StemmerPt()
+  },
+  ru: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerRu = require('@nlpjs/lang-ru/src/stemmer-ru')
+    return new StemmerRu()
+  },
+  tr: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerTr = require('@nlpjs/lang-tr/src/stemmer-tr')
+    return new StemmerTr()
+  },
+  it: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerIt = require('@nlpjs/lang-it/src/stemmer-it')
+    return new StemmerIt()
+  },
+  fr: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerFr = require('@nlpjs/lang-fr/src/stemmer-fr')
+    return new StemmerFr()
+  },
+  de: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerDe = require('@nlpjs/lang-de/src/stemmer-de')
+    return new StemmerDe()
+  },
+  ro: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerRo = require('@nlpjs/lang-ro/src/stemmer-ro')
+    return new StemmerRo()
+  },
+  el: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerEl = require('@nlpjs/lang-el/src/stemmer-el')
+    return new StemmerEl()
+  },
+  cs: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerCs = require('@nlpjs/lang-cs/src/stemmer-cs')
+    return new StemmerCs()
+  },
+  uk: () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const StemmerUk = require('@nlpjs/lang-uk/src/stemmer-uk')
+    return new StemmerUk()
+  },
+  hr: () => new StemmerHr(),
+  sk: () => new StemmerSk(),
+})
 
 export function stemmerFor(locale: Locale): Stemmer {
-  const stem = stemmers[languageFromLocale(locale)]
+  const stem = stemmers.value(languageFromLocale(locale))
   if (!stem) {
     throw new Error(`No stemmer configured for locale '${locale}'`)
   }
