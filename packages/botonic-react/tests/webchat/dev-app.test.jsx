9ede045feffa440d9f37be58ee14565ce6860e0d
Eric Marcos <ericmarcos.p@gmail.com>
2020-12-04 18:42:10 +0100

    test(react): add DevApp and WebchatApp tests

---
+++ packages/botonic-react/tests/webchat/dev-app.test.jsx
@@ -0,0 +1,25 @@
+import { act, screen } from '@testing-library/react'
+
+import { ROLES } from '../../src/constants'
+import { DevApp } from '../../src/dev-app'
+import { expectNotToHaveRoles, expectToHaveRoles } from '../helpers/test-utils'
+
+describe('TEST: Dev App', () => {
+  // https://stackoverflow.com/questions/42805128/does-jest-reset-the-jsdom-document-after-every-suite-or-test
+  afterEach(() => {
+    document.getElementsByTagName('body')[0].innerHTML = ''
+  })
+
+  // To avoid TypeError: frame.scrollTo is not a function
+  window.HTMLElement.prototype.scrollTo = function () {}
+
+  it('TEST: DevApp renders webchat and debug tab', async () => {
+    const devApp = new DevApp({ routes: [] })
+    await act(async () => {
+      devApp.render()
+    })
+    expectToHaveRoles([ROLES.TRIGGER_BUTTON], screen)
+    expectNotToHaveRoles([ROLES.WEBCHAT], screen)
+    expect(screen.getByText(/Botonic Dev Console/i)).toBeTruthy()
+  })
+})
