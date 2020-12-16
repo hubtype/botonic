20d56383b249ce9b6dd90b9c4d92640386f06dca
Dani Pinyol <github@dpinol.com>
2020-12-16 08:16:12 +0100

    chore(contentful): fix cd's in CI scripts
    remove NBSP
    upgrade ts eslint dev dep

--- scripts/ci/bump-version.sh
+++ scripts/ci/bump-version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$BIN_DIR/.." || exit 1
+cd "$BIN_DIR/../.." || exit 1
 
 
 cd packages || exit 1
 
-VERSION=$1 
+VERSION=$1
 PHASE=$2
 
 update_botonic_deps (){
   # $template $1
-  # $VERSION $2
+  # $VERSION $2
   # $PHASE $3
   echo " - Updating botonic deps for '$1'"
   if [[ "$3" == "rc" ]]
@@ -28,10 +28,10 @@ if [ -z "$PHASE" ]
       echo "Bumping $package to $VERSION"
       echo "===================================="
 
-      # Update botonic dependencies for every template
+      # Update botonic dependencies for every template
       if [[ "$package" == "botonic-cli" ]]
       then
-        cd "templates" || exit
+        cd "templates" || exit
         for project in *; do
           cd "$project"
           update_botonic_deps "$project" "$VERSION" "$PHASE"
@@ -45,7 +45,7 @@ if [ -z "$PHASE" ]
       then
         update_botonic_deps "$package" "$VERSION" "$PHASE"
       fi
-      
+
       # Updates package.json and package-lock.json
       nice npm version $VERSION > /dev/null
       cd ..
