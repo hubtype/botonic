ffca5b6bf105666e1831ec60dd0bc51ade183f33
Lolo <manuel_fidalgo@hotmail.com>
2021-01-29 14:52:47 +0100

    chore: configure code coverage in botonic-cli

---
+++ packages/botonic-cli/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  roots: ['src/'],
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(js|jsx)$',
+  testPathIgnorePatterns: [
+    'lib',
+    '.*.d.ts',
+    'tests/helpers',
+    '.*.helper.js',
+    'tests/__mocks__',
+  ],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!/node_modules/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@botonic|react-children-utilities).+\\.(js|jsx)$',
+  ],
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  snapshotSerializers: [],
+  modulePaths: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/__mocks__/file-mock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+}
