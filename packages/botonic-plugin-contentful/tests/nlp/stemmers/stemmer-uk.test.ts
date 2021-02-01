4a15103100136bb7ac329a075ddd1f72c66af448
Eric Lozano <ericlozano98@gmail.com>
2021-02-01 13:27:49 +0100

    fix(contentful): stemmer-uk has been implemented without look-behind regex.

---
+++ packages/botonic-plugin-contentful/tests/nlp/stemmers/stemmer-uk.test.ts
@@ -0,0 +1,15 @@
+import { tokenizerPerLocale } from '../../../src/nlp'
+import { StemmerUk } from '../../../src/nlp/stemmers/stemmer-uk'
+
+test.each<any>([
+  ['розмовляючи', ['розмовляюч']],
+  ['говорити', ['говор']],
+  ['парковка', ['парковк']],
+  ['експеримент', ['експеримент']],
+  ['зустрічі', ['зустріч']],
+])('TEST: Ukrainian stemmer("%s")->"%s"', (word: string, expected: string) => {
+  const tokenizer = tokenizerPerLocale('hr')
+  const sut = new StemmerUk()
+  const result = sut.stem(tokenizer.tokenize(word, false))
+  expect(result).toEqual(expected)
+})
