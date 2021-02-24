c5f96edd3b0aee3ca526954b1e6b0b26b68882b4
Eric Lozano <ericlozano98@gmail.com>
2021-02-24 13:34:11 +0100

    chore(ner): prediction and model saving/loading implemented

--- packages/botonic-nlp/src/tasks/ner/botonic-ner.ts
+++ packages/botonic-nlp/src/tasks/ner/botonic-ner.ts
@@ -3,10 +3,10 @@ import {
   loadLayersModel,
   Scalar,
   tensor,
-  Tensor2D,
   Tensor3D,
 } from '@tensorflow/tfjs-node'
-import { mkdirSync } from 'fs'
+import { mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
 
 import { DatabaseManager } from '../../embeddings/database-manager'
 import { Embedder } from '../../embeddings/embedder'
@@ -16,12 +16,13 @@ import { Sample } from '../../parser/types'
 import { Codifier } from '../../preprocess/codifier'
 import { PADDING_TOKEN, UNKNOWN_TOKEN } from '../../preprocess/constants'
 import { trainTestSplit } from '../../preprocess/selection'
+import { Locale } from '../../types'
 import { BotonicTaskTemplate } from '../botonic-task-template'
 import { ModelEvaluation } from '../types'
-import { NEUTRAL_ENTITY } from './constants'
+import { MODEL_CONFIG_FILENAME, NEUTRAL_ENTITY } from './constants'
 import { createBiLstmModel } from './models/bilstm-model'
 import { NerPreprocessor } from './preprocessor'
-import { Entity, ModelTemplate } from './types'
+import { Entity, ModelConfig, ModelTemplate, Set } from './types'
 
 export class BotonicNer extends BotonicTaskTemplate {
   entities: string[]
@@ -29,21 +30,40 @@ export class BotonicNer extends BotonicTaskTemplate {
   private tokensCodifier: Codifier
   private entitiesCodifier: Codifier
   private model: LayersModel
-  private xTrain: Tensor2D
-  private xTest: Tensor2D
-  private yTrain: Tensor3D
-  private yTest: Tensor3D
+  private trainSet: Set
+  private testSet: Set
 
-  loadData(path: string, testSize = 0.2, shuffle = true): void {
-    const data = Parser.parse(path)
-    this.entities = [NEUTRAL_ENTITY].concat(data.entities)
-
-    const { train, test } = trainTestSplit(data.samples, testSize, shuffle)
-
+  private constructor(locale: Locale, maxLength: number) {
+    super(locale, maxLength)
     this.preprocessor = new NerPreprocessor(
       this.maxLength,
       this.preprocessEngines
     )
+  }
+
+  static with(locale: Locale, maxLength: number): BotonicNer {
+    return new BotonicNer(locale, maxLength)
+  }
+
+  static async from(path: string): Promise<BotonicNer> {
+    const config: ModelConfig = JSON.parse(
+      readFileSync(join(path, MODEL_CONFIG_FILENAME), 'utf-8')
+    )
+    const ner = new BotonicNer(config.locale, config.maxLength)
+    ner.entitiesCodifier = Codifier.with(config.entities, true, false)
+    ner.tokensCodifier = Codifier.with(config.vocabulary, false, true)
+
+    ner.model = await loadLayersModel(`file://${path}/model.json`)
+    return ner
+  }
+
+  loadData(path: string, testSize = 0.2, shuffle = true): void {
+    const { entities, samples } = Parser.parse(path)
+
+    this.entities = [NEUTRAL_ENTITY].concat(entities)
+
+    const { train, test } = trainTestSplit(samples, testSize, shuffle)
+
     const preprocessedTrainSet = this.preprocessor.preprocess(train)
     const preprocessedTestSet = this.preprocessor.preprocess(test)
 
@@ -54,21 +74,19 @@ export class BotonicNer extends BotonicTaskTemplate {
 
     this.entitiesCodifier = Codifier.with(this.entities, true, false)
 
-    this.xTrain = tensor(
-      preprocessedTrainSet.x.map(s => this.tokensCodifier.encode(s))
-    )
+    this.trainSet = {
+      x: tensor(preprocessedTrainSet.x.map(s => this.tokensCodifier.encode(s))),
+      y: tensor(
+        preprocessedTrainSet.y.map(s => this.entitiesCodifier.encode(s))
+      ),
+    }
 
-    this.xTest = tensor(
-      preprocessedTestSet.x.map(s => this.tokensCodifier.encode(s))
-    )
-
-    this.yTrain = tensor(
-      preprocessedTrainSet.y.map(s => this.entitiesCodifier.encode(s))
-    )
-
-    this.yTest = tensor(
-      preprocessedTestSet.y.map(s => this.entitiesCodifier.encode(s))
-    )
+    this.testSet = {
+      x: tensor(preprocessedTestSet.x.map(s => this.tokensCodifier.encode(s))),
+      y: tensor(
+        preprocessedTestSet.y.map(s => this.entitiesCodifier.encode(s))
+      ),
+    }
   }
 
   async createModel(
@@ -99,18 +117,17 @@ export class BotonicNer extends BotonicTaskTemplate {
     }
   }
 
-  async loadModel(path: string): Promise<void> {
-    this.model = await loadLayersModel(`file://${path}/model.json`)
-  }
-
   async train(epochs: number, batchSize: number): Promise<void> {
-    await this.model.fit(this.xTrain, this.yTrain, { epochs, batchSize })
+    await this.model.fit(this.trainSet.x, this.trainSet.y, {
+      epochs,
+      batchSize,
+    })
   }
 
   async evaluate(): Promise<ModelEvaluation> {
     const [loss, accuracy] = this.model.evaluate(
-      this.xTest,
-      this.yTest
+      this.testSet.x,
+      this.testSet.y
     ) as Scalar[]
     return {
       loss: loss.arraySync(),
@@ -118,37 +135,52 @@ export class BotonicNer extends BotonicTaskTemplate {
     }
   }
 
-  predict(text: string, threshold = 0.8): Entity[] {
+  predict(text: string, advanced = false): Entity[] {
     const preprocessedData = this.preprocessor.preprocess([
       { text, entities: [], class: '' } as Sample,
     ])
     const tokens = preprocessedData.x[0]
     const x = tensor([this.tokensCodifier.encode(tokens)])
-    const prediction = (this.model.predict(x) as Tensor3D).arraySync()[0]
-    const confidences = prediction.map(c => Math.max(...c))
-    const labels = this.entitiesCodifier.decode(prediction)
-
-    const entities: Entity[] = []
-    tokens.forEach((token, i) => {
-      const confidence = confidences[i]
-      const label = labels[i]
-      if (
-        token != PADDING_TOKEN &&
-        label != NEUTRAL_ENTITY &&
-        confidence >= threshold
-      ) {
-        entities.push({
-          text: token,
-          label: labels[i],
-          confidence: confidences[i],
-        })
-      }
-    })
-    return entities
+    const prediction = this.model.predict(x) as Tensor3D
+    return this.generateEntityPredictions(tokens, prediction, advanced)
   }
 
-  async saveModel(path: string): Promise<void> {
+  private generateEntityPredictions(
+    tokens: string[],
+    prediction: Tensor3D,
+    advanced: boolean
+  ): Entity[] {
+    const confidences = prediction.arraySync()[0]
+    const entities = tokens.map((token, idx) => {
+      const tokenConfidences = confidences[idx]
+      const confidence = Math.max(...tokenConfidences)
+      const label = this.entities[tokenConfidences.indexOf(confidence)]
+      const entity: Entity = { text: token, confidence, label }
+      if (advanced) {
+        entity.predictions = tokenConfidences.map((c, id) => {
+          return { label: this.entities[id], confidence: c }
+        })
+      }
+      return entity
+    })
+    return advanced
+      ? entities.filter(e => e.text !== PADDING_TOKEN)
+      : entities.filter(
+          e => e.text !== PADDING_TOKEN && e.label !== NEUTRAL_ENTITY
+        )
+  }
+
+  async save(path: string): Promise<void> {
     mkdirSync(path, { recursive: true })
+
+    const config: ModelConfig = {
+      locale: this.locale,
+      maxLength: this.maxLength,
+      vocabulary: this.tokensCodifier.vocabulary,
+      entities: this.entitiesCodifier.vocabulary,
+    }
+
+    writeFileSync(join(path, MODEL_CONFIG_FILENAME), JSON.stringify(config))
     await this.model.save(`file://${path}`)
   }
 }
--- packages/botonic-nlp/src/tasks/ner/constants.ts
+++ packages/botonic-nlp/src/tasks/ner/constants.ts
@@ -1,1 +1,3 @@
 export const NEUTRAL_ENTITY = 'O'
+
+export const MODEL_CONFIG_FILENAME = 'model-config.json'
