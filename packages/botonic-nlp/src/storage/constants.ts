c2d94c8cd47bd6452f48a4282ee7349fab91353f
Eric Lozano <ericlozano98@gmail.com>
2021-03-11 15:28:17 +0100

    chore(storage): model filename set as constant

--- packages/botonic-nlp/src/storage/constants.ts
+++ packages/botonic-nlp/src/storage/constants.ts
@@ -1,1 +1,2 @@
 export const CONFIG_FILENAME = 'config.json'
+export const MODEL_FILENAME = 'model.json'
--- packages/botonic-nlp/src/storage/model-storage.ts
+++ packages/botonic-nlp/src/storage/model-storage.ts
@@ -1,9 +1,11 @@
 import { LayersModel, loadLayersModel } from '@tensorflow/tfjs-node'
 import { mkdirSync } from 'fs'
 
+import { MODEL_FILENAME } from './constants'
+
 export class ModelStorage {
   static async load(path: string): Promise<LayersModel> {
-    return await loadLayersModel(`file://${path}/model.json`)
+    return await loadLayersModel(`file://${path}/${MODEL_FILENAME}`)
   }
 
   static async save(model: LayersModel, path: string): Promise<void> {
