c30c42f11306f9ea3c9818dc0946e6a71974147b
Eric Lozano <ericlozano98@gmail.com>
2021-03-15 16:32:10 +0100

    feat(cli): botonic train --task=[task]

--- packages/botonic-cli/src/commands/train.ts
+++ packages/botonic-cli/src/commands/train.ts
@@ -1,44 +1,66 @@
 import { Command, flags } from '@oclif/command'
 import colors from 'colors'
-import * as path from 'path'
+import { join } from 'path'
 
 import { spawnNpmScript, track } from '../utils'
 
+const DEFAULT_TASK = 'text-classification'
+const TASKS = ['text-classification', 'ner']
+
 export default class Run extends Command {
-  static description = 'Train your bot with NLU'
+  static description = 'Train your bot with NLP'
 
   static examples = [
-    `$ botonic train
-    TRAINING MODEL FOR {LANGUAGE}...
+    `$ botonic train --task={${TASKS.join('|')}}
+    TRAINING MODEL FOR {TASK}...
     `,
   ]
 
   static flags = {
-    lang: flags.string(),
+    task: flags.string(),
   }
 
   static args = []
 
   async run(): Promise<void> {
-    const botonicNLUPath: string = path.join(
+    const { flags } = this.parse(Run)
+    const task = flags.task ?? DEFAULT_TASK
+
+    if (!TASKS.includes(task)) {
+      console.log(
+        colors.red(
+          `Unsupported task '${task}'. Available tasks: '${TASKS.join(
+            "', '"
+          )}'.`
+        )
+      )
+      return
+    }
+
+    const botonicNlpPath = join(
       process.cwd(),
       'node_modules',
       '@botonic',
-      'nlu'
+      `nlp`
     )
+
     try {
-      await import(botonicNLUPath)
+      await import(botonicNlpPath)
     } catch (e) {
       console.log(e)
       console.log(
         colors.red(
-          `You don't have @botonic/plugin-nlu installed.\nPlease, install it with the following command:`
+          `You don't have @botonic/plugin-${task} installed.\nPlease, install it with the following command:`
         )
       )
-      console.log(`$ npm install @botonic/plugin-nlu`)
+      console.log(`$ npm install @botonic/plugin-${task}`)
       return
     }
+
     track('Trained with Botonic train')
-    spawnNpmScript('train', () => 'Finished training')
+    spawnNpmScript(
+      `train:${flags.task ?? DEFAULT_TASK}`,
+      () => `Finished training `
+    )
   }
 }
