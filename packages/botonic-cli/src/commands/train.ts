d70f0a15e6d79c75eec220dc793513fec369e8f4
Eric Lozano <ericlozano98@gmail.com>
2021-03-19 16:30:37 +0100

    feat(cli): choose nlp task on train command

--- packages/botonic-cli/src/commands/train.ts
+++ packages/botonic-cli/src/commands/train.ts
@@ -1,21 +1,27 @@
 import { Command, flags } from '@oclif/command'
 import colors from 'colors'
-import * as path from 'path'
+import { existsSync } from 'fs'
+import { join } from 'path'
 
 import { Telemetry } from '../analytics/telemetry'
 import { spawnNpmScript } from '../util/system'
 
+enum TASKS {
+  NER = 'ner',
+  TEXT_CLASSIFICATION = 'text-classification',
+}
+
 export default class Run extends Command {
-  static description = 'Train your bot with NLU'
+  static description = 'Train your bot with NLP'
 
   static examples = [
-    `$ botonic train
-    TRAINING MODEL FOR {LANGUAGE}...
+    `$ botonic train [--task=<${Object.values(TASKS).join('|')}>]
+    TRAINING MODEL...
     `,
   ]
 
   static flags = {
-    lang: flags.string(),
+    task: flags.string(),
   }
 
   static args = []
@@ -23,27 +29,79 @@ export default class Run extends Command {
   private telemetry = new Telemetry()
 
   async run(): Promise<void> {
-    this.telemetry.trackTrain()
-    const botonicNLUPath: string = path.join(
+    const { flags } = this.parse(Run)
+    const task = flags.task
+    if (task) {
+      this.runSpecificTask(task)
+    } else {
+      this.runAllTasks()
+    }
+  }
+
+  private runAllTasks(): void {
+    this.runNerTask()
+    this.runTextClassificationTask()
+  }
+
+  private runSpecificTask(task: string): void {
+    switch (task) {
+      case TASKS.NER:
+        this.runNerTask()
+        break
+      case TASKS.TEXT_CLASSIFICATION:
+        this.runTextClassificationTask()
+        break
+      default:
+        this.logInvalidTask(task)
+        break
+    }
+  }
+
+  private runNerTask(): void {
+    if (this.isPackageInstalled(`plugin-${TASKS.NER}`)) {
+      track(`Trained with Botonic train:${TASKS.NER}`)
+      spawnNpmScript(`train:${TASKS.NER}`, () => `Finished training `)
+    }
+  }
+
+  private runTextClassificationTask(): void {
+    if (this.isPackageInstalled(`plugin-${TASKS.TEXT_CLASSIFICATION}`)) {
+      track(`Trained with Botonic train:${TASKS.TEXT_CLASSIFICATION}`)
+      spawnNpmScript(
+        `train:${TASKS.TEXT_CLASSIFICATION}`,
+        () => `Finished training `
+      )
+    }
+  }
+
+  private logInvalidTask(task: string): void {
+    console.log(
+      colors.red(
+        `Unsupported task '${task}'. Available tasks: '${Object.values(
+          TASKS
+        ).join("', '")}'.`
+      )
+    )
+  }
+
+  private isPackageInstalled(packageName: string): boolean {
+    const path = join(
       process.cwd(),
       'node_modules',
       '@botonic',
-      'nlu'
+      packageName,
+      'dist'
     )
-    try {
-      await import(botonicNLUPath)
-    } catch (e) {
-      const error = String(e)
-      this.telemetry.trackError(`Running botonic train: ${error}`)
-      console.log(e)
+    if (existsSync(path)) {
+      return true
+    } else {
       console.log(
         colors.red(
-          `You don't have @botonic/plugin-nlu installed.\nPlease, install it with the following command:`
+          `Training process has been stopped because you don't have @botonic/${packageName} installed.\nPlease, install it with the following command:`
         )
       )
-      console.log(`$ npm install @botonic/plugin-nlu`)
-      return
+      console.log(`$ npm install @botonic/${packageName}`)
+      return false
     }
-    spawnNpmScript('train', () => 'Finished training')
   }
 }
