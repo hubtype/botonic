385dfd327fa31e145073d6bca850d52bcf6480fb
Eric Lozano <ericlozano98@gmail.com>
2021-03-22 13:16:00 +0100

    feat(preprocess): input generator implemented

---
+++ packages/botonic-plugin-ner/tests/preprocess/input-generator.test.ts
@@ -0,0 +1,17 @@
+import { InputGenerator } from '../../src/preprocess/input-generator'
+import * as constantsHelper from '../helper/constants-helper'
+import * as toolsHelper from '../helper/tools-helper'
+
+describe('Input generator', () => {
+  test('generate input', () => {
+    const generator = new InputGenerator(
+      toolsHelper.preprocessor,
+      toolsHelper.tokenCodifier,
+      toolsHelper.entitiesCodifier
+    )
+
+    const sut = generator.generate(constantsHelper.SENTENCE)
+    expect(sut.sequence).toEqual(constantsHelper.SEQUENCE)
+    expect(sut.input.arraySync()).toEqual(constantsHelper.INPUT.arraySync())
+  })
+})
