8142f1bf68c8dbc43e3fa40c2d39e383d0d848a1
Eric Lozano <ericlozano98@gmail.com>
2021-03-23 17:12:15 +0100

    refactor(config): request changes applied to general config

--- packages/botonic-plugin-ner/jest.config.js
+++ packages/botonic-plugin-ner/jest.config.js
@@ -2,11 +2,4 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!/node_modules/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transformIgnorePatterns: [
-    'node_modules/(?!@botonic|@tensorflow).+\\.(js|jsx)$',
-  ],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': 'babel-jest',
-  },
 }
