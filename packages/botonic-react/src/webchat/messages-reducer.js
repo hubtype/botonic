92f88c667741084869509952e287dd59a1a78421
Marc Rabat <marcrabat@gmail.com>
2021-03-24 16:08:43 +0100

    chore(react): revert msgComponents duplicates check

--- packages/botonic-react/src/webchat/messages-reducer.js
+++ packages/botonic-react/src/webchat/messages-reducer.js
@@ -7,24 +7,11 @@ import {
   UPDATE_REPLIES,
 } from './actions'
 
-const getMsgComponentId = m => {
-  if (m.props.id) return m.props.id
-  if (m.props.value) return m.props.value.input && m.props.value.input.id
-  return undefined
-}
-
 export const messagesReducer = (state, action) => {
   switch (action.type) {
     case ADD_MESSAGE:
       return addMessageReducer(state, action)
     case ADD_MESSAGE_COMPONENT:
-      if (
-        state.messagesComponents &&
-        state.messagesComponents.find(
-          m => getMsgComponentId(m) === action.payload.id
-        )
-      )
-        return state
       return {
         ...state,
         messagesComponents: [
