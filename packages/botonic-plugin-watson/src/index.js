0d6f2bbd50bb0976eee46ba5ee5fafc1199d7575
Marc Rabat <marcrabat@gmail.com>
2021-03-25 17:59:18 +0100

    refactor(plugin-watson): revive watson plugin with assistantV2

--- packages/botonic-plugin-watson/src/index.js
+++ packages/botonic-plugin-watson/src/index.js
@@ -1,36 +1,92 @@
-import AssistantV1 from 'ibm-watson/assistant/v1'
-import IamAuthenticator from 'ibm-watson/auth'
+import axios from 'axios'
 
+// TODO: Documentate changes
+
+const REQUIRED_OPTIONS = ['apikey', 'url', 'assistant_id']
 export default class BotonicPluginWatson {
   constructor(options) {
-    this.options = options
+    try {
+      this.validateOptions(options)
+      this.init(options)
+    } catch (e) {
+      console.error(String(e))
+    }
   }
 
-  async pre({ input, session, lastRoutePath }) {
+  init(options) {
+    this.apiKey = options.apikey
+    this.url = options.url
+    this.assistantId = options.assistant_id
+    this.version = options.version || '2020-04-01'
+    this.sessionId = this.getSessionId()
+  }
+
+  validateOptions(options) {
+    if (!options) throw new Error('No options provided.')
+    const requiredOptionsProvided = Object.entries(options).filter(([k, _]) =>
+      REQUIRED_OPTIONS.includes(k)
+    )
+    if (requiredOptionsProvided.length !== REQUIRED_OPTIONS.length)
+      throw new Error('Missing one or more fields.')
+    if (requiredOptionsProvided.some(([_, v]) => v === undefined))
+      throw new Error('One or more fields are not defined.')
+  }
+
+  getAssistantEndpoint() {
+    return `${this.url}/v2/assistants/${this.assistantId}`
+  }
+
+  async getSessionId() {
+    const {
+      data: { session_id },
+    } = await axios.post(
+      `${this.getAssistantEndpoint()}/sessions?version=${this.version}`,
+      {},
+      {
+        auth: { username: 'apikey', password: this.apiKey },
+        validateStatus: status => status === 201,
+      }
+    )
+    return session_id
+  }
+
+  async pre({ input }) {
     let intent = null
     let confidence = 0
     let intents = []
     let entities = []
-
     try {
-      const assistant = new AssistantV1({
-        authenticator: new IamAuthenticator({ apikey: this.options.apiKey }),
-        url: this.options.url,
-        version: '2018-09-19',
+      // TODO: Understand sessionId logic/api-limits
+      const sessionId = await this.sessionId
+      const {
+        data: { output },
+      } = await axios.post(
+        `${this.getAssistantEndpoint()}/sessions/${sessionId}/message?version=${
+          this.version
+        }`,
+        { input: { text: input.data } },
+        {
+          auth: { username: 'apikey', password: this.apiKey },
+          headers: { 'Content-Type': 'application/json' },
+          validateStatus: status => status === 200,
+        }
+      )
+      // TODO: Convert results to our Botonic standard
+      intent = output.intents[0].intent
+      confidence = output.intents[0].confidence
+      intents = output.intents
+      entities = output.entities
+      const defaultFallback = output.generic //example in dialogflow plugin, decide which name?
+      Object.assign(input, {
+        intent,
+        confidence,
+        intents,
+        entities,
+        defaultFallback,
       })
-
-      const res = await assistant.message({
-        input: { text: input.data },
-        workspaceId: this.options.workspaceId,
-      })
-      intent = res.result.intents[0].intent
-      confidence = res.result.intents[0].intent.confidence
-      intents = res.result.intents
-      entities = res.result.entities
-    } catch (error) {
-      console.log(error)
+    } catch (e) {
+      console.error(String(e))
     }
-    Object.assign(input, { intent, confidence, intents, entities })
   }
 
   post({ input, session, lastRoutePath, response }) {}
