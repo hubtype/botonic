9855c6ccd9cdc8bedf11e416339ac6c382164bcf
Eric Lozano <ericlozano98@gmail.com>
2021-03-30 15:04:32 +0200

    feat(plugin-watson): entities directly returned

--- packages/botonic-plugin-watson/src/watson-output-parser.js
+++ packages/botonic-plugin-watson/src/watson-output-parser.js
@@ -10,18 +10,12 @@ export default class WatsonOutputParser {
       intent: intent.intent,
       confidence: intent.confidence,
       intents: output.intents,
-      entities: this.parseEntities(output.entities),
+      entities: output.entities,
     }
   }
 
   static getIntentWithMaxConfidence(intents) {
     intents.sort((i1, i2) => (i1.confidence > i2.confidence ? -1 : 1))
     return intents.length == 0 ? this.UNKNOWN_INTENT : intents[0]
-  }
-
-  static parseEntities(entities) {
-    return entities.map(e => {
-      return { entity: e.entity, value: e.value, confidence: e.confidence }
-    })
   }
 }
