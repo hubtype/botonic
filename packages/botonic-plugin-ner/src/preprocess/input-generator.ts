706fb9b65a839e1f5a5942354817f428ff5385b7
Eric Lozano <46075558+elozano98@users.noreply.github.com>
2021-03-30 13:29:55 +0200

    feat(ner-plugin): Preprocess implemented (#1393)
    * feat(preprocess): input generator implemented
    
    * refactor(plugin-ner): InputGenerator methods sorted
    
    * refactor(plugin-ner): entitiesCodifier dependency removed

---
+++ packages/botonic-plugin-ner/src/preprocess/input-generator.ts
@@ -0,0 +1,43 @@
+import { Codifier } from '@botonic/nlp/dist/preprocess/codifier'
+import {
+  PADDING_TOKEN,
+  UNKNOWN_TOKEN,
+} from '@botonic/nlp/dist/preprocess/constants'
+import { Preprocessor } from '@botonic/nlp/dist/preprocess/preprocessor'
+import { tensor, Tensor2D } from '@tensorflow/tfjs'
+
+export class InputGenerator {
+  constructor(
+    readonly preprocessor: Preprocessor,
+    readonly tokenCodifier: Codifier
+  ) {}
+
+  generate(text: string): { sequence: string[]; input: Tensor2D } {
+    const sequence = this.processText(text)
+    const input = tensor([this.processTokens(sequence)]) as Tensor2D
+    return { sequence, input }
+  }
+
+  private processText(text: string): string[] {
+    return this.preprocessor.pad(
+      this.preprocessor.stem(
+        this.preprocessor.removeStopwords(
+          this.preprocessor.tokenize(this.preprocessor.normalize(text))
+        )
+      ),
+      PADDING_TOKEN
+    )
+  }
+
+  private processTokens(sequence: string[]): number[] {
+    return this.tokenCodifier.encode(
+      this.maskUnknownTokens(sequence)
+    ) as number[]
+  }
+
+  private maskUnknownTokens(tokens: string[]): string[] {
+    return tokens.map(t =>
+      this.tokenCodifier.vocabulary.includes(t) ? t : UNKNOWN_TOKEN
+    )
+  }
+}
