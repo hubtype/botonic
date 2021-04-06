b8a009d9ae04596e0a5dcb7dec091b411d31e22a
Eric Lozano <ericlozano98@gmail.com>
2021-04-06 15:57:23 +0200

    test(plugin-ner): processor tests renamed

--- packages/botonic-plugin-ner/tests/process/text-processor.test.ts
+++ packages/botonic-plugin-ner/tests/process/text-processor.test.ts
@@ -1,15 +1,15 @@
-import { InputGenerator } from '../../src/preprocess/input-generator'
+import { TextProcessor } from '../../src/process/text-processor'
 import * as constantsHelper from '../helper/constants-helper'
 import * as toolsHelper from '../helper/tools-helper'
 
 describe('Input generator', () => {
   test('generate input', () => {
-    const generator = new InputGenerator(
-      toolsHelper.preprocessor,
-      toolsHelper.tokenCodifier
+    const processor = new TextProcessor(
+      constantsHelper.VOCABULARY,
+      toolsHelper.preprocessor
     )
 
-    const sut = generator.generate(constantsHelper.SENTENCE)
+    const sut = processor.process(constantsHelper.SENTENCE)
     expect(sut.sequence).toEqual(constantsHelper.SEQUENCE)
     expect(sut.input.arraySync()).toEqual(constantsHelper.INPUT.arraySync())
   })
