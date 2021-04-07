7980f542eccfb3e5b05ffef82622fc9ba59b86cd
Eric Lozano <ericlozano98@gmail.com>
2021-04-07 15:37:53 +0200

    refactor(plugin-ner): Codifier replaced by LabelEncoder

--- packages/botonic-plugin-ner/tests/helper/tools-helper.ts
+++ packages/botonic-plugin-ner/tests/helper/tools-helper.ts
@@ -1,7 +1,5 @@
-import { Codifier } from '@botonic/nlp/lib/preprocess/codifier'
 import { Preprocessor } from '@botonic/nlp/lib/preprocess/preprocessor'
 
-import { LOCALE, MAX_LENGTH, VOCABULARY } from './constants-helper'
+import { LOCALE, MAX_LENGTH } from './constants-helper'
 
 export const preprocessor = new Preprocessor(LOCALE, MAX_LENGTH)
-export const tokenCodifier = new Codifier(VOCABULARY, { isCategorical: false })
