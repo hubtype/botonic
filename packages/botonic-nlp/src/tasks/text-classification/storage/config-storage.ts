3dabec6f2f47aa37aab40823cff8eb3e298681dd
Eric Lozano <ericlozano98@gmail.com>
2021-04-08 16:02:23 +0200

    refactor(nlp): static methods removed from storer

--- packages/botonic-nlp/src/tasks/text-classification/storage/config-storage.ts
+++ packages/botonic-nlp/src/tasks/text-classification/storage/config-storage.ts
@@ -1,17 +1,20 @@
 import { mkdirSync, writeFileSync } from 'fs'
 import { join } from 'path'
 
-import { CONFIG_FILENAME } from '../../../storage/constants'
 import { readJSON } from '../../../utils/file-utils'
 import { TextClassificationConfig } from './types'
 
 export class TextClassificationConfigStorage {
-  static load(path: string): TextClassificationConfig {
-    return readJSON(join(path, CONFIG_FILENAME)) as TextClassificationConfig
+  public readonly CONFIG_FILENAME = 'config.json'
+
+  load(path: string): TextClassificationConfig {
+    return readJSON(
+      join(path, this.CONFIG_FILENAME)
+    ) as TextClassificationConfig
   }
 
-  static save(path: string, config: TextClassificationConfig): void {
+  save(path: string, config: TextClassificationConfig): void {
     mkdirSync(path, { recursive: true })
-    writeFileSync(join(path, CONFIG_FILENAME), JSON.stringify(config))
+    writeFileSync(join(path, this.CONFIG_FILENAME), JSON.stringify(config))
   }
 }
--- packages/botonic-nlp/tests/tasks/text-classification/storage/config-storage.test.ts
+++ packages/botonic-nlp/tests/tasks/text-classification/storage/config-storage.test.ts
@@ -1,13 +1,14 @@
 import { existsSync, rmdirSync } from 'fs'
 import { join } from 'path'
 
-import { CONFIG_FILENAME } from '../../../../src/storage/constants'
 import { TextClassificationConfigStorage } from '../../../../src/tasks/text-classification/storage/config-storage'
 import * as helper from '../../../helpers/tasks/text-classification/constants-helper'
 
 describe('Config handler', () => {
+  const storer = new TextClassificationConfigStorage()
+
   test('Load config', () => {
-    const config = TextClassificationConfigStorage.load(helper.MODEL_DIR_PATH)
+    const config = storer.load(helper.MODEL_DIR_PATH)
     expect(config.locale).toEqual(helper.LOCALE)
     expect(config.maxLength).toEqual(helper.MAX_LENGTH)
     expect(config.vocabulary).toEqual(helper.VOCABULARY)
@@ -19,14 +20,14 @@ describe('Config handler', () => {
       helper.TEXT_CLASSIFICATION_DIR_PATH,
       'test-config-storage'
     )
-    TextClassificationConfigStorage.save(path, {
+    storer.save(path, {
       locale: helper.LOCALE,
       maxLength: helper.MAX_LENGTH,
       vocabulary: helper.VOCABULARY,
       classes: helper.CLASSES,
     })
-    expect(existsSync(join(path, CONFIG_FILENAME))).toBeTruthy()
+    expect(existsSync(join(path, storer.CONFIG_FILENAME))).toBeTruthy()
     rmdirSync(path, { recursive: true })
-    expect(existsSync(join(path, CONFIG_FILENAME))).toBeFalsy()
+    expect(existsSync(join(path, storer.CONFIG_FILENAME))).toBeFalsy()
   })
 })
