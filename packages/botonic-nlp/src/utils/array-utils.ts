6c114ed2c5751539336135744cb0e055e819bd3e
Eric Lozano <ericlozano98@gmail.com>
2021-04-14 11:17:04 +0200

    refactor(nlp): randomSort renamed to shuffle

--- packages/botonic-nlp/src/utils/array-utils.ts
+++ packages/botonic-nlp/src/utils/array-utils.ts
@@ -2,6 +2,12 @@ export function unique<T>(array: T[]): T[] {
   return Array.from(new Set(array))
 }
 
-export function randomSort<T>(array: T[]): T[] {
-  return array.sort(() => Math.random() - 0.5)
+export function shuffle<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
 }
