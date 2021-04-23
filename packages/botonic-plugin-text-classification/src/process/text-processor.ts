f42280ae142015fd25126e6782ddd6a782bf4bdc
Eric Lozano <ericlozano98@gmail.com>
2021-04-23 13:20:00 +0200

    feat(plugin-text-classification): TextProcessor implemented

--- packages/botonic-plugin-text-classification/src/process/text-processor.ts
+++ packages/botonic-plugin-text-classification/src/process/text-processor.ts
@@ -1,5 +1,11 @@
+import { LabelEncoder } from '@botonic/nlp/lib/encode/label-encoder'
+import {
+  PADDING_TOKEN,
+  UNKNOWN_TOKEN,
+} from '@botonic/nlp/lib/preprocess/constants'
 import { Preprocessor } from '@botonic/nlp/lib/preprocess/preprocessor'
 import type { Tensor2D } from '@tensorflow/tfjs'
+import { tensor } from '@tensorflow/tfjs'
 
 export class TextProcessor {
   constructor(
@@ -8,7 +14,21 @@ export class TextProcessor {
   ) {}
 
   process(text: string): { sequence: string[]; input: Tensor2D } {
-    //TODO: pending to be implemented
-    return { sequence: null, input: null }
+    const sequence = this.preprocessor.preprocess(text, PADDING_TOKEN)
+    const maskedSequence = this.maskUnknownTokens(sequence)
+    const encodedSequence = this.encodeSequence(maskedSequence)
+    const input = tensor([encodedSequence]) as Tensor2D
+    return { sequence, input }
+  }
+
+  private maskUnknownTokens(sequence: string[]): string[] {
+    return sequence.map(token =>
+      this.vocabulary.includes(token) ? token : UNKNOWN_TOKEN
+    )
+  }
+
+  private encodeSequence(sequence: string[]): number[] {
+    const encoder = new LabelEncoder(this.vocabulary)
+    return encoder.encode(sequence)
   }
 }
