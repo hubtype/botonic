0320afb6972c044afcf5b69df65d50d2806eeeea
Eric Lozano <ericlozano98@gmail.com>
2021-04-23 10:39:53 +0200

    refactor(plugin-text-classification): top-down structure implemented

--- packages/botonic-plugin-text-classification/src/index.ts
+++ packages/botonic-plugin-text-classification/src/index.ts
@@ -1,18 +1,28 @@
 import type { Plugin, PluginPostRequest, PluginPreRequest } from '@botonic/core'
 import { INPUT } from '@botonic/core'
 
+import { ModelSelector } from './model/model-selector'
 import type { PluginOptions } from './types'
+import { detectLocale } from './utils/locale-utils'
 
 export default class BotonicPluginTextClassification implements Plugin {
-  constructor(readonly options: PluginOptions) {}
+  private modelsSelector: Promise<ModelSelector>
+
+  constructor(readonly options: PluginOptions) {
+    this.modelsSelector = ModelSelector.build(this.options.locales)
+  }
 
   async pre(request: PluginPreRequest): Promise<void> {
     try {
       if (request.input.type == INPUT.TEXT && !request.input.payload) {
-        //   TODO: Pending to be implemented.
+        const inputText = request.input.data
+        const detectedLocale = detectLocale(inputText, this.options.locales)
+        const ner = (await this.modelsSelector).select(detectedLocale)
+        const entities = ner.classify(inputText)
+        Object.assign(request.input, { entities })
       }
     } catch (e) {
-      console.error(`Cannot classify the input: ${request.input}`)
+      console.error(`Cannot classify the input: ${request.input}`, e)
     }
   }
 
