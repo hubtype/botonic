47666eb03625e9ad245c45e43d36100960ec7c95
Eric Lozano <ericlozano98@gmail.com>
2021-04-23 12:13:30 +0200

    feat(plugin-text-classification): ModelInfo implemented

--- packages/botonic-plugin-text-classification/src/model/model-info.ts
+++ packages/botonic-plugin-text-classification/src/model/model-info.ts
@@ -1,19 +1,24 @@
 import { TextClassificationConfig } from '@botonic/nlp/lib/tasks/text-classification/storage/types'
 import { Locale } from '@botonic/nlp/lib/types'
-import { LayersModel } from '@tensorflow/tfjs'
+import { LayersModel, loadLayersModel } from '@tensorflow/tfjs'
+import axios, { AxiosPromise } from 'axios'
+
+import { CONFIG_FILENAME, MODEL_FILENAME } from '../constants'
 
 export class ModelInfo {
+  private readonly config: AxiosPromise<TextClassificationConfig>
+  private readonly model: Promise<LayersModel>
+
   constructor(readonly locale: Locale, readonly uri: string) {
-    // TODO: pending to be implemented
+    this.config = axios({ url: `${this.uri}/${CONFIG_FILENAME}` })
+    this.model = loadLayersModel(`${this.uri}/${MODEL_FILENAME}`)
   }
 
   async getConfig(): Promise<TextClassificationConfig> {
-    // TODO: pending to be implemented
-    return null
+    return (await this.config).data
   }
 
   async getModel(): Promise<LayersModel> {
-    // TODO: pending to be implemented
-    return null
+    return await this.model
   }
 }
