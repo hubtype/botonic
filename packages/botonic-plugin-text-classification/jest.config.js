028cdbd1af3068bced50ee996d8c7339def6101e
Eric Lozano <46075558+elozano98@users.noreply.github.com>
2021-04-26 12:39:10 +0200

    feat(plugin-text-classification): BotonicPluginTextClassification implementation #BOT-342 (#1472)
    * feat(plugin-text-classification): plugin configuration
    
    * feat(plugin-text-classification): BotonicPluginTextClassification first implementation
    
    * refactor(plugin-text-classification): error correctly logged
    
    * refactor(plugin-text-classification): top-down structure implemented
    
    * feat(plugin-text-classification): bump to botonic-nlp alpha version 2

---
+++ packages/botonic-plugin-text-classification/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!/node_modules/'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+}
---
+++ packages/botonic-plugin-text-classification/src/utils/locale-utils.ts
@@ -0,0 +1,5 @@
+import { Locale } from '@botonic/nlp/lib/types'
+
+export function detectLocale(input: string, locales: Locale[]): Locale {
+  return 'en'
+}
