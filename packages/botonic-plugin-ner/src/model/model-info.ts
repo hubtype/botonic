9d38295b9b9c8db29aa24b4934a63af0484d4bf0
Eric Lozano <46075558+elozano98@users.noreply.github.com>
2021-04-28 15:45:27 +0200

    refactor(plugin-ner): Cross-fetch used #BOT-357 (#1496)
    * refactor(plugin-ner): node-fetch dependency replaced by cross-fetch
    
    * refactor(plugin-ner): fetch comment moved

--- packages/botonic-plugin-ner/src/model/model-info.ts
+++ packages/botonic-plugin-ner/src/model/model-info.ts
@@ -2,6 +2,7 @@ import { NerConfig } from '@botonic/nlp/lib/tasks/ner/storage/types'
 import { Locale } from '@botonic/nlp/lib/types'
 import { LayersModel, loadLayersModel } from '@tensorflow/tfjs'
 import axios, { AxiosPromise } from 'axios'
+import { fetch } from 'cross-fetch'
 
 import { CONFIG_FILENAME, MODEL_FILENAME } from '../constants'
 
@@ -11,7 +12,9 @@ export class ModelInfo {
 
   constructor(readonly locale: Locale, readonly uri: string) {
     this.config = axios({ url: `${this.uri}/${CONFIG_FILENAME}` })
-    this.model = loadLayersModel(`${this.uri}/${MODEL_FILENAME}`)
+    this.model = loadLayersModel(`${this.uri}/${MODEL_FILENAME}`, {
+      fetchFunc: fetch, // default fetch only works in browser
+    })
   }
 
   async getConfig(): Promise<NerConfig> {
