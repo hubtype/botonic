cb4fe64650638125d359a6058ab52dd56bbe1e65
Dani Pinyol <github@dpinol.com>
2021-05-03 14:19:38 +0200

    chore(dx): minor improvements

--- packages/botonic-dx/sample-config/jest.config.js
+++ packages/botonic-dx/sample-config/jest.config.js
@@ -1,3 +1,4 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const config = require('@botonic/dx/baseline/jest.config.js')
 // You can here patch in config your custom configuration
 // eg. avoid jest to complain on folders without tests
