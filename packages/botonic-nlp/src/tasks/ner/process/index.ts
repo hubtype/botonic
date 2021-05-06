3447d929077d1f273ef92d9578eaf4a0d2a33948
Eric Lozano <ericlozano98@gmail.com>
2021-05-06 15:32:57 +0200

    refactor(nlp): ner process module exports updated

--- packages/botonic-nlp/src/tasks/ner/process/index.ts
+++ packages/botonic-nlp/src/tasks/ner/process/index.ts
@@ -1,6 +1,6 @@
 import { NEUTRAL_ENTITY } from './constants'
 import { PredictionProcessor } from './prediction-processor'
 import { Processor } from './processor'
-import { Entity } from './types'
+import { Entity, Prediction } from './types'
 
-export { Entity, NEUTRAL_ENTITY, PredictionProcessor, Processor }
+export { Entity, NEUTRAL_ENTITY, Prediction, PredictionProcessor, Processor }
