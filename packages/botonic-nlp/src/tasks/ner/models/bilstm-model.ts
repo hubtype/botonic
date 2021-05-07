91064ffdf6d7c4056235aa12116f601dc188ec61
Eric Lozano <46075558+elozano98@users.noreply.github.com>
2021-05-07 11:30:33 +0200

    refactor(nlp): ner and text classification model layers renamed (#1523)

--- packages/botonic-nlp/src/tasks/ner/models/bilstm-model.ts
+++ packages/botonic-nlp/src/tasks/ner/models/bilstm-model.ts
@@ -11,6 +11,8 @@ import {
 
 import { NerModelParameters } from './types'
 
+const MODEL_NAME = 'BiLstmNerModel'
+
 const DEFAULT_DROPOUT = 0.1
 const DEFAULT_UNITS = 128
 const DEFAULT_LEARNING_RATE = 0.001
@@ -25,10 +27,10 @@ export function createBiLstmModel(
     learningRate: DEFAULT_LEARNING_RATE,
   }
 ): LayersModel {
-  const inputs = input({ name: 'InputLayer', shape: [maxLength] })
+  const inputs = input({ name: `${MODEL_NAME}_InputLayer`, shape: [maxLength] })
 
   const embeddingsLayer = layers.embedding({
-    name: 'EmbeddingsLayer',
+    name: `${MODEL_NAME}_EmbeddingsLayer`,
     inputDim: embeddingsMatrix.shape[0] as number,
     outputDim: embeddingsMatrix.shape[1] as number,
     inputLength: maxLength,
@@ -37,12 +39,12 @@ export function createBiLstmModel(
   })
 
   const dropoutLayer = layers.dropout({
-    name: 'DropoutLayer',
+    name: `${MODEL_NAME}_DropoutLayer`,
     rate: params.dropout ?? DEFAULT_DROPOUT,
   })
 
   const bidirectionalLSTM = layers.bidirectional({
-    name: 'BidirectionalLayer',
+    name: `${MODEL_NAME}_BidirectionalLayer`,
     layer: layers.lstm({
       units: params.units ?? DEFAULT_UNITS,
       returnSequences: true,
@@ -51,7 +53,7 @@ export function createBiLstmModel(
   })
 
   const timeDistributedLayer = layers.timeDistributed({
-    name: 'TimeDistributedLayer',
+    name: `${MODEL_NAME}_TimeDistributedLayer`,
     layer: layers.dense({
       units: entities.length,
       activation: 'softmax',
@@ -62,7 +64,7 @@ export function createBiLstmModel(
     bidirectionalLSTM.apply(dropoutLayer.apply(embeddingsLayer.apply(inputs)))
   ) as SymbolicTensor
 
-  const nerModel = model({ name: 'BiLstmNerModel', inputs, outputs })
+  const nerModel = model({ name: MODEL_NAME, inputs, outputs })
   nerModel.compile({
     optimizer: train.adam(params.learningRate ?? DEFAULT_LEARNING_RATE),
     loss: 'categoricalCrossentropy',
--- packages/botonic-nlp/src/tasks/text-classification/models/constants.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_DROPOUT = 0.3
-export const DEFAULT_UNITS = 128
-export const DEFAULT_LEARNING_RATE = 0.001
