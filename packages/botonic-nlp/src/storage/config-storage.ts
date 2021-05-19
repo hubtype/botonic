38324e348126d906e74ba7282c8ae9ae046632c0
Eric Lozano <ericlozano98@gmail.com>
2021-05-19 16:03:09 +0200

    refactor(nlp): config and model storage refactored

---
+++ packages/botonic-nlp/src/storage/config-storage.ts
@@ -0,0 +1,24 @@
+import { mkdirSync, writeFileSync } from 'fs'
+import { join } from 'path'
+
+import { Locale } from '../types'
+import { readJSON } from '../utils/file-utils'
+
+export interface NlpTaskConfig {
+  locale: Locale
+  maxLength: number
+  vocabulary: string[]
+}
+
+export class ConfigStorage {
+  public readonly CONFIG_FILENAME = 'config.json'
+
+  load<T extends NlpTaskConfig>(path: string): T {
+    return readJSON(join(path, this.CONFIG_FILENAME))
+  }
+
+  save<T extends NlpTaskConfig>(path: string, config: T): void {
+    mkdirSync(path, { recursive: true })
+    writeFileSync(join(path, this.CONFIG_FILENAME), JSON.stringify(config))
+  }
+}
