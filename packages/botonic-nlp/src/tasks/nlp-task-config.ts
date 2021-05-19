9276e8b04a64809452bcfbaa008eb4d8d52a14f7
Eric Lozano <ericlozano98@gmail.com>
2021-05-19 16:29:17 +0200

    refactor(nlp): nlp-task-config removed

--- packages/botonic-nlp/src/tasks/nlp-task-config.ts
+++
@@ -1,9 +0,0 @@
-import { Preprocessor } from '../preprocess'
-import { Locale } from '../types'
-
-export interface NlpTaskConfig {
-  locale: Locale
-  maxLength: number
-  vocabulary: string[]
-  preprocessor: Preprocessor
-}
