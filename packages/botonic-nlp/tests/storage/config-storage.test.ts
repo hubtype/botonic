1ad235363f778307e8ba401a6a44560ddb5adc05
Eric Lozano <ericlozano98@gmail.com>
2021-05-19 17:03:07 +0200

    test(nlp): new ConfigStorage test created

---
+++ packages/botonic-nlp/tests/storage/config-storage.test.ts
@@ -0,0 +1,46 @@
+import { existsSync, rmdirSync } from 'fs'
+import { join } from 'path'
+
+import { ConfigStorage } from '../../src/storage/config-storage'
+import * as helper from '../helpers/constants-helper'
+
+describe('Config Storage', () => {
+  const sut = new ConfigStorage()
+
+  test('Load NER Config', () => {
+    const config = sut.load(helper.NER_MODEL_DIR_PATH)
+    expect(config).toEqual({
+      locale: helper.LOCALE,
+      maxLength: helper.MAX_SEQUENCE_LENGTH,
+      vocabulary: helper.VOCABULARY,
+      entities: ['O'].concat(helper.ENTITIES),
+    })
+  })
+
+  test('Load Intent Config', () => {
+    const config = sut.load(helper.INTENT_CLASSIFIER_MODEL_DIR_PATH)
+    expect(config).toEqual({
+      locale: helper.LOCALE,
+      maxLength: helper.MAX_SEQUENCE_LENGTH,
+      vocabulary: helper.VOCABULARY,
+      intents: helper.INTENTS,
+    })
+  })
+
+  test('Save Intent Config', () => {
+    const tmpPath = join(helper.HELPER_DIR, 'tmpConfigStorage')
+    sut.save(
+      {
+        locale: helper.LOCALE,
+        maxLength: helper.MAX_SEQUENCE_LENGTH,
+        vocabulary: helper.VOCABULARY,
+        intents: helper.INTENTS,
+      },
+      tmpPath
+    )
+    const configPath = join(tmpPath, sut.CONFIG_FILENAME)
+    expect(existsSync(configPath)).toBeTruthy()
+    rmdirSync(tmpPath, { recursive: true })
+    expect(existsSync(configPath)).toBeFalsy()
+  })
+})
