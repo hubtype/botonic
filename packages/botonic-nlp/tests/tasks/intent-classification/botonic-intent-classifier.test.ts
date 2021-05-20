d0beb5956d1eff223977acb1ac91074b1e7c4d59
Eric Lozano <ericlozano98@gmail.com>
2021-05-20 10:00:12 +0200

    test(nlp): BotonicIntentClassifier and BotonicNer tests updated

--- packages/botonic-nlp/tests/tasks/intent-classification/botonic-intent-classifier.test.ts
+++ packages/botonic-nlp/tests/tasks/intent-classification/botonic-intent-classifier.test.ts
@@ -1,6 +1,3 @@
-import { existsSync, rmdirSync } from 'fs'
-import { join } from 'path'
-
 import { DatabaseStorage } from '../../../src/embeddings/database/storage'
 import { BotonicIntentClassifier } from '../../../src/tasks/intent-classification/botonic-intent-classifier'
 import { INTENT_CLASSIFIER_TEMPLATE } from '../../../src/tasks/intent-classification/models/types'
@@ -10,13 +7,15 @@ import * as toolsHelper from '../../helpers/tools-helper'
 describe('Botonic Intent Classifier', () => {
   const { trainSet, testSet } = toolsHelper.dataset.split()
   const vocabulary = trainSet.extractVocabulary(toolsHelper.preprocessor)
-  const sut = new BotonicIntentClassifier({
-    locale: constantsHelper.LOCALE,
-    maxLength: constantsHelper.MAX_SEQUENCE_LENGTH,
-    vocabulary: vocabulary,
-    intents: constantsHelper.INTENTS,
-    preprocessor: toolsHelper.preprocessor,
-  })
+  const sut = new BotonicIntentClassifier(
+    {
+      locale: constantsHelper.LOCALE,
+      maxLength: constantsHelper.MAX_SEQUENCE_LENGTH,
+      vocabulary: vocabulary,
+      intents: constantsHelper.INTENTS,
+    },
+    toolsHelper.preprocessor
+  )
 
   test('Train and Evaluate model', async () => {
     const model = await sut.createModel(
@@ -29,28 +28,22 @@ describe('Botonic Intent Classifier', () => {
     )
     sut.setModel(model)
     await sut.train(trainSet, 4, 8)
+
     const { accuracy, loss } = await sut.evaluate(testSet)
+
     expect(accuracy).toBeGreaterThan(0.01)
     expect(loss).toBeLessThan(2)
   })
 
-  test('Save Model', async () => {
-    const model = await sut.createModel(
-      INTENT_CLASSIFIER_TEMPLATE.SIMPLE_NN,
-      await DatabaseStorage.with(
-        constantsHelper.LOCALE,
-        constantsHelper.EMBEDDINGS_TYPE,
-        constantsHelper.EMBEDDINGS_DIMENSION
-      )
+  test('Classify Intent', async () => {
+    const sut = await BotonicIntentClassifier.load(
+      constantsHelper.INTENT_CLASSIFIER_MODEL_DIR_PATH,
+      toolsHelper.preprocessor
     )
-    sut.setModel(model)
-    const path = join(
-      constantsHelper.HELPER_DIR,
-      'tmp-botonic-intent-classifier'
-    )
-    await sut.saveModel(path)
-    expect(existsSync(path)).toBeTruthy()
-    rmdirSync(path, { recursive: true })
-    expect(existsSync(path)).toBeFalsy()
+
+    const intents = sut.classify('I want to return this jacket')
+
+    expect(intents.length).toEqual(constantsHelper.INTENTS.length)
+    expect(intents[0].label).toEqual('ReturnProduct')
   })
 })
--- packages/botonic-nlp/tests/tasks/ner/botonic-ner.test.ts
+++ packages/botonic-nlp/tests/tasks/ner/botonic-ner.test.ts
@@ -1,21 +1,22 @@
 import { BotonicNer } from '../../../src/tasks/ner/botonic-ner'
 import { NER_TEMPLATE } from '../../../src/tasks/ner/models/types'
-import { NEUTRAL_ENTITY } from '../../../src/tasks/ner/process/constants'
 import * as constantsHelper from '../../helpers/constants-helper'
 import * as toolsHelper from '../../helpers/tools-helper'
 
 describe('Botonic NER', () => {
   const { trainSet, testSet } = toolsHelper.dataset.split()
   const vocabulary = trainSet.extractVocabulary(toolsHelper.preprocessor)
-  const sut = new BotonicNer({
-    locale: constantsHelper.LOCALE,
-    maxLength: constantsHelper.MAX_SEQUENCE_LENGTH,
-    vocabulary: vocabulary,
-    entities: constantsHelper.ENTITIES,
-    preprocessor: toolsHelper.preprocessor,
-  })
+  const sut = new BotonicNer(
+    {
+      locale: constantsHelper.LOCALE,
+      maxLength: constantsHelper.MAX_SEQUENCE_LENGTH,
+      vocabulary: vocabulary,
+      entities: constantsHelper.ENTITIES,
+    },
+    toolsHelper.preprocessor
+  )
 
-  test('Evaluate model', async () => {
+  test('Train and Evaluate model', async () => {
     // arrange
     const model = await sut.createModel(
       NER_TEMPLATE.BILSTM,
@@ -46,18 +47,5 @@ describe('Botonic NER', () => {
     expect(entities.length).toEqual(1)
     expect(entities.map(e => e.label)).toEqual(['product'])
     expect(entities.map(e => e.text)).toEqual(['jacket'])
-  })
-
-  test('Loading model', async () => {
-    const sut = await BotonicNer.load(
-      constantsHelper.NER_MODEL_DIR_PATH,
-      toolsHelper.preprocessor
-    )
-    expect(sut.locale).toEqual(constantsHelper.LOCALE)
-    expect(sut.maxLength).toEqual(constantsHelper.MAX_SEQUENCE_LENGTH)
-    expect(sut.entities).toEqual(
-      [NEUTRAL_ENTITY].concat(constantsHelper.ENTITIES)
-    )
-    expect(sut.vocabulary).toEqual(constantsHelper.VOCABULARY)
   })
 })
