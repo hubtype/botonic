e4865727626cfb07c33945ea4970a7fca40f4cac
Eric Lozano <ericlozano98@gmail.com>
2021-05-21 10:55:33 +0200

    refactor(plugin-intent-classification): fetch comment updated

--- packages/botonic-plugin-intent-classification/src/model/model-info.ts
+++ packages/botonic-plugin-intent-classification/src/model/model-info.ts
@@ -13,7 +13,7 @@ export class ModelInfo {
   constructor(readonly locale: Locale, readonly uri: string) {
     this.config = axios({ url: `${this.uri}/${CONFIG_FILENAME}` })
     this.model = loadLayersModel(`${this.uri}/${MODEL_FILENAME}`, {
-      fetchFunc: fetch, // fetchFunc is defined to replace window.fetch to work in node environment
+      fetchFunc: fetch, // replace window.fetch to work in node environment
     })
   }
 
