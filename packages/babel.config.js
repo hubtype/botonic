7b048d5fd1ff36249ac637b6e6fb17b7270b5d52
Dani Pinyol <github@dpinol.com>
2021-06-02 11:04:44 +0200

    chore: upgrade deprecated babel plugin
    babel-plugin-add-module-exports->@babel/plugin-transform-modules-commonjs

--- packages/babel.config.js
+++ packages/babel.config.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [
     require('@babel/plugin-proposal-object-rest-spread'),
     require('@babel/plugin-proposal-class-properties'),
-    require('babel-plugin-add-module-exports'),
+    require('@babel/plugin-transform-modules-commonjs'),
     require('@babel/plugin-transform-runtime')
   ]
 }
