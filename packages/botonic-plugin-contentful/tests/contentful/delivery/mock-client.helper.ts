6be857e7e933da86fed5e61f3e328f5af7b95b78
Dani Pinyol <github@dpinol.com>
2021-06-14 16:52:50 +0200

    feat(contentful): if memoization fails, don't cache the error
    It's better to return the previous sucessful result than
    the new error

---
+++ packages/botonic-plugin-contentful/tests/contentful/delivery/mock-client.helper.ts
@@ -0,0 +1,68 @@
+import contentful, {
+  Asset,
+  AssetCollection,
+  ContentType,
+  Entry,
+  EntryCollection,
+} from 'contentful'
+
+import { ReducedClientApi } from '../../../src/contentful/delivery/client-api'
+
+// not using mockito because (maybe due to https://github.com/medikoo/memoizee/issues/117),
+// consecutive calls without a sleep in between were not hitting the cache
+export class MockClientApi implements ReducedClientApi {
+  error: Error | undefined
+  numCalls = 0
+
+  asset = ({ ka: 'va' } as any) as Asset
+  assetCollection = ({
+    items: [this.asset],
+  } as any) as AssetCollection
+
+  contentType = ({ kct: 'vct' } as any) as ContentType
+
+  entry = ({ ke: 've' } as any) as Entry<any>
+  entryCollection = ({
+    items: [],
+  } as any) as EntryCollection<any>
+
+  getAsset(id: string, query: any): Promise<contentful.Asset> {
+    this.numCalls++
+    if (this.error) {
+      return Promise.reject(this.error)
+    }
+    return Promise.resolve(this.asset)
+  }
+
+  getAssets(query: any): Promise<contentful.AssetCollection> {
+    this.numCalls++
+    if (this.error) {
+      return Promise.reject(this.error)
+    }
+    return Promise.resolve(this.assetCollection)
+  }
+
+  getContentType(id: string): Promise<contentful.ContentType> {
+    this.numCalls++
+    if (this.error) {
+      return Promise.reject(this.error)
+    }
+    return Promise.resolve(this.contentType)
+  }
+
+  getEntries<T>(query: any): Promise<EntryCollection<T>> {
+    this.numCalls++
+    if (this.error) {
+      return Promise.reject(this.error)
+    }
+    return Promise.resolve(this.entryCollection)
+  }
+
+  getEntry<T>(id: string, query: any): Promise<Entry<T>> {
+    this.numCalls++
+    if (this.error) {
+      return Promise.reject(this.error)
+    }
+    return Promise.resolve(this.entry)
+  }
+}
