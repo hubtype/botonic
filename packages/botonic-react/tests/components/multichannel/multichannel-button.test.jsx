bb2f97b2bbbc6bd73de65d18e6f7f356e99ea7c0
Adrià Sastre <adria.sastre@gmail.com>
2021-07-06 15:27:35 +0200

    test(react): add whatsapp buttons and webviews tests in multichannel button and multichannel text components

--- packages/botonic-react/tests/components/multichannel/multichannel-button.test.jsx
+++ packages/botonic-react/tests/components/multichannel/multichannel-button.test.jsx
@@ -13,6 +13,9 @@ describe('Multichannel buttons:', () => {
   Buttons.withUrl = (
     <Button url='https://docs.botonic.io/'>button text with URL</Button>
   )
+  Buttons.withWebview = (
+    <Button webview='webview'>button text with Webview</Button>
+  )
   test('with payload', () => {
     const sut = (
       <MultichannelButton {...Buttons.withPayload.props}>
@@ -39,6 +42,26 @@ describe('Multichannel buttons:', () => {
     const sut = (
       <MultichannelButton {...Buttons.withUrl.props}>
         {Buttons.withUrl.props.children}
+      </MultichannelButton>
+    )
+    const rendered = whatsappRenderer(sut).toJSON()
+    expect(rendered).toMatchSnapshot()
+  })
+
+  test('with webview', () => {
+    const sut = (
+      <MultichannelButton {...Buttons.withWebview.props}>
+        {Buttons.withWebview.props.children}
+      </MultichannelButton>
+    )
+    const rendered = whatsappRenderer(sut).toJSON()
+    expect(rendered).toMatchSnapshot()
+  })
+
+  test('truncate text if more than 20 chars in whatsapp buttons', () => {
+    const sut = (
+      <MultichannelButton asText={false} {...Buttons.withPayload.props}>
+        {Buttons.withPayload.props.children}
       </MultichannelButton>
     )
     const rendered = whatsappRenderer(sut).toJSON()
