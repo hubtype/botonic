a0548a7d814c3b1cb419a59d317ca04c8816d376
Adrià Sastre <adria.sastre@gmail.com>
2021-07-12 17:34:26 +0200

    feat(api): refactor localDevDataProvider to use JsonDB to store date locally in a JSON file + add getUsers and getEvents functions

--- packages/botonic-api/src/data-provider/local-data-provider.ts
+++ packages/botonic-api/src/data-provider/local-data-provider.ts
@@ -1,55 +1,155 @@
 import { BotonicEvent } from '@botonic/core/src/models/events'
 import { User } from '@botonic/core/src/models/user'
+import { JsonDB } from 'node-json-db'
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
 
 import { DataProvider } from '.'
 
 export class LocalDevDataProvider implements DataProvider {
-  users: any
-  events: BotonicEvent[]
-  connections: {}
+  private readonly DB_PATH = 'tmp/localDb'
+  private readonly SEPARATOR = '/'
+  private readonly paths = {
+    USERS: '/users',
+    EVENTS: '/events',
+    CONNECTIONS: '/connections',
+  }
+  db: JsonDB
 
   constructor() {
-    this.users = []
-    this.events = []
-    this.connections = []
+    this.db = new JsonDB(new Config(this.DB_PATH, true, false, this.SEPARATOR))
+  }
+
+  getUsers(limit = 10, offset = 0): User[] {
+    const path = this.paths.USERS
+    try {
+      const userList = this.db.getObject<Record<string, User>>(path)
+      const users = Object.values(userList)
+      const from = offset * limit
+      const to = limit !== 0 ? from + limit : undefined
+      return users.slice(from, to)
+    } catch (e) {
+      console.error('Error fetching users from local DB', e)
+    }
+    return []
   }
 
   getUser(userId: string): User | undefined {
-    const filteredUser = this.users.filter(u => u.id === userId)
-    if (filteredUser.length === 0) return undefined
-    return filteredUser[0]
+    const path = this.createPath([this.paths.USERS, userId])
+    try {
+      return this.db.exists(path) ? this.db.getObject<User>(path) : undefined
+    } catch (e) {
+      console.error(`Error fetching user with ID '${userId}' from local DB`, e)
+    }
+    return undefined
   }
 
   saveUser(user: User): User {
-    this.users.push(user)
+    const path = this.createPath([this.paths.USERS, user.id])
+    try {
+      this.db.push(path, user, false)
+    } catch (e) {
+      console.error(`Error saving user with ID '${user.id}' to local DB`, e)
+    }
     return user
   }
 
   updateUser(user: User): User {
-    this.users = this.users.map(u => (u.id === user.id ? user : u))
+    const path = this.createPath([this.paths.USERS, user.id])
+    try {
+      this.db.push(path, user, true)
+    } catch (e) {
+      console.error(`Error updating user with ID '${user.id}' to local DB`, e)
+    }
     return user
   }
 
-  getEvent(id: string) {
-    return this.events.find(e => e.userId === id)
+  getEvents(limit = 10, offset = 0): BotonicEvent[] {
+    const path = this.paths.EVENTS
+    try {
+      const eventList = this.db.getObject<Record<string, BotonicEvent>>(path)
+      const events = Object.values(eventList)
+      const from = offset * limit
+      const to = limit !== 0 ? from + limit : undefined
+      return events.slice(from, to)
+    } catch (e) {
+      console.error('Error fetching events from local DB', e)
+    }
+    return []
+  }
+
+  getEvent(id: string): BotonicEvent | undefined {
+    const path = this.createPath([this.paths.EVENTS, id])
+    try {
+      return this.db.exists(path)
+        ? this.db.getObject<BotonicEvent>(path)
+        : undefined
+    } catch (e) {
+      console.error(`Error fetching event with ID '${id}' from local DB`, e)
+    }
+    return undefined
   }
 
   saveEvent(event: BotonicEvent): BotonicEvent {
-    this.events.push(event)
+    const path = this.createPath([this.paths.EVENTS, event.eventId])
+    try {
+      this.db.push(path, event, false)
+    } catch (e) {
+      console.error(
+        `Error saving event with user ID '${event.userId}' to local DB`,
+        e
+      )
+    }
     return event
   }
 
   addConnection(websocketId: string) {
-    this.connections[`${websocketId}`] = ''
+    const path = this.createPath([this.paths.CONNECTIONS, websocketId])
+    try {
+      this.db.push(path, '', false)
+    } catch (e) {
+      console.error(
+        `Error adding connection with ID '${websocketId}' to local DB`,
+        e
+      )
+    }
   }
   updateConnection(websocketId: string, userId: string) {
-    this.connections[`${websocketId}`] = userId
+    const path = this.createPath([this.paths.CONNECTIONS, websocketId])
+    try {
+      this.db.push(path, userId, true)
+    } catch (e) {
+      console.error(
+        `Error updating connection with ID '${websocketId}' to local DB`,
+        e
+      )
+    }
   }
   deleteConnection(websocketId: string) {
-    delete this.connections[`${websocketId}`]
+    const path = this.createPath([this.paths.CONNECTIONS, websocketId])
+    try {
+      this.db.delete(path)
+    } catch (e) {
+      console.error(
+        `Error deleting connection with ID '${websocketId}' to local DB`,
+        e
+      )
+    }
   }
 
-  getUserByWebsocketId(websocketId: string) {
-    return this.users.find(u => u.websocketId === websocketId)
+  getUserByWebsocketId(websocketId: string): User | undefined {
+    const path = this.paths.USERS
+    try {
+      this.db.find<User>(path, (user: User) => user.websocketId === websocketId)
+    } catch (e) {
+      console.error(
+        `Error fetching user by websocket ID with ID '${websocketId}' from local DB`,
+        e
+      )
+    }
+    return undefined
+  }
+
+  private createPath(urlChunks: string[]): string {
+    return urlChunks.join(this.SEPARATOR)
   }
 }
