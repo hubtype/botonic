4971ba2d0f3f922897cce24df2e70722034bd771
Adrià Sastre <adria.sastre@gmail.com>
2021-07-21 13:56:08 +0200

    feat(api): store user and events in bot-input endpoint + separate bot-input to independent file

---
+++ packages/botonic-api/src/rest/routes/bot-input.ts
@@ -0,0 +1,77 @@
+import { Session } from '@botonic/core'
+import { BotonicOutputParser } from '@botonic/core/lib'
+import { MessageEventFrom } from '@botonic/core/lib/models/events/message'
+import { BotonicEvent } from '@botonic/core/src/models/events'
+import { MessageEventAck } from '@botonic/core/src/models/events/message'
+import { User } from '@botonic/core/src/models/user'
+import { NodeApp } from '@botonic/react'
+import { Request, Router } from 'express'
+import { ulid } from 'ulid'
+import { v4 } from 'uuid'
+
+import { dataProviderFactory } from '../../data-provider'
+
+const dp = dataProviderFactory(process.env.DATA_PROVIDER_URL)
+
+export default function botInputRouter(bot: NodeApp): Router {
+  const router = Router()
+  const botonicOutputParser = new BotonicOutputParser()
+
+  router.route('/').post(async (req, res) => {
+    // TODO: parse: Boolean arg to indicate if we should parse the output or not (default true)
+    const user =
+      (await getUser(req)) ??
+      (await createUser(req.body.session.user.provider_id))
+
+    const output = await bot.input({
+      input: req.body.input,
+      session: { user },
+      lastRoutePath: user.route,
+    })
+
+    const parsedUserEvent = botonicOutputParser.parseFromUserInput(
+      req.body.input
+    )
+    await dp.saveEvent({
+      ...parsedUserEvent,
+      userId: user.id,
+      eventId: ulid(),
+      createdAt: new Date().toISOString(),
+      from: MessageEventFrom.USER,
+      ack: MessageEventAck.SENT,
+    } as BotonicEvent)
+
+    const messages = output.parsedResponse
+    for (const messageEvent of messages) {
+      await dp.saveEvent({
+        ...messageEvent,
+        userId: user.id,
+        eventId: ulid(),
+        createdAt: new Date().toISOString(),
+        from: MessageEventFrom.BOT,
+        ack: MessageEventAck.SENT,
+      })
+    }
+    res.json(output)
+  })
+
+  return router
+}
+
+async function getUser(req: Request): Promise<User | undefined> {
+  return (
+    (await dp.getUser(req.body.session.user.id)) ??
+    (await dp.getUserByField('provider_id', req.body.session.user.provider_id))
+  )
+}
+
+async function createUser(providerId: string): Promise<User> {
+  const user: User = {
+    id: v4(),
+    providerId,
+    isOnline: true,
+    route: '/',
+    session: {} as Session,
+  }
+  return dp.saveUser(user)
+}
--- packages/botonic-api/src/rest/routes/index.ts
+++ packages/botonic-api/src/rest/routes/index.ts
@@ -1,21 +1,14 @@
+import { NodeApp } from '@botonic/react'
 import { Router } from 'express'
 
+import botInputRouter from './bot-input'
 import events from './events'
 import users from './users'
 
-export const routes = bot => {
+export const routes = (bot: NodeApp) => {
   const router = Router()
   router.use('/users', users)
   router.use('/events', events)
-  router.route('/bot-input').post(async (req, res) => {
-    // TODO: parse: Boolean arg to indicate if we should parse the output or not (default true)
-    // TODO: get session and route from DataProvider
-    const output = await bot.app.input({
-      input: req.body.input,
-      session: req.body.session,
-      lastRoutePath: req.body.route,
-    })
-    res.json(output)
-  })
+  router.use('/bot-input', botInputRouter(bot))
   return router
 }
