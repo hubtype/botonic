0060eb88d38a4098447f690e1466d275eec249d6
Marc Rabat <marcrabat@gmail.com>
2021-07-28 11:36:08 +0200

    chore(react): fix lint error

--- packages/botonic-react/src/components/multichannel/multichannel-text.jsx
+++ packages/botonic-react/src/components/multichannel/multichannel-text.jsx
@@ -194,8 +194,11 @@ export const MultichannelText = props => {
   if (isFacebook(requestContext)) {
     const text = getText(props.children)
     const multichannelFacebook = new MultichannelFacebook()
-    const { texts, propsLastText, propsWithoutChildren } =
-      multichannelFacebook.convertText(props, text[0])
+    const {
+      texts,
+      propsLastText,
+      propsWithoutChildren,
+    } = multichannelFacebook.convertText(props, text[0])
     return (
       <>
         {texts &&
