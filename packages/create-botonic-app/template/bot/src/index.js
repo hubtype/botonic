be0738233d39180ecb78800244b1e98bfad56378
Adrià Sastre <adria.sastre@gmail.com>
2021-07-28 11:45:14 +0200

    refactor(create-botonic-app): Export only the NodeApp in bot/src/index.js (#1731)
    * feat: add new package create-botonic-app
    
    * refactor(create-botonic-app): export only the NodeApp in bot/index.js
    
    * chore(react): fix lint error
    
    * chore(create-botonic-app): remove duplicated action
    
    Co-authored-by: Eric Marcos <ericmarcos.p@gmail.com>
    Co-authored-by: Marc Rabat <marcrabat@gmail.com>

--- packages/create-botonic-app/template/bot/src/index.js
+++ packages/create-botonic-app/template/bot/src/index.js
@@ -1,7 +1,8 @@
 import { NodeApp } from '@botonic/react'
-import { routes } from './routes'
-import { plugins } from './plugins'
-import { locales } from './locales'
 
-export const config = { defaultDelay: 0, defaultTyping: 0 }
-export let app = new NodeApp({ routes, locales, plugins, ...config })
+import { locales } from './locales'
+import { plugins } from './plugins'
+import { routes } from './routes'
+
+const config = { defaultDelay: 0, defaultTyping: 0 }
+export const app = new NodeApp({ routes, locales, plugins, ...config })
