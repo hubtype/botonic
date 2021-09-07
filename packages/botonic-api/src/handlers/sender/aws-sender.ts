3b47c2d600350534ab6597e92c60d6392285e62e
Marc Rabat <marcrabat@gmail.com>
2021-09-07 17:59:15 +0200

    chore(api): fix typo

--- packages/botonic-api/src/handlers/sender/aws-sender.ts
+++ packages/botonic-api/src/handlers/sender/aws-sender.ts
@@ -2,14 +2,14 @@ import { ApiGatewayManagementApi } from 'aws-sdk'
 
 import { WSS_PROTOCOL_PREFIX } from '../..'
 
-const apigwManagementApi = new ApiGatewayManagementApi({
+const apiGwManagementApi = new ApiGatewayManagementApi({
   apiVersion: '2018-11-29',
   endpoint: process.env.WEBSOCKET_URL.split(WSS_PROTOCOL_PREFIX)[1],
 })
 
 export async function awsSender({ messages, websocketId }) {
   for (const message of messages) {
-    await apigwManagementApi
+    await apiGwManagementApi
       .postToConnection({
         ConnectionId: websocketId,
         Data: JSON.stringify(message),
