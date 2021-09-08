579d923340ecbbc219b43be14af5600f0cda362a
Marc Rabat <marcrabat@gmail.com>
2021-09-08 13:12:40 +0200

    chore(api): access environment var safely and bump dev version

--- packages/botonic-api/src/handlers/sender/aws-sender.ts
+++ packages/botonic-api/src/handlers/sender/aws-sender.ts
@@ -4,7 +4,7 @@ import { WSS_PROTOCOL_PREFIX } from '../..'
 
 const apiGwManagementApi = new ApiGatewayManagementApi({
   apiVersion: '2018-11-29',
-  endpoint: process.env.WEBSOCKET_URL.split(WSS_PROTOCOL_PREFIX)[1],
+  endpoint: process.env.WEBSOCKET_URL?.split(WSS_PROTOCOL_PREFIX)[1],
 })
 
 export async function awsSender({ messages, websocketId }) {
