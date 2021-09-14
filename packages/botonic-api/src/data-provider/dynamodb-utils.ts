087e86becb50c9b9a9071bac98a61efa1f27008a
Marc Rabat <marcrabat@gmail.com>
2021-09-14 18:03:41 +0200

    chore(api): protect capitalize method from null results

--- packages/botonic-api/src/data-provider/dynamodb-utils.ts
+++ packages/botonic-api/src/data-provider/dynamodb-utils.ts
@@ -14,6 +14,7 @@ export const USER_PREFIX = 'USR#'
 export const EVENT_PREFIX = 'EVT#'
 
 function capitalize(string: string) {
+  if (!string) return ''
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
