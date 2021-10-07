3392d249dc149e45cf3a80360c455bce99037b97
Albert <albert.gomariz63@gmail.com>
2021-10-07 18:09:41 +0200

    test(directus): add button target carousel test

--- packages/updated-directus/tests/directus/contents/button.test.ts
+++ packages/updated-directus/tests/directus/contents/button.test.ts
@@ -4,7 +4,7 @@ const BUTTON_TEXT_TARGET = 'd467a152-a746-4ed0-9e6f-d83a6d0c3a5b'
 const BUTTON_PAYLOAD_TARGET = '09f88f45-c717-449b-b979-51308e779744'
 const BUTTON_IMAGE_TARGET = 'ff8b23c4-872f-4b81-bb6a-d30d4d044a7b'
 const BUTTON_URL_TARGET = 'eefd1c61-0881-4bf7-b863-94e6bb1a52ed'
-
+const BUTTON_CAROUSEL_TARGET = 'b4aca626-b988-40de-a26f-5bac3005b3c4'
 test('Test: directus button with Text target', async () => {
   const directus = testDirectus()
   const testButton = await directus.button(BUTTON_TEXT_TARGET, testContext())
@@ -33,4 +33,15 @@ test('Test: directus button with url target', async () => {
   const directus = testDirectus()
   const testButton = await directus.button(BUTTON_URL_TARGET, testContext())
   expect(testButton.callback.url).toEqual('www.google.com')
+})
+
+test('Test: directus button with carousel target', async () => {
+  const directus = testDirectus()
+  const testButton = await directus.button(
+    BUTTON_CAROUSEL_TARGET,
+    testContext()
+  )
+  expect(testButton.callback.payload).toEqual(
+    'carousel$3c2bfdb6-8e2f-43f8-9173-32956c11c48d'
+  )
 })
