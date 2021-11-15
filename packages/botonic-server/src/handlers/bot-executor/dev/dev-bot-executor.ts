a37520c37eecb236194d8789f5e95d8b075af644
Marc Rabat <marcrabat@gmail.com>
2021-11-15 14:57:10 +0100

    chore: add annotations for future changes, and minor fixes

--- packages/botonic-server/src/handlers/bot-executor/dev/dev-bot-executor.ts
+++ packages/botonic-server/src/handlers/bot-executor/dev/dev-bot-executor.ts
@@ -1,5 +1,11 @@
 import { botExecutor } from '../bot-executor'
 
+const eventHandlers = {
+  onReceivedMessage: () => {},
+  onBotExecuted: () => {},
+  onBotAction: () => {},
+}
+
 export function devBotExecutor(bot, dataProvider, dispatchers) {
-  return botExecutor(bot, dataProvider, dispatchers)
+  return botExecutor(bot, dataProvider, dispatchers, eventHandlers)
 }
