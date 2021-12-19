6390baefd2b8b73a704cecc40a853f147469e79d
Marc Rabat <marcrabat@gmail.com>
2021-12-19 14:39:34 +0100

    chore(react): remove share and full-screen

--- packages/botonic-react/src/components/image.jsx
+++ packages/botonic-react/src/components/image.jsx
@@ -1,13 +1,14 @@
 import 'react-photoswipe/lib/photoswipe.css'
 
 import { INPUT, isBrowser } from '@botonic/core'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { PhotoSwipe } from 'react-photoswipe'
 import styled from 'styled-components'
 
 import { ROLES } from '../constants'
 import { Portal } from '../webchat/components/portal-modal'
 import { Message } from './message'
+import { getMeta } from './size-checker'
 
 const StyledImage = styled.img`
   border-radius: 8px;
@@ -23,7 +24,33 @@ const serialize = imageProps => {
 
 export const Image = props => {
   let content = props.children
+
   const [isGalleryOpened, setIsGalleryOpened] = useState(false)
+  const [imageDimensions, setImageDimensions] = useState({})
+  useEffect(() => {
+    // Remove fullscreen and share buttons
+    if (isGalleryOpened) {
+      const fullScreenButton = document.getElementsByClassName(
+        'pswp__button pswp__button--fs'
+      )[0]
+      fullScreenButton.remove()
+      const shareButton = document.getElementsByClassName(
+        'pswp__button pswp__button--share'
+      )[0]
+      shareButton.remove()
+    }
+  }, [isGalleryOpened])
+
+  useEffect(async () => {
+    if (props.src !== undefined) {
+      let dimensions = await getMeta(props.src)
+      if (dimensions.width <= 1200 || dimensions.height <= 1200) {
+        dimensions.width = 2 * dimensions.width
+        dimensions.height = 2 * dimensions.height
+      }
+      setImageDimensions(dimensions)
+    }
+  }, [props.src])
 
   if (isBrowser()) {
     content = (
@@ -33,7 +60,13 @@ export const Image = props => {
           <Portal>
             <PhotoSwipe
               isOpen={true}
-              items={[{ src: props.src, w: 1200, h: 900 }]}
+              items={[
+                {
+                  src: props.src,
+                  w: imageDimensions.width,
+                  h: imageDimensions.height,
+                },
+              ]}
               options={{ bgOpacity: 0.7 }}
               onClose={() => setIsGalleryOpened(false)}
             />
