d136a89195b00c627b1ee9ad65ac5a963d9e6311
Marc Rabat <marcrabat@gmail.com>
2021-12-22 12:55:56 +0100

    chore(core): add types and clarifications to decompression algorithm

--- packages/botonic-core/src/pusher-utils.ts
+++ packages/botonic-core/src/pusher-utils.ts
@@ -4,13 +4,13 @@ import { inflate } from 'pako/dist/pako_inflate.js'
  *
  * @param compressedData a string representing gzipped data previously encoded to base64 for very large contents
  * @returns a string representing the information of a very large content
- * Ref: https://stackoverflow.com/questions/4875020/javascript-decompress-inflate-unzip-ungzip-strings
+ * Ref: https://stackoverflow.com/questions/4875020/javascript-decompress-inflate-unzip-ungzip-strings (check jsfiddle proposal)
  */
-export function decompressData(compressedData) {
-  const strData = atob(compressedData)
-  const charData = strData.split('').map(x => x.charCodeAt(0))
-  const binData = new Uint8Array(charData)
+export function decompressData(compressedData: string): string {
+  const strData = atob(compressedData) // Decode base64 (convert ascii to binary)
+  const charData = strData.split('').map(x => x.charCodeAt(0)) // Convert binary string to character-number array
+  const binData = new Uint8Array(charData) // Turn number array into byte-array
   const data = inflate(binData)
   // @ts-ignore
-  return String.fromCharCode.apply(null, new Uint8Array(data))
+  return String.fromCharCode.apply(null, new Uint8Array(data)) // Convert gunzipped byteArray back to ascii string
 }
