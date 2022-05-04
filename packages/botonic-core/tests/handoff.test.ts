a0531e8a0a883ac9ec5c8b91471a154d9387c9c2
Adrià Sastre <adria.sastre@gmail.com>
2022-05-04 10:18:46 +0200

    feat(core): Handoff improvements (autoAssignOnWaiting and forceAssignIfNotAvailable) (#2231)
    * feat(core): add forceAssignIfNotAvailable in HandoffBuilder to be able to not assign the case to the agent if it's not available
    
    * feat(core): add autoAssignOnWaiting in HandoffBuilder to be able to auto assign a case in waiting to an agent
    
    * feat(core): set true as a default value for forceAssignIfNotAvailable

--- packages/botonic-core/tests/handoff.test.ts
+++ packages/botonic-core/tests/handoff.test.ts
@@ -6,6 +6,7 @@ describe('Handoff', () => {
   test.each([
     [
       `create_case:{
+        "force_assign_if_not_available":true,
         "queue":"q1",
         "on_finish":"payload1"
        }`,
@@ -14,6 +15,7 @@ describe('Handoff', () => {
     ],
     [
       `create_case:{
+        "force_assign_if_not_available":true,
         "on_finish":"${PATH_PAYLOAD_IDENTIFIER}path1"
        }`,
       '',
@@ -29,6 +31,7 @@ describe('Handoff', () => {
     [
       `create_case:` +
         JSON.stringify({
+          force_assign_if_not_available: true,
           queue: 'q1',
           agent_email: 'email1',
           case_info: '{}{:::: m"ho menjo tot}',
@@ -45,6 +48,7 @@ describe('Handoff', () => {
     [
       `create_case:` +
         JSON.stringify({
+          force_assign_if_not_available: true,
           on_finish: `${PATH_PAYLOAD_IDENTIFIER}path1`,
         }),
       new HandOffBuilder({}).withOnFinishPath('path1'),
@@ -52,6 +56,7 @@ describe('Handoff', () => {
     [
       `create_case:` +
         JSON.stringify({
+          force_assign_if_not_available: true,
           agent_id: '1234',
         }),
       new HandOffBuilder({}).withAgentId('1234'),
@@ -69,8 +74,42 @@ describe('Handoff', () => {
     const expectedBotonicAction =
       'create_case:' +
       JSON.stringify({
+        force_assign_if_not_available: true,
         auto_idle_message: 'the case is in IDLE status',
       })
     expect(builder._session._botonic_action).toEqual(expectedBotonicAction)
   })
+
+  test.each([undefined, true, false])(
+    'sends the force_assign_if_not_available parameter',
+    (forceAssign: boolean | undefined) => {
+      const builder =
+        forceAssign !== undefined
+          ? new HandOffBuilder({}).withForceAssignIfNotAvailable(forceAssign)
+          : new HandOffBuilder({})
+      const value = forceAssign ?? true
+      builder.handOff()
+      const expectedBotonicAction =
+        'create_case:' +
+        JSON.stringify({ force_assign_if_not_available: value })
+      expect(builder._session._botonic_action).toEqual(expectedBotonicAction)
+    }
+  )
+
+  test.each([undefined, true, false])(
+    'sends the auto_assign_on_waiting parameter',
+    (autoAssignOnWaiting: boolean | undefined) => {
+      const builder =
+        autoAssignOnWaiting !== undefined
+          ? new HandOffBuilder({}).withAutoAssignOnWaiting(autoAssignOnWaiting)
+          : new HandOffBuilder({})
+      builder.handOff()
+      const defaultParams = { force_assign_if_not_available: true }
+      const params = autoAssignOnWaiting
+        ? { ...defaultParams, auto_assign_on_waiting: true }
+        : defaultParams
+      const expectedBotonicAction = 'create_case:' + JSON.stringify(params)
+      expect(builder._session._botonic_action).toEqual(expectedBotonicAction)
+    }
+  )
 })
