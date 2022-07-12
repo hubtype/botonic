1a0eb4ed96fc022e584e5393ba603bc0dee58596
Adrià Sastre <adria.sastre@gmail.com>
2022-07-12 13:50:16 +0200

    feat(react): refactor useStorageState to enable the usage of storages differents than localStorage and sessionStorage

--- packages/botonic-react/src/webchat/use-storage-state-hook.js
+++ packages/botonic-react/src/webchat/use-storage-state-hook.js
@@ -1,9 +1,44 @@
-import { useLocalStorage, useSessionStorage } from 'react-use-storage'
+import { useCallback, useEffect, useState } from 'react'
 
-export function useStorageState(storage, storageKey) {
-  const [botonicLocalState, saveLocalState] = useLocalStorage(storageKey)
-  const [botonicSessionState, saveSessionState] = useSessionStorage(storageKey)
-  if (storage === null) return [undefined, undefined]
-  if (storage === sessionStorage) return [botonicSessionState, saveSessionState]
-  return [botonicLocalState, saveLocalState]
+//Code taken from https://github.com/leny/react-use-storage
+const evtTarget = new EventTarget()
+
+export function useStorageState(storage, key, defaultValue) {
+  const raw = storage?.getItem(key)
+
+  const [value, setValue] = useState(raw ? JSON.parse(raw) : defaultValue)
+
+  const updater = useCallback(
+    (updatedValue, remove = false) => {
+      setValue(updatedValue)
+      storage[remove ? 'removeItem' : 'setItem'](
+        key,
+        JSON.stringify(updatedValue)
+      )
+      evtTarget.dispatchEvent(
+        new CustomEvent('storage_change', { detail: { key } })
+      )
+    },
+    [key]
+  )
+
+  defaultValue != null && !raw && updater(defaultValue)
+
+  useEffect(() => {
+    const listener = ({ detail }) => {
+      if (detail.key === key) {
+        const lraw = storage?.getItem(key)
+
+        lraw !== raw && setValue(JSON.parse(lraw))
+      }
+    }
+
+    evtTarget.addEventListener('storage_change', listener)
+    return () => evtTarget.removeEventListener('storage_change', listener)
+  })
+
+  if (storage === null) {
+    return [undefined, undefined]
+  }
+  return [value, updater, () => updater(null, true)]
 }
