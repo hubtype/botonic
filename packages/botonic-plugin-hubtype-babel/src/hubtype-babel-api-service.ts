32799693444a623ce1039a6f92a70e5b10351280
Eric Lozano <ericlozano98@gmail.com>
2022-09-28 13:49:10 +0200

    host added to plugins option

--- packages/botonic-plugin-hubtype-babel/src/hubtype-babel-api-service.ts
+++ packages/botonic-plugin-hubtype-babel/src/hubtype-babel-api-service.ts
@@ -1,14 +1,15 @@
 import axios from 'axios'
 
 export class HubtypeBabelApiService {
-  private readonly HOST = 'https://api.hubtype.com'
-
-  constructor(readonly projectId: string) {}
+  constructor(
+    readonly projectId: string,
+    readonly host: string = 'https://api.hubtype.com'
+  ) {}
 
   async inference(text: string, accessToken: string) {
     return await axios({
       method: 'POST',
-      url: `${this.HOST}/v1/babel/projects/${this.projectId}/inference/`,
+      url: `${this.host}/v1/babel/projects/${this.projectId}/inference/`,
       headers: {
         Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
