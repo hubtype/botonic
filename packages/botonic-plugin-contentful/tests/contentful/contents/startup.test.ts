40aabc6ba1b9062e6828c3c9cd9c08153b09fd94
Albert <albert.gomariz63@gmail.com>
2022-10-14 11:46:14 +0200

    test(botonic-plugin-contentful): fix button related tests

--- packages/botonic-plugin-contentful/tests/contentful/contents/startup.test.ts
+++ packages/botonic-plugin-contentful/tests/contentful/contents/startup.test.ts
@@ -11,6 +11,13 @@ import { TEST_CAROUSEL_MAIN_ID } from './carousel.test'
 test('TEST: contentful startUp', async () => {
   const id = 'PfMPIeS6zD1Rix6Px9m0u'
   const startUp = await testContentful().startUp(id)
+  const startUpButton = new Button(
+    TEST_CAROUSEL_MAIN_ID,
+    'INICIO',
+    'Start menu',
+    new ContentCallback(ContentType.CAROUSEL, TEST_CAROUSEL_MAIN_ID)
+  )
+  startUpButton.customFields = { customFieldNumber: 3.14159265389 }
   expect(startUp).toEqual(
     new StartUp(
       new CommonFields(id, 'BANNER', {
@@ -21,14 +28,7 @@ test('TEST: contentful startUp', async () => {
 
       'https://images.ctfassets.net/p2iyhzd1u4a7/1T0ntgNJnDUSwz59zGMZO6/8f114c3d3fe8f541fefbec5a539dad35/red.jpg',
       'Le damos la bienvenida a su nuevo asistente virtual.',
-      [
-        new Button(
-          TEST_CAROUSEL_MAIN_ID,
-          'INICIO',
-          'Start menu',
-          new ContentCallback(ContentType.CAROUSEL, TEST_CAROUSEL_MAIN_ID)
-        ),
-      ]
+      [startUpButton]
     )
   )
 })
