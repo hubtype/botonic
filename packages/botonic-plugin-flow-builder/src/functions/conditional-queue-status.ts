cd1e7ccf82e46155c561d984cf93b244d217335e
Marc Rabat <marcrabat@gmail.com>
2023-02-22 12:23:45 +0100

    fix(plugin-flow-builder): rename queueId to queue_id

--- packages/botonic-plugin-flow-builder/src/functions/conditional-queue-status.ts
+++ packages/botonic-plugin-flow-builder/src/functions/conditional-queue-status.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export async function conditionalQueueStatus({ queueId }): Promise<string> {
+export async function conditionalQueueStatus({ queue_id }): Promise<string> {
   const response = await axios.get(
-    `https://api.hubtype.com/v1/queues/${queueId}/availability/`
+    `https://api.hubtype.com/v1/queues/${queue_id}/availability/`
   )
   const isAvailable = response.data.available
   return isAvailable ? 'open' : 'closed'
