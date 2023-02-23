ba422d704ed6c3581bc59b19f1fe03e337446e60
Oriol Raventos <o.raventos.89@gmail.com>
2023-02-23 17:48:45 +0100

    feat(plugin-flow-builder): pass getLocale function from constructor

--- packages/botonic-plugin-flow-builder/src/action.tsx
+++ packages/botonic-plugin-flow-builder/src/action.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { FlowContent } from './content-fields/content-base'
 import { doHandoff } from './handoff'
 import { HtHandoffNode } from './hubtype-models'
+import BotonicPluginFlowBuilder from './index'
 
 // TODO: remove this from here and use the new "start" attribute in the flow
 export const START = '08c7df06-0c7c-4f06-b8c1-4157582efeb2'
@@ -17,27 +18,31 @@ export default class FlowBuilderAction extends React.Component<FlowBuilderAction
   static contextType = RequestContext
 
   static async botonicInit(request: ActionRequest): Promise<any> {
-    const flowBuilderPlugin = request.plugins.hubtypeFlowBuilder as any
+    console.log(request)
+    const flowBuilderPlugin = request.plugins
+      .flowBuilder as BotonicPluginFlowBuilder
+
+    const locale = flowBuilderPlugin.getLocale(request.session)
 
     let payload = request.input.payload ? request.input.payload : START
     if (!request.input.payload) {
       const intentPayload = await flowBuilderPlugin.getPayloadByInput(
         request.input,
-        'es-ES'
+        locale
       )
       if (intentPayload) {
         payload = intentPayload
       }
       const keywordPayload = await flowBuilderPlugin.getPayloadByKeyword(
         request.input,
-        'es-ES'
+        locale
       )
       if (keywordPayload) {
         payload = keywordPayload
       }
     }
     // We use only Spanish because they are the backend examples
-    const content = await flowBuilderPlugin.getContents(payload, 'es-ES')
+    const content = await flowBuilderPlugin.getContents(payload, locale)
 
     if (content.length == 0) {
       const handoffMsg = await doHandoff(request)
