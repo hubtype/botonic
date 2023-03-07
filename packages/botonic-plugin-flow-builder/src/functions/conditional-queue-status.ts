27c57b662474d466727350be15579f91ecf2004b
Marc Rabat <marcrabat@gmail.com>
2023-03-07 19:22:01 +0100

    chore(plugin-flow-builder): fix initialization of options and read HUBTYPE_API_URL from env var

--- packages/botonic-plugin-flow-builder/src/functions/conditional-queue-status.ts
+++ packages/botonic-plugin-flow-builder/src/functions/conditional-queue-status.ts
@@ -1,9 +1,21 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import axios from 'axios'
 
-export async function conditionalQueueStatus({ queue_id }): Promise<string> {
+import { getWebpackEnvVar } from '../utils'
+
+const _HUBTYPE_API_URL_ = getWebpackEnvVar(
+  // @ts-ignore
+  typeof HUBTYPE_API_URL !== 'undefined' && HUBTYPE_API_URL,
+  'HUBTYPE_API_URL',
+  'https://api.hubtype.com'
+)
+export async function conditionalQueueStatus({
+  queue_id,
+}: {
+  queue_id: string
+}): Promise<string> {
   const response = await axios.get(
-    // `https://api.int.qa.hubtype.com/v1/queues/${queue_id}/availability/`
-    `https://api.hubtype.com/v1/queues/${queue_id}/availability/`,
+    `${_HUBTYPE_API_URL_}/v1/queues/${queue_id}/availability/`,
     // TODO: Make it configurable in the future
     {
       params: {
