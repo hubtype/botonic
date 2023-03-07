f986123ccf163c8629e6e45c6e8667e4bf45368f
Marc Rabat Pla <35448568+vanbasten17@users.noreply.github.com>
2023-03-07 10:43:20 +0100

    Release/0.21.0 (#2299)
    * chore: remove unused packages botonic-nlu and botonic-plugin-nlu
    
    * chore: bump botonic packages to 0.21.0

--- packages/botonic-nlu/src/preprocessing-tools/normalizer.ts
+++
@@ -1,7 +0,0 @@
-import { Normalizer } from '../types'
-
-export class DefaultNormalizer implements Normalizer {
-  normalize(sentence: string): string {
-    return sentence.trim().toLowerCase()
-  }
-}
