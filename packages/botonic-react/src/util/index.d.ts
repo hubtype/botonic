19b01bba992f8abedbbfb5cdbd496435e8015dbe
van Basten 17 <35448568+vanbasten17@users.noreply.github.com>
2023-05-23 08:09:14 +0200

    refactor(react): utils module to typescript (#2504)
    * refactor(react): create index for utils folder
    
    * chore(react): remove typing .d.ts files from linter

--- packages/botonic-react/src/util/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement, ReactNode } from 'react'
-
-export function staticAsset(path: string): string
-export function hasComplexChildren(
-  element: ReactNode
-): ReactElement<{ children: ReactNode[] }>
-
-export const isDev: boolean
-export const isProd: boolean
