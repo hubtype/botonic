44efd0d94201977716b0404c7998080bbd32f01e
Oriol Raventos <o.raventos.89@gmail.com>
2023-07-10 11:59:28 +0200

    replace(flow-builder): if extra_data is null set it with isFirstFallbackOption

--- packages/botonic-plugin-flow-builder/src/action/index.tsx
+++ packages/botonic-plugin-flow-builder/src/action/index.tsx
@@ -103,8 +103,11 @@ async function getTargetNode(
 }
 
 async function getFallbackNode(cmsApi: FlowBuilderApi, request: ActionRequest) {
+  if (!request.session.user.extra_data) {
+    request.session.user.extra_data = { isFirstFallbackOption: true }
+  }
   const isFirstFallbackOption =
-    request.session.user.extra_data.isFirstFallbackOption ?? true
+    request.session.user.extra_data.isFirstFallbackOption
   const fallbackNode = cmsApi.getFallbackNode(isFirstFallbackOption)
   request.session.user.extra_data.isFirstFallbackOption = !isFirstFallbackOption
 
