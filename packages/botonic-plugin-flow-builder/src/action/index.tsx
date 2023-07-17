85f464debdd213e58545f52214396db598d3d4ef
Oriol Raventos <o.raventos.89@gmail.com>
2023-07-17 17:25:47 +0200

    refactor(flow-builder): replaceVariables to be a function of FlowText

--- packages/botonic-plugin-flow-builder/src/action/index.tsx
+++ packages/botonic-plugin-flow-builder/src/action/index.tsx
@@ -7,7 +7,7 @@ import { ActionRequest, Multichannel, RequestContext } from '@botonic/react'
 import React from 'react'
 
 import { FlowBuilderApi } from '../api'
-import { SOURCE_INFO_SEPARATOR, VARIABLE_REGEX } from '../constants'
+import { SOURCE_INFO_SEPARATOR } from '../constants'
 import { FlowContent, FlowHandoff, FlowText } from '../content-fields'
 import { HtNodeWithContent } from '../content-fields/hubtype-fields'
 import { getFlowBuilderPlugin } from '../helpers'
@@ -105,18 +105,4 @@ async function getFallbackNode(cmsApi: FlowBuilderApi, request: ActionRequest) {
   }
   await trackEvent(request, event)
   return fallbackNode
-}
-
-function replaceVariables(text: string, extraData?: Record<string, any>) {
-  const matches = text.match(VARIABLE_REGEX)
-
-  if (matches && extraData) {
-    matches.forEach(match => {
-      const variable = match.slice(1, -1)
-      const value = extraData[variable] ? extraData[variable] : match
-      text = text.replace(match, value)
-    })
-  }
-
-  return text
 }
