565007f8ee0523e3666aec71da1b8397f38f6aee
Oriol Raventos <o.raventos.89@gmail.com>
2023-09-05 15:31:35 +0200

    feat(plugin-knowledge-bases): create plugin with knowledge api call

---
+++ packages/botonic-plugin-knowledge-bases/src/hubtype-knowledge-api-service.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosResponse } from 'axios'
+
+const DEFAULT_TIMEOUT = 10000
+
+export class HubtypeApiService {
+  private knowledgeBaseId: string
+  private host: string
+  private timeout: number
+
+  constructor(knowledgeBaseId: string, host: string, timeout?: number) {
+    this.knowledgeBaseId = knowledgeBaseId
+    this.host = host
+    this.timeout = timeout || DEFAULT_TIMEOUT
+  }
+
+  async inference(
+    authToken: string,
+    chatId: string
+  ): Promise<
+    AxiosResponse<{
+      ai: string
+      has_knowledge: boolean
+      sources: string[]
+    }>
+  > {
+    return await axios({
+      method: 'POST',
+      url: `${this.host}v1/ai/knowledge_bases/${this.knowledgeBaseId}/inference/`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+      data: { chat_id: chatId },
+      timeout: this.timeout,
+    })
+  }
+}
---
+++ packages/botonic-plugin-knowledge-bases/src/types.ts
@@ -0,0 +1,12 @@
+export interface PluginKnowledgeBaseOptions {
+  knowledgeBaseId: string
+  host: string
+  authToken?: string
+  timeout?: number
+}
+
+export interface KnowledgebaseResponse {
+  ai: string
+  hasKnowledge: boolean
+  sources: string[]
+}
