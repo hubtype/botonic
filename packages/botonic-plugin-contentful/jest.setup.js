59cf00cfaa8df2910c6b459cb58cd678be6d01e7
CarlosELorenzo <ce.lorenzol@gmail.com>
2023-09-08 12:17:13 -0400

    chore(plugin-contentful): increase jest timeout

--- packages/botonic-plugin-contentful/jest.setup.js
+++ packages/botonic-plugin-contentful/jest.setup.js
@@ -2,6 +2,6 @@
 // contention on the other tests (due to the sleep while retrying?)
 // That's why we need such a large default timeout
 // In the future we should run independently integration and unit tests
-jest.setTimeout(15000)
+jest.setTimeout(30000)
 // uncomment to extend jest test timeout while debugging from IDE
 // jest.setTimeout(3000000)
