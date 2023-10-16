d3e5af84c5174efa5f477a4f5f4988ec8ea2e9f8
AinaVendrell <ainavendrell99@gmail.com>
2023-10-16 10:24:58 +0200

    feat: increase the limit of contents in topContents method

--- packages/botonic-plugin-contentful/src/contentful/contents/contents.ts
+++ packages/botonic-plugin-contentful/src/contentful/contents/contents.ts
@@ -43,6 +43,12 @@ export class ContentsDelivery extends ResourceDelivery {
     const entryCollection: EntryCollection<CommonEntryFields> =
       await this.delivery.getEntries(context, this.query(model, paging))
     let entries = entryCollection.items
+    if (entryCollection.total > paging.limit) {
+      const paging2 = new PagingOptions(1000, 1000)
+      const entryCollection2: EntryCollection<CommonEntryFields> =
+        await this.delivery.getEntries(context, this.query(model, paging2))
+      entries.push(...entryCollection2.items)
+    }
     if (filter) {
       entries = entries.filter(entry =>
         filter(ContentfulEntryUtils.commonFieldsFromEntry(entry))
