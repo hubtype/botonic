e83b891af20ed05625b5593248ab25c44f3670c9
Oriol Raventos <o.raventos.89@gmail.com>
2024-02-09 16:31:34 +0100

    chore(botonic): remove botonic-intent-classification package

--- packages/botonic-plugin-intent-classification/tests/helper/tools-helper.ts
+++
@@ -1,5 +0,0 @@
-import { Preprocessor } from '@botonic/nlp/lib/preprocess'
-
-import { LOCALE, MAX_SEQUENCE_LENGTH } from './constants-helper'
-
-export const preprocessor = new Preprocessor(LOCALE, MAX_SEQUENCE_LENGTH)
