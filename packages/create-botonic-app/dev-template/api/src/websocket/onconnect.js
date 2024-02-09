402c82e8d57fcf97e475d4e92cbcde0da2ca2730
Oriol Raventos <o.raventos.89@gmail.com>
2024-02-09 16:41:18 +0100

    chore(botonic): remove create-botonic-app package

--- packages/create-botonic-app/dev-template/api/src/websocket/onconnect.js
+++
@@ -1,3 +0,0 @@
-export const onConnect = async websocketId => {
-  console.log('Connected Successfully')
-}
--- packages/create-botonic-app/template/api/src/rest/index.js
+++
@@ -1,7 +0,0 @@
-import { restServerFactory } from '@botonic/api/src/rest'
-import { app } from './app'
-
-export default restServerFactory({
-  env: ENV,
-  app,
-})
