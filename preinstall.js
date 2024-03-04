e1e40132133c988367d491efdf454dd6a0c0de29
Oriol Raventos <o.raventos.89@gmail.com>
2024-03-04 11:55:25 +0100

    chore(plugin): update github actions using monorepo and pre-commit-config

--- preinstall.js
+++ preinstall.js
@@ -7,23 +7,23 @@ if (process.env.BOTONIC_NO_INSTALL_ROOT_DEPENDENCIES) {
 function isSaveDev() {
   if (process.env.npm_config_argv) {
     // npm <7
-    var npm_args = JSON.parse(process.env.npm_config_argv)["cooked"];
-    return npm_args.includes("--save-dev");
+    var npm_args = JSON.parse(process.env.npm_config_argv)['cooked']
+    return npm_args.includes('--save-dev')
   }
-  return process.env.npm_config_save_dev;
+  return process.env.npm_config_save_dev
 }
 
 if (isSaveDev()) {
-  var child_process = require('child_process');
-  console.log("Installing common botonic development dependencies:")
+  var child_process = require('child_process')
+  console.log('Installing common botonic development dependencies:')
   process.chdir(__dirname)
   child_process.exec('npm install -D', (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error: ${error}`);
+      console.error(`Error: ${error}`)
       process.exit(1)
     }
-    console.log(stdout);
-    console.error(stderr);
-    console.log("Done. Installing package dependencies:")
-  });
+    console.log(stdout)
+    console.error(stderr)
+    console.log('Done. Installing package dependencies:')
+  })
 }
