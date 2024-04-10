71cadc6e04653fb9cf9eebc8c671776e241c5309
vanbasten17 <marcrabat@gmail.com>
2024-04-10 10:52:53 +0200

    refactor(react): simplify types in whatsapp-cta-url-button

--- packages/botonic-react/src/components/whatsapp-cta-url-button.tsx
+++ packages/botonic-react/src/components/whatsapp-cta-url-button.tsx
@@ -13,19 +13,27 @@ import {
 } from './multichannel/multichannel-utils'
 import { whatsappMarkdown } from './multichannel/whatsapp/markdown'
 
-export const WHATSAPP_MAX_BUTTON_LIST_CHARS = 24
-export const WHATSAPP_MAX_BUTTON_LIST_DESCRIPTION_CHARS = 72
-export const WHATSAPP_MAX_BUTTON_LIST_ID_CHARS = 200
-
-export interface WhatsappCTAUrlButtonProps {
+export interface WhatsappCTAUrlButtonCommonProps {
   header?: string
   body: string
   footer?: string
   displayText: string
-  url?: string
-  webview?: any
+}
+
+export interface WhatsappCTAUrlButtonUrlProps
+  extends WhatsappCTAUrlButtonCommonProps {
+  url: string
+}
+
+export interface WhatsappCTAUrlButtonWebviewProps
+  extends WhatsappCTAUrlButtonCommonProps {
+  webview: any
   params?: any
 }
+
+export type WhatsappCTAUrlButtonProps =
+  | WhatsappCTAUrlButtonUrlProps
+  | WhatsappCTAUrlButtonWebviewProps
 
 const serialize = _whatsappCTAUrlButtonProps => {
   // TODO: Implement to have data persistance in localStorage, not needed for this WhatsApp development
@@ -33,9 +41,6 @@ const serialize = _whatsappCTAUrlButtonProps => {
 }
 
 export const WhatsappCTAUrlButton = (props: WhatsappCTAUrlButtonProps) => {
-  if (!props.url && !props.webview) {
-    console.error('You must provide at least a url or a webview')
-  }
   const renderBrowser = () => {
     // Return a dummy message for browser
     const message = `${JSON.stringify(props)}`
@@ -61,9 +66,10 @@ export const WhatsappCTAUrlButton = (props: WhatsappCTAUrlButtonProps) => {
         ? truncateText(props.footer, WHATSAPP_MAX_FOOTER_CHARS)
         : undefined,
       displayText: truncateText(props.displayText, WHATSAPP_MAX_BUTTON_CHARS),
-      url: props.webview
-        ? generateWebviewUrlWithParams(props.webview, props.params)
-        : props.url,
+      url:
+        'webview' in props
+          ? generateWebviewUrlWithParams(props.webview, props.params)
+          : props.url,
     }
     return (
       // @ts-ignore Property 'message' does not exist on type 'JSX.IntrinsicElements'.
