3ac108d574f26c07a4af399f64a02e836aea96c7
Oriol Raventos <o.raventos.89@gmail.com>
2024-05-12 12:29:05 +0200

    refactor(plugin-flow-builder): pass the function getSmartIntentNodeByInput to a SmartIntentsApi class to implement a mock

--- packages/botonic-plugin-flow-builder/src/user-input/smart-intent.ts
+++ packages/botonic-plugin-flow-builder/src/user-input/smart-intent.ts
@@ -4,43 +4,67 @@ import axios from 'axios'
 import { FlowBuilderApi } from '../api'
 import { HtSmartIntentNode } from '../content-fields/hubtype-fields/smart-intent'
 
-export async function getSmartIntentNodeByInput(
-  cmsApi: FlowBuilderApi,
-  request: ActionRequest
-): Promise<HtSmartIntentNode | undefined> {
-  const smartIntentNodes = cmsApi.getSmartIntentNodes()
-  if (smartIntentNodes.length === 0) {
-    return undefined
+interface InferenceParam {
+  name: string
+  definition: string
+}
+export class SmartIntentsApi {
+  public cmsApi: FlowBuilderApi
+  public currentRequest: ActionRequest
+
+  constructor(cmsApi: FlowBuilderApi, request: ActionRequest) {
+    this.currentRequest = request
+    this.cmsApi = cmsApi
   }
 
-  const intentsInferenceParams = smartIntentNodes.map(smartIntentNode => {
-    return {
-      name: smartIntentNode.content.title,
-      definition: smartIntentNode.content.description,
-    }
-  })
-  intentsInferenceParams.push({
-    name: 'Other',
-    definition: 'The text does not belong to any other intent.',
-  })
+  async getNodeByInput(): Promise<HtSmartIntentNode | undefined> {
+    const smartIntentNodes = this.cmsApi.getSmartIntentNodes()
 
-  try {
-    const response = await axios({
+    if (smartIntentNodes.length === 0) {
+      return undefined
+    }
+
+    const params = this.getInferenceParams(smartIntentNodes)
+    try {
+      const response = await this.getInference(params)
+      return smartIntentNodes.find(
+        smartIntentNode =>
+          smartIntentNode.content.title === response.data.intent_name
+      )
+    } catch (e) {
+      console.error(e)
+      return undefined
+    }
+  }
+
+  private getInferenceParams(
+    smartIntentNodes: HtSmartIntentNode[]
+  ): InferenceParam[] {
+    const intentsInferenceParams = smartIntentNodes.map(smartIntentNode => {
+      return {
+        name: smartIntentNode.content.title,
+        definition: smartIntentNode.content.description,
+      }
+    })
+    intentsInferenceParams.push({
+      name: 'Other',
+      definition: 'The text does not belong to any other intent.',
+    })
+    return intentsInferenceParams
+  }
+
+  private async getInference(
+    inferenceParams: InferenceParam[]
+  ): Promise<{ data: { intent_name: string } }> {
+    return await axios({
       method: 'POST',
       url: `${process.env.HUBTYPE_API_URL}/external/v1/ai/smart_intents/inference/`,
       headers: {
-        Authorization: `Bearer ${request.session._access_token}`,
+        Authorization: `Bearer ${this.currentRequest.session._access_token}`,
         'Content-Type': 'application/json',
       },
-      data: { text: request.input.data, intents: intentsInferenceParams },
+      data: { text: this.currentRequest.input.data, intents: inferenceParams },
       timeout: 10000,
     })
-    return smartIntentNodes.find(
-      smartIntentNode =>
-        smartIntentNode.content.title === response.data.intent_name
-    )
-  } catch (e) {
-    console.error(e)
-    return undefined
   }
 }
