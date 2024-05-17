03feafd38690d1739b8fd6e3fe5b7e3009b0369f
Oriol Raventos <o.raventos.89@gmail.com>
2024-05-17 12:15:38 +0200

    chore(plugin-flow-builder): add jest config

---
+++ packages/botonic-plugin-hubtype-analytics/babel.config.js
@@ -0,0 +1,21 @@
+/*
+ * This babel configuration is used along with Jest for execute tests,
+ * do not modify to avoid conflicts with webpack.config.js.
+ */
+// require("@babel/register");
+
+/*
+ * This babel configuration is used along with Jest for execute tests,
+ * do not modify to avoid conflicts with webpack.config.js.
+ */
+
+module.exports = {
+  sourceType: 'unambiguous',
+  // .map files are not generated unless babel invoked with --source-maps
+  sourceMaps: true,
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    require('@babel/plugin-transform-modules-commonjs'),
+    require('@babel/plugin-transform-runtime'),
+  ],
+}
