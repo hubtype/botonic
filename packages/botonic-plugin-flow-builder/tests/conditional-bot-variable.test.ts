68738bc9a55c92aeaf0c7a49f22cfebcdfb2b40f
Oriol Raventos <o.raventos.89@gmail.com>
2024-06-10 12:36:58 +0200

    test(plugin-flow-builder): fixed 0 in conditional number

--- packages/botonic-plugin-flow-builder/tests/conditional-bot-variable.test.ts
+++ packages/botonic-plugin-flow-builder/tests/conditional-bot-variable.test.ts
@@ -61,7 +61,7 @@ describe('Check the contents returned by the plugin after conditional custom nod
   )
 
   test.each([
-    //{ messageExpected: 'The user has no bags in the booking', bagsAdded: 0 }, // REVIEW: If pass a 0 to conditioanl bot variable return default result becasue 0 is saved as '0' in backend
+    { messageExpected: 'The user has no bags in the booking', bagsAdded: 0 },
     { messageExpected: 'The user has 1 bag in the booking', bagsAdded: 1 },
     { messageExpected: 'The user has 2 bags in the booking', bagsAdded: 2 },
     {
