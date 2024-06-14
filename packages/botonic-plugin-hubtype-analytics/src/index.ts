98b34bdc14aa3a7e6a88c494173f838f1d02694f
vanbasten17 <marcrabat@gmail.com>
2024-06-14 12:07:33 +0200

    feat(plugin-hubtype-analytics): disable analytics if is_test_integration

--- packages/botonic-plugin-hubtype-analytics/src/index.ts
+++ packages/botonic-plugin-hubtype-analytics/src/index.ts
@@ -48,9 +48,9 @@ export default class BotonicPluginHubtypeAnalytics implements Plugin {
   }
 
   async trackEvent(request: BotRequest, htEventProps: HtEventProps) {
+    if (request.session.is_test_integration) return Promise.resolve(undefined)
     const requestData = this.getRequestData(request)
     const event = createHtEvent(requestData, htEventProps)
-
     return this.sendEvent(request, event)
   }
 
