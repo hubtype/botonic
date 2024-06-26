b59c9b2af9437f3a0a240aa479715e707f8f31e9
Albert <albert.gomariz63@gmail.com>
2024-06-26 16:19:02 +0200

    feat(botonic-react): change staticAsset function + add unit tests

--- packages/botonic-react/tests/utils.test.js
+++ packages/botonic-react/tests/utils.test.js
@@ -1,5 +1,8 @@
+/**
+ * @jest-environment jsdom
+ */
+import { isURL, staticAsset } from '../src/util/environment'
 import { deserializeRegex, stringifyWithRegexs } from '../src/util/regexs'
-
 describe('Regex serialization / deserialization', () => {
   const regexsItems = [
     { regex: /regex/i, string: '"/regex/i"', samples: ['Regex', 'regex'] },
@@ -24,5 +27,102 @@ describe('Regex serialization / deserialization', () => {
         expect(match).toBe(true)
       })
     })
+  })
+})
+
+jest.mock('../src/util/environment', () => ({
+  ...jest.requireActual('../src/util/environment'),
+  isURL: jest.fn(),
+  normalize: jest.fn(),
+}))
+
+const createScript = src => {
+  const script = document.createElement('script')
+  script.src = src
+  document.head.appendChild(script)
+  return script
+}
+
+const removeScript = script => {
+  document.head.removeChild(script)
+}
+
+describe('staticAsset function', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('should return the path if it is a URL', () => {
+    //arrange
+    isURL.mockReturnValue(true)
+    const url = 'https://example.com/asset.png'
+
+    //act
+    const result = staticAsset(url)
+
+    //assert
+    expect(result).toBe(url)
+  })
+
+  test('should resolve the static asset path if it is not a URL and file name is webchat.botonic.js', () => {
+    //arrange
+    isURL.mockReturnValue(false)
+
+    const assetPath = 'asset.svg'
+    const basePath = 'http://localhost/'
+    const scriptBaseURL = `${basePath}webchat.botonic.js`
+    const script = createScript(scriptBaseURL)
+
+    //act
+    const result = staticAsset(assetPath)
+
+    //assert
+    expect(result).toBe(basePath + assetPath)
+
+    removeScript(script)
+  })
+
+  test('should resolve static asset path if filename is different than webchat.botonic.js', () => {
+    //arrange
+    isURL.mockReturnValue(false)
+
+    const assetPath = 'asset.svg'
+    const basePath = 'http://localhost/'
+    const scriptBaseURL = `${basePath}webchat.botonic.js?version=1.0.1`
+    const script = createScript(scriptBaseURL)
+
+    //act
+    const result = staticAsset(assetPath)
+
+    //assert
+    expect(result).toBe(basePath + assetPath)
+
+    removeScript(script)
+  })
+
+  test('should log an error and return normalized path if script element is not found', () => {
+    //arrange
+    isURL.mockReturnValue(false)
+
+    const consoleErrorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => {})
+
+    const path = 'asset.svg'
+    const basePath = 'http://localhost/'
+    const scriptBaseURL = `${basePath}webchat.js?version=1.0.1`
+    const script = createScript(scriptBaseURL)
+
+    //act
+    const result = staticAsset(path)
+
+    //assert
+    expect(result).toBe(path)
+    expect(console.error).toHaveBeenCalledWith(
+      `Could not resolve path: '${path}'`
+    )
+
+    consoleErrorSpy.mockRestore()
+    removeScript(script)
   })
 })
