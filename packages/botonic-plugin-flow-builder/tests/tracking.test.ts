001c938d6d15ac980dc1a30574fc671c0bd0f91c
Oriol Raventos <o.raventos.89@gmail.com>
2024-07-09 10:14:21 +0200

    test(plugin-flow-builder): add a test for fallback event

--- packages/botonic-plugin-flow-builder/tests/tracking.test.ts
+++ packages/botonic-plugin-flow-builder/tests/tracking.test.ts
@@ -7,7 +7,7 @@ import { trackEventMock } from './__mocks__/track-event'
 import { basicFlow } from './helpers/flows/basic'
 import { createFlowBuilderPluginAndGetContents } from './helpers/utils'
 
-describe('Check the events tracked when a contents are displayed', () => {
+describe('Check tracked events when a contents are displayed', () => {
   process.env.NODE_ENV = ProcessEnvNodeEnvs.PRODUCTION
 
   test('Track nlu_keyword and flow_node events', async () => {
@@ -44,6 +44,44 @@ describe('Check the events tracked when a contents are displayed', () => {
         flowName: 'Different messages',
         flowNodeId: '7afe0981-e9d3-4e3e-b9d1-5d362d3873b3',
         flowNodeContentId: 'BUTTONS_REPLACEMENT_VARIABLE',
+        flowNodeIsMeaningful: false,
+      }
+    )
+  })
+
+  test('Track fallback and flow_node events', async () => {
+    const userInput = 'I want to cancel my flight'
+
+    await createFlowBuilderPluginAndGetContents({
+      flowBuilderOptions: {
+        flow: basicFlow,
+        trackEvent: trackEventMock,
+      },
+      requestArgs: {
+        input: { data: userInput, type: INPUT.TEXT },
+      },
+    })
+    expect(trackEventMock).toHaveBeenCalledTimes(2)
+    expect(trackEventMock).toHaveBeenNthCalledWith(
+      1,
+      expect.anything(),
+      'fallback',
+      {
+        userInput: userInput,
+        fallbackOut: 1,
+        fallbackMessageId: undefined,
+      }
+    )
+    expect(trackEventMock).toHaveBeenNthCalledWith(
+      2,
+      expect.anything(),
+      'flow_node',
+      {
+        flowThreadId: expect.anything(),
+        flowId: '03bafba6-c0fa-5449-9d42-bd98b44fe370',
+        flowName: 'Fallback',
+        flowNodeId: '802474a3-cb77-45a9-bff5-ca5eb762eb78',
+        flowNodeContentId: '1ST_FALLBACK_MSG',
         flowNodeIsMeaningful: false,
       }
     )
