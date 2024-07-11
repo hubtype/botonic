093171d183151d044da7ef4a3c23b3072cde9690
Oriol Raventos <o.raventos.89@gmail.com>
2024-07-11 18:08:10 +0200

    refactor(botonic-react): remove framer-motion

--- packages/botonic-react/src/shared/styles.ts
+++ packages/botonic-react/src/shared/styles.ts
@@ -9,3 +9,24 @@ export const Fade = styled.div`
   opacity: 0;
   animation: ${fadeIn} 0.8s forwards;
 `
+
+const scaleUp = keyframes`
+  from { transform: scale(1); }
+  to { transform: scale(1.2); }
+`
+
+const scaleDown = keyframes`
+  from { transform: scale(1.2); }
+  to { transform: scale(1); }
+`
+
+export const Scale = styled.div`
+  display: inline-block;
+  animation-fill-mode: forwards;
+  &:hover {
+    animation: ${scaleUp} 0.2s forwards;
+  }
+  &:not():hover {
+    animation: ${scaleDown} 0.2s forwards;
+  }
+`
