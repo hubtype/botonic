ca177267c18ebfee1687e4d3eebc90dafd205415
Oriol Raventos <o.raventos.89@gmail.com>
2024-07-13 17:01:54 +0200

    refactor(plugin-flow-builder): remove console.log

--- packages/botonic-plugin-flow-builder/tests/__mocks__/track-event.ts
+++ packages/botonic-plugin-flow-builder/tests/__mocks__/track-event.ts
@@ -1,12 +1,5 @@
 import { jest } from '@jest/globals'
 
-// import { TrackEventFunction } from '../../src/types'
-
 export const trackEventMock = jest.fn((_request, _eventAction, _args) => {
-  console.log('trackEventMock called', {
-    request: _request,
-    eventAction: _eventAction,
-    args: _args,
-  })
   return Promise.resolve()
 })
