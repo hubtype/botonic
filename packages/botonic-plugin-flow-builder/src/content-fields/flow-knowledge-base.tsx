deb8a7bd3d07110db277b20773e5b8d0e580e464
Oriol Raventos <o.raventos.89@gmail.com>
2024-07-22 15:41:36 +0200

    feat(plugin-flow-builder): add botInteractionId in Text generated from knowledge base

--- packages/botonic-plugin-flow-builder/src/content-fields/flow-knowledge-base.tsx
+++ packages/botonic-plugin-flow-builder/src/content-fields/flow-knowledge-base.tsx
@@ -10,12 +10,14 @@ export class FlowKnowledgeBase extends ContentFieldsBase {
   public sources: string[] = []
   public text: string = ''
   public inferenceId?: string
+  public botInteractionId: string
 
   static fromHubtypeCMS(component: HtKnowledgeBaseNode): FlowKnowledgeBase {
     const newKnowledgeBase = new FlowKnowledgeBase(component.id)
     newKnowledgeBase.code = component.code
     newKnowledgeBase.feedbackEnabled = component.content.feedback_enabled
     newKnowledgeBase.sources = component.content.sources
+    newKnowledgeBase.botInteractionId = component.content.bot_interaction_id
 
     return newKnowledgeBase
   }
