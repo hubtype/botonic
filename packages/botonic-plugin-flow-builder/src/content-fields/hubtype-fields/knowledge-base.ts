e488273d2ab1b07efe5ebfb420a7d3879cd37d81
Oriol Raventos <o.raventos.89@gmail.com>
2024-07-23 10:21:10 +0200

    refactor(plugin-flow-builder): use bot_interaction_id from request.input

--- packages/botonic-plugin-flow-builder/src/content-fields/hubtype-fields/knowledge-base.ts
+++ packages/botonic-plugin-flow-builder/src/content-fields/hubtype-fields/knowledge-base.ts
@@ -5,7 +5,6 @@ export interface HtKnowledgeBaseNode extends HtBaseNode {
   type: HtNodeWithContentType.KNOWLEDGE_BASE
   content: {
     feedback_enabled: boolean
-    bot_interaction_id: string
     sources: string[]
   }
 }
