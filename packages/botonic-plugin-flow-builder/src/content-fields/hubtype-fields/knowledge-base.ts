36e49e4c865a96a860d237b1cd5f5bc59eaf49a8
vanbasten17 <marcrabat@gmail.com>
2024-08-13 11:48:27 +0200

    chore: define interface for SourceData

--- packages/botonic-plugin-flow-builder/src/content-fields/hubtype-fields/knowledge-base.ts
+++ packages/botonic-plugin-flow-builder/src/content-fields/hubtype-fields/knowledge-base.ts
@@ -1,13 +1,15 @@
 import { HtBaseNode } from './common'
 import { HtNodeWithContentType } from './node-types'
 
+interface SourceData {
+  id: string
+  name: string
+}
+
 export interface HtKnowledgeBaseNode extends HtBaseNode {
   type: HtNodeWithContentType.KNOWLEDGE_BASE
   content: {
     feedback_enabled: boolean
-    sources_data: {
-      id: string
-      name: string
-    }[]
+    sources_data: SourceData[]
   }
 }
