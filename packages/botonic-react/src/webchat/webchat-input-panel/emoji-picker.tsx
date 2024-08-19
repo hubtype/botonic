be71afa725c15da073ce7b842e6583e3fd4644d9
Oriol Raventos <o.raventos.89@gmail.com>
2024-08-19 12:57:18 +0200

    refactor(botonic-react): create a file for opened emoji picker component

--- packages/botonic-react/src/webchat/webchat-input-panel/emoji-picker.tsx
+++ packages/botonic-react/src/webchat/webchat-input-panel/emoji-picker.tsx
@@ -57,30 +57,3 @@ export const EmojiPicker = ({
     </>
   )
 }
-
-interface OpenedEmojiPickerProps {
-  onClick: () => void
-  onEmojiClick: (event: any) => void
-}
-
-export const OpenedEmojiPicker = ({
-  onClick,
-  onEmojiClick,
-}: OpenedEmojiPickerProps) => {
-  const { ref, isComponentVisible } = useComponentVisible(true, onClick)
-  return (
-    <div ref={ref as LegacyRef<HTMLDivElement>}>
-      {isComponentVisible && (
-        <OpenedEmojiPickerContainer role={ROLES.EMOJI_PICKER}>
-          <Picker
-            width='100%'
-            height='19rem'
-            previewConfig={{ showPreview: false }}
-            onEmojiClick={onEmojiClick}
-            autoFocusSearch={false}
-          />
-        </OpenedEmojiPickerContainer>
-      )}
-    </div>
-  )
-}
