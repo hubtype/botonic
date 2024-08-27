b194cb71d85b3a069a50413f354a5b83fc9d8375
Oriol Raventós <36898236+Iru89@users.noreply.github.com>
2024-08-27 13:49:11 +0200

    cli: use async await insted of then in deploy (#2885)
    ## Description
    
    Refactor the deploy file to always use async await instead of chaining
    asyncorn functions with callbacks inside the then function.

--- packages/botonic-cli/src/botonic-api-service.ts
+++ packages/botonic-cli/src/botonic-api-service.ts
@@ -1,41 +1,91 @@
-import axios, { AxiosPromise, Method } from 'axios'
+/* eslint-disable @typescript-eslint/naming-convention */
+import axios, {
+  AxiosHeaders,
+  AxiosInstance,
+  AxiosPromise,
+  AxiosResponse,
+} from 'axios'
 import childProcess from 'child_process'
 import colors from 'colors'
 import FormData from 'form-data'
 import { createReadStream, unlinkSync } from 'fs'
 import ora from 'ora'
-import qs from 'qs'
+import { stringify } from 'qs'
 import * as util from 'util'
 
-import { BotInfo, OAuth } from './interfaces'
-
-const exec = util.promisify(childProcess.exec)
 import {
   BotCredentialsHandler,
   GlobalCredentialsHandler,
 } from './analytics/credentials-handler'
+import {
+  AnalyticsInfo,
+  BotDetail,
+  BotListItem,
+  BotsList,
+  Me,
+  OAuth,
+} from './interfaces'
+import { BotConfigJSON } from './util/bot-config'
 import { pathExists } from './util/file-system'
+
+const exec = util.promisify(childProcess.exec)
 
 const BOTONIC_CLIENT_ID: string =
   process.env.BOTONIC_CLIENT_ID || 'jOIYDdvcfwqwSs7ZJ1CpmTKcE7UDapZDOSobFmEp'
 const BOTONIC_URL: string = process.env.BOTONIC_URL || 'https://api.hubtype.com'
 
+interface RequestArgs {
+  apiVersion?: string
+  path: string
+  body?: any
+  headers?: any
+  params?: any
+}
 export class BotonicAPIService {
   clientId: string = BOTONIC_CLIENT_ID
   baseUrl: string = BOTONIC_URL
-  baseApiUrl = this.baseUrl + '/v1/'
   loginUrl: string = this.baseUrl + '/o/token/'
   botCredentialsHandler = new BotCredentialsHandler()
   globalCredentialsHandler = new GlobalCredentialsHandler()
   oauth?: OAuth
-  me: any
-  analytics: any
-  bot: BotInfo | null
-  headers: Record<string, string> | null = null
+  me?: Me
+  analytics: AnalyticsInfo
+  bot: BotDetail | null
+  headers: AxiosHeaders
+  apiClient: AxiosInstance
 
   constructor() {
     this.loadGlobalCredentials()
     this.loadBotCredentials()
+    this.setHeaders(this.oauth?.access_token)
+
+    this.apiClient = axios.create({
+      baseURL: BOTONIC_URL,
+      headers: this.headers,
+    })
+
+    const onFullfilled = (response: AxiosResponse) => {
+      return response
+    }
+
+    const onRejected = async (error: any) => {
+      const originalRequest = error.config
+      const retry = originalRequest?._retry
+
+      if (error.response?.status === 401 && !retry) {
+        originalRequest._retry = true
+        await this.refreshToken()
+        const nextRequest = {
+          ...originalRequest,
+          headers: this.headers,
+        }
+
+        return this.apiClient.request(nextRequest)
+      }
+      return Promise.reject(error)
+    }
+
+    this.apiClient.interceptors.response.use(onFullfilled, onRejected)
   }
 
   beforeExit(): void {
@@ -43,7 +93,7 @@ export class BotonicAPIService {
     this.saveBotCredentials()
   }
 
-  botInfo(): BotInfo {
+  botInfo(): BotDetail {
     if (!this.bot) {
       throw new Error('Not bot info available')
     }
@@ -57,36 +107,34 @@ export class BotonicAPIService {
     return this.oauth
   }
 
-  loadGlobalCredentials(): void {
+  private loadGlobalCredentials(): void {
     const credentials = this.globalCredentialsHandler.load()
     if (credentials) {
       this.oauth = credentials.oauth
       this.me = credentials.me
       this.analytics = credentials.analytics
-      if (this.oauth) {
-        this.headers = {
-          Authorization: `Bearer ${this.oauth.access_token}`,
-          'content-type': 'application/json',
-          'x-segment-anonymous-id': this.analytics.anonymous_id,
-        }
-      }
     }
   }
 
-  loadBotCredentials(): void {
+  private loadBotCredentials(): void {
     const credentials = this.botCredentialsHandler.load()
-    if (credentials) {
-      // eslint-disable-next-line no-prototype-builtins
-      if (credentials.hasOwnProperty('bot')) {
-        this.bot = credentials.bot
-      } else {
-        // Allow users < v0.1.12 to upgrade smoothly
-        this.bot = credentials as any as BotInfo
-      }
+
+    if (credentials?.bot) {
+      this.bot = credentials.bot
     }
   }
 
-  saveGlobalCredentials(): void {
+  private setHeaders(accessToken?: string) {
+    if (accessToken) {
+      this.headers = new AxiosHeaders({
+        Authorization: `Bearer ${accessToken}`,
+        'content-type': 'application/json',
+        'x-segment-anonymous-id': this.analytics.anonymous_id,
+      })
+    }
+  }
+
+  private saveGlobalCredentials(): void {
     this.globalCredentialsHandler.createDirIfNotExists()
     this.globalCredentialsHandler.dump({
       oauth: this.oauth,
@@ -95,7 +143,7 @@ export class BotonicAPIService {
     })
   }
 
-  saveBotCredentials(): void {
+  private saveBotCredentials(): void {
     this.botCredentialsHandler.dump({
       bot: this.bot,
     })
@@ -129,175 +177,180 @@ export class BotonicAPIService {
     if (pathExists(pathToCredentials)) unlinkSync(pathToCredentials)
   }
 
-  async api(
-    path: string,
-    body: any = null,
-    method: Method = 'get',
-    headers: any = null,
-    params: any = null
-  ): Promise<any> {
-    let b = 0
-    try {
-      return await axios({
-        method: method,
-        url: this.baseApiUrl + path,
-        headers: headers || this.headers,
-        data: body,
-        params: params,
-      })
-    } catch (e: any) {
-      if (e.response.status == 401) {
-        b = 1
-      } else {
-        return e
-      }
-    }
-    if (b == 1) {
-      await this.refreshToken()
-    }
-    return axios({
-      method: method,
-      url: this.baseApiUrl + path,
+  private async apiPost({
+    apiVersion = 'v1',
+    path,
+    body,
+    headers,
+    params,
+  }: RequestArgs): Promise<any> {
+    return this.apiClient.post(`${this.baseUrl}/${apiVersion}/${path}`, body, {
       headers: headers || this.headers,
-      data: body,
-      params: params,
+      params,
     })
   }
 
-  async refreshToken(): Promise<any> {
-    const data = qs.stringify({
+  private async apiGet({
+    apiVersion = 'v1',
+    path,
+    headers,
+    params,
+  }: RequestArgs): Promise<any> {
+    return this.apiClient.get(`${this.baseUrl}/${apiVersion}/${path}`, {
+      headers: headers || this.headers,
+      params,
+    })
+  }
+
+  private async refreshToken(): Promise<void> {
+    const data = stringify({
       callback: 'none',
       grant_type: 'refresh_token',
       refresh_token: this.getOauth().refresh_token,
       client_id: this.clientId,
     })
 
-    const resp = await axios({
+    const oauthResponse = await axios.post(this.loginUrl, data, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      method: 'post',
-      url: this.loginUrl,
-      data: data,
     })
-    if (!resp) return
-    this.oauth = resp.data
-    this.headers = {
-      Authorization: `Bearer ${this.getOauth().access_token}`,
-      'content-type': 'application/json',
-      'x-segment-anonymous-id': this.analytics.anonymous_id,
+
+    if (oauthResponse.status !== 200) {
+      throw new Error('Error refreshing token')
     }
+    this.oauth = oauthResponse.data
+
+    const accessToken = this.getOauth().access_token
+    this.setHeaders(accessToken)
     this.saveGlobalCredentials()
-    // eslint-disable-next-line consistent-return
-    return resp
   }
 
-  async login(email: string, password: string): Promise<any> {
-    const data = qs.stringify({
+  async login(email: string, password: string): Promise<void> {
+    const data = stringify({
       username: email,
       password: password,
       grant_type: 'password',
       client_id: this.clientId,
     })
 
-    let resp = await axios({
+    const loginResponse = await axios.post(this.loginUrl, data, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      method: 'post',
-      url: this.loginUrl,
-      data: data,
     })
-    this.oauth = resp.data
-    this.headers = {
-      Authorization: `Bearer ${this.getOauth().access_token}`,
-      'content-type': 'application/json',
-      'x-segment-anonymous-id': this.analytics.anonymous_id,
+    this.oauth = loginResponse.data
+
+    const accessToken = this.getOauth().access_token
+    this.setHeaders(accessToken)
+
+    const meResponse = await this.apiGet({ path: 'users/me' })
+    if (meResponse) {
+      this.me = meResponse.data
     }
-    resp = await this.api('users/me')
-    if (resp) this.me = resp.data
-    return resp
   }
 
   signup(
     email: string,
     password: string,
-    org_name: string,
+    orgName: string,
     campaign: any
   ): Promise<any> {
-    const url = `${this.baseApiUrl}users/`
-    const signup_data = { email, password, org_name, campaign }
-    return axios({ method: 'post', url: url, data: signup_data })
+    const signupData = { email, password, org_name: orgName, campaign }
+    return this.apiPost({ path: 'users/', body: signupData })
   }
 
-  async saveBot(botName: string): Promise<AxiosPromise> {
-    const resp = await this.api('bots/', { name: botName }, 'post')
-    if (resp.data) this.setCurrentBot(resp.data)
+  async createBot(botName: string): Promise<AxiosPromise> {
+    const resp = await this.apiPost({
+      apiVersion: 'v2',
+      path: 'bots/',
+      body: { name: botName },
+    })
+
+    if (resp.data) {
+      this.setCurrentBot(resp.data)
+    }
+
     return resp
   }
 
-  async getMe(): Promise<AxiosPromise> {
-    return this.api('users/me/')
+  private async getMe(): AxiosPromise<Me> {
+    return this.apiGet({ path: 'users/me/' })
   }
 
-  async getBots(): Promise<AxiosPromise> {
-    return this.api('bots/bots_paginator/', null, 'get', null, {
-      organization_id: this.me.organization_id,
+  async getBots(): AxiosPromise<BotsList> {
+    const botsResponse = await this.apiGet({ apiVersion: 'v2', path: 'bots/' })
+
+    if (botsResponse.data.next) {
+      this.getMoreBots(botsResponse.data.results, botsResponse.data.next)
+    }
+
+    return botsResponse
+  }
+
+  private async getMoreBots(bots: BotListItem[], nextBots?: string) {
+    if (!nextBots) {
+      return bots
+    }
+
+    const resp = await this.apiGet({
+      apiVersion: 'v2',
+      path: nextBots.split(`${this.baseUrl}/v2/`)[1],
     })
-  }
-
-  async getMoreBots(bots: any, nextBots: any) {
-    if (!nextBots) return bots
-    const resp = await this.api(
-      nextBots.split(this.baseApiUrl)[1],
-      null,
-      'get',
-      null
-    )
     resp.data.results.map(b => bots.push(b))
     nextBots = resp.data.next
+
     return this.getMoreBots(bots, nextBots)
   }
 
   async getProviders(): Promise<AxiosPromise> {
-    return this.api('provider_accounts/', null, 'get', null, {
-      bot_id: this.botInfo().id,
+    return this.apiGet({
+      path: 'provider_accounts/',
+      params: {
+        bot_id: this.botInfo().id,
+      },
     })
   }
 
-  async deployBot(bundlePath: string): Promise<any> {
+  async deployBot(
+    bundlePath: string,
+    botConfigJson: BotConfigJSON
+  ): Promise<any> {
     try {
-      const _authenticated = await this.getMe()
+      await this.getMe()
     } catch (e) {
       console.log(`Error authenticating: ${String(e)}`)
     }
+
     const form = new FormData()
     const data = createReadStream(bundlePath)
     form.append('bundle', data, 'botonic_bundle.zip')
+    form.append('bot_config', JSON.stringify(botConfigJson))
     const headers = await this.getHeaders(form)
-    return await this.api(
-      `bots/${this.botInfo().id}/deploy_botonic_new/`,
-      form,
-      'post',
-      { ...this.headers, ...headers },
-      { version: '0.7' }
-    )
+
+    return await this.apiPost({
+      apiVersion: 'v2',
+      path: `bots/${this.botInfo().id}/deploy/`,
+      body: form,
+      headers: {
+        ...this.headers,
+        ...headers,
+      },
+    })
   }
 
   async deployStatus(deployId: string): Promise<AxiosPromise> {
-    return this.api(
-      `bots/${this.botInfo().id}/deploy_botonic_status/`,
-      null,
-      'get',
-      null,
-      { deploy_id: deployId }
-    )
+    return this.apiGet({
+      apiVersion: 'v2',
+      path: `bots/${this.botInfo().id}/deploy_status/`,
+      params: { deploy_id: deployId },
+    })
   }
 
-  async getHeaders(form: any): Promise<Record<string, any>> {
+  private async getHeaders(form: FormData): Promise<Record<string, any>> {
     //https://github.com/axios/axios/issues/1006#issuecomment-352071490
     return new Promise((resolve, reject) => {
-      form.getLength((err: any, length: any) => {
+      form.getLength((err: any, length: number) => {
         if (err) {
           reject(err)
         }
