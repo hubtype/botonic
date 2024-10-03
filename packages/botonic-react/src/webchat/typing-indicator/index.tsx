efebace7549223f0119374da4fee4f13c7b880a7
Oriol Raventos <o.raventos.89@gmail.com>
2024-10-03 10:42:21 +0200

    refactor(botonic-react): create typing indicator with typescript and styled component

---
+++ packages/botonic-react/src/webchat/typing-indicator/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Dot, TypingIndicatorWrapper } from './styles'
+
+export const TypingIndicator = () => (
+  <TypingIndicatorWrapper>
+    <Dot />
+    <Dot />
+    <Dot />
+  </TypingIndicatorWrapper>
+)
