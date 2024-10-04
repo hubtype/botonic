9e1afb2e78a525e3c63a2fabfdade7927288b3d1
vanbasten17 <marcrabat@gmail.com>
2024-10-04 10:10:23 +0200

    fix: fix scroll to bottom via querySelector instead of ref

--- packages/botonic-react/src/webchat/hooks/use-scroll-to-bottom.ts
+++ packages/botonic-react/src/webchat/hooks/use-scroll-to-bottom.ts
@@ -1,25 +1,22 @@
-import { useContext } from 'react'
-
-import { WebchatContext } from '../../contexts'
-import { getWebchatElement } from '../../util/dom'
+import {
+  getScrollableMessagesListElement,
+  getWebchatElement,
+} from '../../util/dom'
 
 export const useScrollToBottom = ({
   host,
   behavior = 'smooth',
   timeout = 200,
 }) => {
-  const {
-    webchatState: { isWebchatOpen },
-    scrollableMessagesListRef,
-  } = useContext(WebchatContext)
-
   const scrollToBottom = () => {
     const webchatElement = getWebchatElement(host)
     if (!webchatElement) return
-    if (!isWebchatOpen) return
+    const scrollableMessagesListElement = getScrollableMessagesListElement(host)
+    if (!scrollableMessagesListElement) return
+
     setTimeout(() => {
-      scrollableMessagesListRef.current?.scrollTo({
-        top: scrollableMessagesListRef.current?.scrollHeight,
+      scrollableMessagesListElement.scrollTo({
+        top: scrollableMessagesListElement.scrollHeight,
         behavior: behavior as ScrollBehavior,
       })
     }, timeout)
