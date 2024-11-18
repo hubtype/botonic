c43742b199fa7cfe0c08613a7fe392ee2a7218e0
Oriol Raventós <36898236+Iru89@users.noreply.github.com>
2024-11-18 11:52:25 +0100

    botonic-react: fix cover component in botonic react #BLT-1223 (#2938)
    ## Description
    
    Create a separate file for the cover component (using typescript and
    reducing the size of the webchat file).
    Add the toggleCoverComponent function in the WebchatContext to avoid
    having to pass the function using props
    
    ## Context
    
    Removing the scroll in a previous PR was not rendering the cover
    component correctly
    
    ## Approach taken / Explain the design
    
    Use a conditional render to render the cover component or the webchat.
    It is not necessary to render the messages under the cover component.

---
+++ packages/botonic-react/src/webchat/cover-component/index.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+
+import { WEBCHAT } from '../../constants'
+import { WebchatContext } from '../../contexts'
+
+interface Props {
+  component: any
+  componentProps: any
+}
+
+export const CoverComponent = ({ component, componentProps }: Props) => {
+  const { getThemeProperty, toggleCoverComponent, webchatState } =
+    useContext(WebchatContext)
+
+  const Cover = component
+
+  const coverComponentProps = getThemeProperty(
+    WEBCHAT.CUSTOM_PROPERTIES.coverComponentProps,
+    componentProps
+  )
+
+  const closeCoverComponent = () => {
+    toggleCoverComponent(false)
+  }
+
+  if (!Cover || !webchatState.isCoverComponentOpen) {
+    return null
+  }
+
+  return <Cover closeComponent={closeCoverComponent} {...coverComponentProps} />
+}
