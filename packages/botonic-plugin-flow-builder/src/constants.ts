9536489344d01e9491195a730a1d444b45ef153d
Oriol Raventós <36898236+Iru89@users.noreply.github.com>
2024-11-25 16:10:57 +0100

    plugin-flow-builder: not show log when check if payload is from bot action node (#2940)
    ## Description
    
    Avoid using getNodeById inside the isBotAction function to not show a
    log `Node with id X is not found` every time that bot received a custom
    payload

--- packages/botonic-plugin-flow-builder/src/constants.ts
+++ packages/botonic-plugin-flow-builder/src/constants.ts
@@ -5,6 +5,8 @@ export const SOURCE_INFO_SEPARATOR = `${SEPARATOR}source_`
 export const VARIABLE_PATTERN = /{([^}]+)}/g
 export const ACCESS_TOKEN_VARIABLE_KEY = '_access_token'
 export const REG_EXP_PATTERN = /^\/(.*)\/([gimyus]*)$/
+export const UUID_REGEXP =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
 export const MAIN_FLOW_NAME = 'Main'
 export const KNOWLEDGE_BASE_FLOW_NAME = 'Knowledge base'
