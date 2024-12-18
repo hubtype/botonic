c559e5c1583af03909c83ec34b609168ec584bed
Oriol Raventos <o.raventos.89@gmail.com>
2024-12-18 13:41:16 +0100

    refactor(botonic-react): use typescript for util/webchat file

--- packages/botonic-react/src/util/webchat.ts
+++ packages/botonic-react/src/util/webchat.ts
@@ -2,6 +2,7 @@ import merge from 'lodash.merge'
 import UAParser from 'ua-parser-js'
 import { v7 as uuidv7 } from 'uuid'
 
+import { ThemeProps } from '../components'
 import { WEBCHAT } from '../constants'
 import { getProperty } from './objects'
 
@@ -11,13 +12,12 @@ import { getProperty } from './objects'
  * If property doesn't exist, returns the defaultValue.
  */
 export const _getThemeProperty =
-  theme =>
-  (property, defaultValue = undefined) => {
-    for (const [k, v] of Object.entries(WEBCHAT.CUSTOM_PROPERTIES)) {
-      if (v == property) {
-        const nestedProperty = getProperty(theme, v)
+  (theme: ThemeProps) => (property: string, defaultValue?: any) => {
+    for (const [key, value] of Object.entries(WEBCHAT.CUSTOM_PROPERTIES)) {
+      if (value === property) {
+        const nestedProperty = getProperty(theme, value)
         if (nestedProperty !== undefined) return nestedProperty
-        const plainProperty = getProperty(theme, k)
+        const plainProperty = getProperty(theme, key)
         if (plainProperty !== undefined) return plainProperty
         return defaultValue
       }
@@ -25,7 +25,10 @@ export const _getThemeProperty =
     return undefined
   }
 
-export const createUser = () => {
+export const createUser = (): {
+  id: string
+  name: string
+} => {
   const parser = new UAParser()
   const ua = parser.getResult()
   let name = `${ua.os.name} ${ua.browser.name}`
@@ -35,9 +38,11 @@ export const createUser = () => {
     name,
   }
 }
-export const initSession = session => {
+export const initSession = (
+  session: any
+): { user: { id: string; name: string } } => {
   if (!session) session = {}
-  const hasUserId = session.user && session.user.id !== undefined
+  const hasUserId = session?.user?.id !== undefined
   if (!session.user || Object.keys(session.user).length === 0 || !hasUserId)
     session.user = !hasUserId ? merge(session.user, createUser()) : createUser()
   return session
@@ -48,6 +53,7 @@ export const shouldKeepSessionOnReload = ({
   devSettings,
 }) => !initialDevSettings || (devSettings && devSettings.keepSessionOnReload)
 
+//TODO: Review param serverConfig if is of type ServerConfig this never have errorMessage
 export const getServerErrorMessage = serverConfig => {
   if (!serverConfig || !serverConfig.errorMessage) return 'Connection issues'
   if (typeof serverConfig.errorMessage === 'function') {
