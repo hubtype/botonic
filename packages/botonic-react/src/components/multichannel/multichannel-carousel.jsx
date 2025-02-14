cb18cdb6640d35c5bcfae6af484cc913ba6ef2f6
Oriol Raventos <o.raventos.89@gmail.com>
2025-02-14 18:46:43 +0100

    refactor: split in small functions to simplify MulichannelCarousel

--- packages/botonic-react/src/components/multichannel/multichannel-carousel.jsx
+++ packages/botonic-react/src/components/multichannel/multichannel-carousel.jsx
@@ -1,4 +1,4 @@
-import { isDev, isWebchat, isWhatsapp } from '@botonic/core'
+import { isDev, isFacebook, isWebchat, isWhatsapp } from '@botonic/core'
 import React, { useContext } from 'react'
 
 import { RequestContext } from '../../contexts'
@@ -14,87 +14,92 @@ import {
   isNodeSubtitle,
   isNodeTitle,
 } from './multichannel-utils'
+import { convertToMarkdownMeta } from './whatsapp/markdown-meta'
 
 export const MultichannelCarousel = props => {
   console.log('MultichannelCarousel', props)
   const requestContext = useContext(RequestContext)
-
-  const getButtons = node => {
-    return [].concat(getFilteredElements(node, isMultichannelButton))
-  }
 
   if (isDev(requestContext.session) || isWebchat(requestContext.session)) {
     return <Carousel {...props}>{props.children}</Carousel>
   }
 
   const messages = []
-  const elements = props.children
-    .map(e => e.props.children)
-    .map((element, i) => {
-      let imageProps = undefined
-      let title = undefined
-      let subtitle = undefined
-      const buttonsChildren = []
+  const childrenElements = props.children.map(e => e.props.children)
 
-      for (const node of element) {
-        if (isNodePic(node)) {
-          imageProps = node.props
-        }
-        if (isNodeTitle(node)) {
-          title = node.props.children
-        }
-        if (isNodeSubtitle(node)) {
-          subtitle = node.props.children
-        }
+  childrenElements.forEach((element, i) => {
+    const {
+      imageProps,
+      title,
+      subtitle,
+      textButton,
+      payloadButton,
+      urlButton,
+    } = extractElementProperties(element)
 
-        console.log('node', node)
-        if (isMultichannelButton(node)) {
-          buttonsChildren.push(node)
-        }
-        //TODO support fragment containing an array
-        if (Array.isArray(node)) {
-          buttonsChildren.push(getButtons(node))
-        }
-      }
+    const textMessage = getTextMessage(requestContext.session, title, subtitle)
 
-      const messageWithImage = <Image src={imageProps.src} />
-      messages.push(messageWithImage)
+    const messageWithImage = <Image src={imageProps.src} />
+    messages.push(messageWithImage)
 
-      const messageText = `*${title}*${subtitle ? ` _${subtitle}_` : ''}`
-
-      const displayText = buttonsChildren[0].props.children
-      const payload = buttonsChildren[0].props.payload
-      const url = buttonsChildren[0].props.url
-
-      if (url && isWhatsapp(requestContext.session)) {
-        const messageWithButtonUrl = (
-          <WhatsappCTAUrlButton
-            body={messageText}
-            displayText={displayText}
-            url={url}
-          />
-        )
-        messages.push(messageWithButtonUrl)
-      } else {
-        const buttons = buttonsChildren.map((button, j) => {
-          return (
-            <Button key={`carousel-element-${i}-button${j}`} payload={payload}>
-              {displayText}
-            </Button>
-          )
-        })
-
-        const messageWithButtonPayload = (
-          <Text key={`carousel-element-${i}-text`}>
-            {messageText}
-            {buttons}
-          </Text>
-        )
-        messages.push(messageWithButtonPayload)
-      }
-
-      return element
-    })
+    if (urlButton && isWhatsapp(requestContext.session)) {
+      const messageWithButtonUrl = (
+        <WhatsappCTAUrlButton
+          key={`carousel-element-${i}-cta-url`}
+          body={title}
+          footer={`_${subtitle}_`}
+          displayText={textButton}
+          url={urlButton}
+        />
+      )
+      messages.push(messageWithButtonUrl)
+    } else {
+      const messageWithButtonPayload = (
+        <Text key={`carousel-element-${i}-text`}>
+          {textMessage}
+          <Button
+            key={`carousel-element-${i}-button`}
+            payload={payloadButton}
+            url={urlButton}
+          >
+            {textButton}
+          </Button>
+        </Text>
+      )
+      messages.push(messageWithButtonPayload)
+    }
+  })
 
   return <>{messages}</>
+}
+
+function getButtons(node) {
+  return [].concat(getFilteredElements(node, isMultichannelButton))
+}
+
+function extractElementProperties(element) {
+  let imageProps, title, subtitle
+  const buttonsChildren = []
+
+  for (const node of element) {
+    if (isNodePic(node)) imageProps = node.props
+    if (isNodeTitle(node)) title = node.props.children
+    if (isNodeSubtitle(node)) subtitle = node.props.children
+    if (isMultichannelButton(node)) buttonsChildren.push(node)
+    if (Array.isArray(node)) buttonsChildren.push(...getButtons(node))
+  }
+
+  // Carousel Element only allow one button
+  const textButton = buttonsChildren[0].props.children
+  const payloadButton = buttonsChildren[0].props.payload
+  const urlButton = buttonsChildren[0].props.url
+
+  return { imageProps, title, subtitle, textButton, payloadButton, urlButton }
+}
+
+function getTextMessage(session, title, subtitle) {
+  const textMessage = `**${title}**${subtitle ? ` _${subtitle}_` : ''}`
+  return isWhatsapp(session) || isFacebook(session)
+    ? convertToMarkdownMeta(textMessage)
+    : textMessage
 }
