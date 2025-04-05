21a7d4ec4e69358cb5e579e027296f8c67e81218
Oriol Raventos <o.raventos.89@gmail.com>
2025-04-05 21:05:53 +0200

    refactor(botonic-react): apply types

--- packages/botonic-react/src/react-bot.tsx
+++ packages/botonic-react/src/react-bot.tsx
@@ -3,9 +3,20 @@ import React from 'react'
 
 import { Text } from './components/text'
 import { RequestContext } from './contexts'
+import { ActionRequest } from './index-types'
+
+type ActionComponentType<P = any> = React.ComponentType<P> & {
+  botonicInit?: (request: ActionRequest) => Promise<P>
+  contextType?: React.Context<typeof RequestContext>
+  render: (props: P) => JSX.Element
+}
+interface RenderReactActionsArgs {
+  request: ActionRequest
+  actions: ActionComponentType[]
+}
 
 export class ReactBot extends CoreBot {
-  constructor(options) {
+  constructor(options: any) {
     super({
       defaultRoutes: [
         {
@@ -13,19 +24,20 @@ export class ReactBot extends CoreBot {
           action: () => <Text>I don't understand you</Text>, // eslint-disable-line
         },
       ],
-      renderer: args => this.renderReactActions(args),
+      renderer: (args: RenderReactActionsArgs) => this.renderReactActions(args),
       ...options,
     })
   }
 
-  async renderReactActions({ request, actions }) {
-    const renderedActions = []
-    let props
-    let renderedAction
+  async renderReactActions({ request, actions }: RenderReactActionsArgs) {
+    const renderedActions: JSX.Element[] = []
+
     for (const Action of actions) {
       if (Action) {
-        props = Action.botonicInit ? await Action.botonicInit(request) : {}
-        renderedAction = (
+        const props = Action.botonicInit
+          ? await Action.botonicInit(request)
+          : {}
+        const renderedAction: JSX.Element = (
           <RequestContext.Provider value={request}>
             <Action {...props} />
           </RequestContext.Provider>
