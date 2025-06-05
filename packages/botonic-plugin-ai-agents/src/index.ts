f756337867c31c3b3eb7949bf3d61f5f19bd001b
Eric Lozano <ericlozano98@gmail.com>
2025-06-05 16:40:43 +0200

    index

--- packages/botonic-plugin-ai-agents/src/index.ts
+++ packages/botonic-plugin-ai-agents/src/index.ts
@@ -2,7 +2,13 @@ import { BotContext, Plugin } from '@botonic/core'
 
 import { AiAgentClient } from './ai-agent-client'
 import { HubtypeClient } from './hubtype-client'
-import { AgenticMessage, AiAgentArgs, PluginAiAgentOptions } from './types'
+import { MANDATORY_TOOLS } from './tools'
+import {
+  AgenticOutputMessage,
+  AiAgentArgs,
+  PluginAiAgentOptions,
+  UserMessage,
+} from './types'
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -19,22 +25,25 @@ export default class BotonicPluginAiAgents implements Plugin {
   async getInference(
     request: BotContext,
     aiAgentArgs: AiAgentArgs
-  ): Promise<AgenticMessage | undefined> {
-    const authToken = isProd ? request.session._access_token : this.authToken
-    if (!authToken) {
-      throw new Error('Auth token is required')
-    }
-
-    const hubtypeClient = new HubtypeClient(authToken)
-    const aiAgentClient = new AiAgentClient(aiAgentArgs)
-
+  ): Promise<AgenticOutputMessage | undefined> {
     try {
+      const authToken = isProd ? request.session._access_token : this.authToken
+      if (!authToken) {
+        throw new Error('Auth token is required')
+      }
+
+      const hubtypeClient = new HubtypeClient(authToken)
       const messages = isProd
         ? await hubtypeClient.getMessages(request, 10)
-        : [{ role: 'user', content: request.input.data } as AgenticMessage]
-      const response = await aiAgentClient.runAgent(messages)
+        : [{ role: 'user', content: request.input.data } as UserMessage]
 
-      return response
+      const customTools = []
+      const aiAgentClient = new AiAgentClient(aiAgentArgs, [
+        ...customTools,
+        ...MANDATORY_TOOLS,
+      ])
+
+      return await aiAgentClient.runAgent(messages)
     } catch (error) {
       console.error('error plugin returns undefined', error)
       return undefined
