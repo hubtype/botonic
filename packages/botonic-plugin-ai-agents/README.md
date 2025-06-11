3410518a370145b3d19b30134f9786a029b4eadb
Oriol Raventos <o.raventos.89@gmail.com>
2025-06-11 17:15:31 +0200

    refacot: add pragma: allowlist secret

--- packages/botonic-plugin-ai-agents/README.md
+++ packages/botonic-plugin-ai-agents/README.md
@@ -7,8 +7,8 @@ You need this env variables when deploy a bot that use this plugin
 .env
 
 ```
-AZURE_OPENAI_API_KEY='FAKE_API_KEY'
+AZURE_OPENAI_API_KEY='FAKE_API_KEY' // pragma: allowlist secret
+AZURE_OPENAI_API_BASE='FAKE_API_URL_BASE'
 AZURE_OPENAI_API_DEPLOYMENT_NAME='gpt-4o-mini_p1'
 AZURE_OPENAI_API_VERSION='2024-12-01-preview'
-AZURE_OPENAI_API_BASE='FAKE_API_URL_BASE'
 ```
--- packages/botonic-plugin-ai-agents/src/constants.ts
+++ packages/botonic-plugin-ai-agents/src/constants.ts
@@ -1,7 +1,7 @@
 export const HUBTYPE_API_URL =
   process.env.HUBTYPE_API_URL || 'https://api.hubtype.com'
 
-export const AZURE_OPENAI_API_KEY = process.env.AZURE_OPENAI_API_KEY
+export const AZURE_OPENAI_API_KEY = process.env.AZURE_OPENAI_API_KEY // pragma: allowlist secret
 export const AZURE_OPENAI_API_DEPLOYMENT_NAME =
   process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME
 export const AZURE_OPENAI_API_VERSION = process.env.AZURE_OPENAI_API_VERSION
