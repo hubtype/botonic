72d53b367f705538629b9c851056190649f163b5
Oriol Raventos <o.raventos.89@gmail.com>
2025-07-16 10:08:55 +0200

    feat(plugin-flow-builder): add tracking when resolve a rating

--- packages/botonic-plugin-flow-builder/src/action/payload.ts
+++ packages/botonic-plugin-flow-builder/src/action/payload.ts
@@ -1,6 +1,10 @@
+import { storeCaseRating } from '@botonic/core'
+import { v7 as uuid } from 'uuid'
+
 import { AGENT_RATING_PAYLOAD, SEPARATOR } from '../constants'
 import { FlowContent } from '../content-fields'
 import { HtNodeWithContent } from '../content-fields/hubtype-fields'
+import { EventAction, trackEvent } from '../tracking'
 import { FlowBuilderContext } from './index'
 
 export async function getContentsByPayload(
@@ -37,6 +41,24 @@ async function resolveRatingPayload(
   const ratingNode = cmsApi.getRatingNodeByButtonId(buttonId)
   const ratingButton = cmsApi.getRatingButtonById(ratingNode, buttonId)
   const { target, text, value } = ratingButton
+  const possibleOptions = ratingNode.content.buttons.map(button => button.text)
+  const possibleValues = ratingNode.content.buttons.map(button => button.value)
+
+  if (request.session._hubtype_case_id) {
+    const event = {
+      action: EventAction.FeedbackCase,
+      feedbackTargetId: request.session._hubtype_case_id,
+      feedbackGroupId: uuid().toString(),
+      possibleOptions,
+      possibleValues,
+      option: text,
+      value,
+    }
+
+    await storeCaseRating(request.session, value)
+    await trackEvent(request, EventAction.FeedbackCase, event)
+  }
+
   const targetNode = target
     ? cmsApi.getNodeById<HtNodeWithContent>(target.id)
     : undefined
