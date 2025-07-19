75fb894b0b1735eeb0ee48d094829b846a4515e7
Oriol Raventos <o.raventos.89@gmail.com>
2025-07-19 13:01:52 +0200

    feat(plugin-flow-builder): allow to send a custom rating message

--- packages/botonic-plugin-flow-builder/src/index.ts
+++ packages/botonic-plugin-flow-builder/src/index.ts
@@ -70,6 +70,7 @@ export default class BotonicPluginFlowBuilder implements Plugin {
   // TODO: Rethink how we construct FlowBuilderApi to be simpler
   public jsonVersion: FlowBuilderJSONVersion
   public apiUrl: string
+  public enableCustomRatingMessage: boolean
 
   constructor(options: BotonicPluginFlowBuilderOptions<ResolvedPlugins, any>) {
     this.apiUrl = options.apiUrl || FLOW_BUILDER_API_URL_PROD
@@ -91,6 +92,7 @@ export default class BotonicPluginFlowBuilder implements Plugin {
       allowKnowledgeBases: options.inShadowing?.allowKnowledgeBases || false,
     }
     this.contentFilters = options.contentFilters || []
+    this.enableCustomRatingMessage = options.enableCustomRatingMessage || false
   }
 
   resolveFlowUrl(request: PluginPreRequest): string {
