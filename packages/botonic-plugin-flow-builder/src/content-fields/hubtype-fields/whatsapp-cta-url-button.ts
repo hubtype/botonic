c5e5577bc1549dc95b78ffdace01326863d8d726
Oriol Raventos <o.raventos.89@gmail.com>
2025-07-31 17:03:16 +0200

    feat: allow create a WhatsappCtaUrl message with header document, video or image

--- packages/botonic-plugin-flow-builder/src/content-fields/hubtype-fields/whatsapp-cta-url-button.ts
+++ packages/botonic-plugin-flow-builder/src/content-fields/hubtype-fields/whatsapp-cta-url-button.ts
@@ -1,5 +1,7 @@
+import { WhatsappCTAUrlHeaderType } from '@botonic/react'
+
 import { HtButton } from './button'
-import { HtBaseNode, HtTextLocale } from './common'
+import { HtBaseNode, HtMediaFileLocale, HtTextLocale } from './common'
 import { HtNodeWithContentType } from './node-types'
 
 export interface HtWhatsappCTAUrlButtonNode extends HtBaseNode {
@@ -7,6 +9,10 @@ export interface HtWhatsappCTAUrlButtonNode extends HtBaseNode {
   content: {
     text: HtTextLocale[]
     header: HtTextLocale[]
+    header_type?: WhatsappCTAUrlHeaderType
+    header_image?: HtMediaFileLocale[]
+    header_video?: HtMediaFileLocale[]
+    header_document?: HtMediaFileLocale[]
     footer: HtTextLocale[]
     button: HtButton
   }
