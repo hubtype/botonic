34286f62d449fd674195b5e1dc1a8e2571ee838a
Oriol Raventos <o.raventos.89@gmail.com>
2025-08-01 11:11:09 +0200

    test: create tests and snapshots for diferents header types

--- packages/botonic-react/tests/components/whatsapp-cta-url-button.test.jsx
+++ packages/botonic-react/tests/components/whatsapp-cta-url-button.test.jsx
@@ -19,11 +19,76 @@ test('renders WhatsappCTAUrlButton component (passing a URL)', () => {
   expect(tree).toMatchSnapshot()
 })
 
+test('renders WhatsappCTAUrlButton component (passing a URL) with image header', () => {
+  const props = {
+    headerType: 'image',
+    header:
+      'https://www.hubtype.com/media/flow_builder/media_files/d712e320-27cc-4af5-83dc-82e01b7b3767/01971b5a-a6d5-7b82-b02c-cda1af13d240/dragon_ball_walpaper.jpg',
+    body: 'This is the body',
+    footer: 'This is the footer',
+    displayText: 'Go to Hubtype',
+    url: 'https://www.hubtype.com',
+  }
+
+  const tree = renderToJSON(<WhatsappCTAUrlButton {...props} />)
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders WhatsappCTAUrlButton component (passing a URL) with video header', () => {
+  const props = {
+    headerType: 'video',
+    header:
+      'https://www.hubtype.com/media/flow_builder/media_files/d712e320-27cc-4af5-83dc-82e01b7b3767/01971b5a-a6d5-7b82-b02c-cda1af13d240/video_test.mp4',
+    body: 'This is the body',
+    footer: 'This is the footer',
+    displayText: 'Go to Hubtype',
+    url: 'https://www.hubtype.com',
+  }
+
+  const tree = renderToJSON(<WhatsappCTAUrlButton {...props} />)
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders WhatsappCTAUrlButton component (passing a URL) with document header', () => {
+  const props = {
+    headerType: 'document',
+    header:
+      'https://www.hubtype.com/media/flow_builder/media_files/d712e320-27cc-4af5-83dc-82e01b7b3767/01971b5a-a6d5-7b82-b02c-cda1af13d240/requisitos_cta_url_whatsapp.pdf',
+    body: 'This is the body',
+    footer: 'This is the footer',
+    displayText: 'Go to Hubtype',
+    url: 'https://www.hubtype.com',
+  }
+
+  const tree = renderToJSON(<WhatsappCTAUrlButton {...props} />)
+  expect(tree).toMatchSnapshot()
+})
+
 test('renders WhatsappCTAUrlButton component (passing a Webview)', () => {
   class MyWebview {}
   const props = {
     headerType: 'text',
     header: 'This is the header',
+    body: 'This is the body',
+    footer: 'This is the footer',
+    displayText: 'Go to webview',
+    webview: MyWebview,
+    params: {
+      numbers: '123',
+      redirectUri: 'www.some-site.com',
+    },
+  }
+
+  const tree = renderToJSON(<WhatsappCTAUrlButton {...props} />)
+  expect(tree).toMatchSnapshot()
+})
+
+test('renders WhatsappCTAUrlButton component (passing a Webview) with image header', () => {
+  class MyWebview {}
+  const props = {
+    headerType: 'image',
+    header:
+      'https://www.hubtype.com/media/flow_builder/media_files/d712e320-27cc-4af5-83dc-82e01b7b3767/01971b5a-a6d5-7b82-b02c-cda1af13d240/dragon_ball_walpaper.jpg',
     body: 'This is the body',
     footer: 'This is the footer',
     displayText: 'Go to webview',
