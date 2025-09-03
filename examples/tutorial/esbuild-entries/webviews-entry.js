51ed479cc09bde3b4616029dd0e922314bf3d373
Oriol Raventos <o.raventos.89@gmail.com>
2025-09-03 12:04:23 +0200

    chore publish all examples and cli

--- examples/tutorial/esbuild-entries/webviews-entry.js
+++
@@ -1,6 +0,0 @@
-import { WebviewApp } from '@botonic/react'
-
-import { locales } from '../src/locales'
-import { webviews } from '../src/webviews'
-
-module.exports = new WebviewApp({ webviews, locales })
