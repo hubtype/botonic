bb9db053e9948554dac2586805a027aaad6b275f
Oriol Raventos <o.raventos.89@gmail.com>
2025-10-02 13:35:14 +0200

    refactor: remove unused key

--- packages/botonic-cli/src/constants.ts
+++ packages/botonic-cli/src/constants.ts
@@ -3,5 +3,4 @@ export const BOTONIC_HOME_DIRNAME = '.botonic'
 export const GLOBAL_CREDENTIALS_FILENAME = 'credentials.json'
 export const BOTONIC_PROJECT_PATH = process.cwd()
 export const BOT_CREDENTIALS_FILENAME = '.botonic.json'
-export const ANALYTICS_WRITE_KEY = 'YD0jpJHNGW12uhLNbgB4wbdTRQ4Cy1Zu'
 export const CLOUD_PROVIDERS = Object.freeze({ HUBTYPE: 'hubtype' })
