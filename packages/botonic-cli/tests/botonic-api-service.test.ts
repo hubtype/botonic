d1241b594cd277ebf8642c1a3a2fff88d91b684a
Oriol Raventos <o.raventos.89@gmail.com>
2025-10-07 18:04:16 +0200

    refacctor: remove unused imports

--- packages/botonic-cli/tests/botonic-api-service.test.ts
+++ packages/botonic-cli/tests/botonic-api-service.test.ts
@@ -2,16 +2,14 @@ import { runCommand } from '@oclif/test'
 import { assert } from 'console'
 import { chdir, cwd } from 'process'
 
-import { BotonicAPIService } from '../src/botonic-api-service.js'
 import { EXAMPLES } from '../src/botonic-examples.js'
 import { createTempDir, removeRecursively } from '../src/util/file-system.js'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { cpSync } from 'fs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-// El root debe ser el directorio del CLI, no el temporal
+
 const CLI_ROOT = path.join(__dirname, '..')
 
 describe('TEST: BotonicApiService', () => {
@@ -23,9 +21,7 @@ describe('TEST: BotonicApiService', () => {
     const tmpPath = createTempDir('botonic-tmp')
 
     try {
-      // // Cambiar al directorio temporal antes de ejecutar el comando
       chdir(tmpPath)
-      // Pasa 'blank' como projectName para evitar el prompt interactivo
       const { stdout, stderr } = await runCommand(
         ['new', 'test-blank', 'blank'],
         {
@@ -33,15 +29,8 @@ describe('TEST: BotonicApiService', () => {
         }
       )
 
-      // El stdout debería contener el mensaje de éxito
       expect(stdout).toContain('was successfully created')
-
-      // cpSync(BLANK_EXAMPLE.localTestPath, path.join('..', tmpPath), {recursive: true})
-      // const botonicApiService = new BotonicAPIService()
-      // const buildOut = await botonicApiService.build()
-      // expect(buildOut).toBe(true)
     } finally {
-      // Restaurar el directorio original y limpiar
       chdir(originalCwd)
       removeRecursively(tmpPath)
     }
