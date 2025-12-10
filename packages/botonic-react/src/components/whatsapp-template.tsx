2b8fb85eab7e6a5356ce170865211b8396c1a696
Oriol Raventos <o.raventos.89@gmail.com>
2025-12-10 13:19:15 +0100

    feat: whatsapp-template with typescript

--- packages/botonic-react/src/components/whatsapp-template.tsx
+++ packages/botonic-react/src/components/whatsapp-template.tsx
@@ -4,16 +4,23 @@ import React from 'react'
 import { renderComponent } from '../util/react'
 import { Message } from './message'
 
-const serialize = whatsappTemplateProps => {
-  return { text: whatsappTemplateProps }
+const serialize = (message: string) => {
+  return { text: message }
 }
 
-// TODO: Type correctly these props
+export interface WhatsappTemplateProps {
+  name: string
+  language: string
+  namespace?: string
+  header?: Record<string, any>
+  body?: Record<string, any>
+  footer?: Record<string, any>
+}
 
-export const WhatsappTemplate = props => {
+export const WhatsappTemplate = (props: WhatsappTemplateProps) => {
   const renderBrowser = () => {
     // Return a dummy message for browser
-    const message = `Template ${props.name} with namespace ${props.namespace} would be sent to the user.`
+    const message = `Template ${props.name} with language ${props.language} and namespace ${props.namespace} would be sent to the user.`
     return (
       <Message json={serialize(message)} {...props} type={INPUT.TEXT}>
         {message}
@@ -23,8 +30,12 @@ export const WhatsappTemplate = props => {
 
   const renderNode = () => {
     return (
+      // @ts-ignore Property 'message' does not exist on type 'JSX.IntrinsicElements'.
       <message
         {...props}
+        name={props.name}
+        language={props.language}
+        namespace={props.namespace}
         header={props.header && JSON.stringify(props.header)}
         body={props.body && JSON.stringify(props.body)}
         footer={props.footer && JSON.stringify(props.footer)}
