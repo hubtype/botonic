b5765f05f7dce565b3ce557b5fcbbc2a5310af2f
vanbasten17 <marcrabat@gmail.com>
2026-03-09 15:53:45 +0100

    updare flow builder typescript with example

--- examples/flow-builder-typescript/src/bot-config.ts
+++ examples/flow-builder-typescript/src/bot-config.ts
@@ -1,10 +1,12 @@
-import { BotConfigJSON } from '@botonic/core'
+import type { BotConfigJSON } from '@botonic/core'
+import { zodToJsonSchema } from 'zod-to-json-schema'
 
 import { customTools } from './server/tools'
 
 const aiAgentTools =
   customTools?.map(customTool => ({
     name: customTool.name,
+    schema: zodToJsonSchema(customTool.schema, { $refStrategy: 'none' }),
     description: customTool.description,
   })) || []
 
