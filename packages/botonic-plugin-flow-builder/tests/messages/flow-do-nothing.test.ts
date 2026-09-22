e00697d41a4699e320a81e32fd9d9de0f818da4a
Oriol Raventos <o.raventos.89@gmail.com>
2026-09-22 17:50:42 +0200

    refactor: update test imports from Jest to Vitest in flow-do-nothing test file

--- packages/botonic-plugin-flow-builder/tests/messages/flow-do-nothing.test.ts
+++ packages/botonic-plugin-flow-builder/tests/messages/flow-do-nothing.test.ts
@@ -1,5 +1,5 @@
 import { EventAction, INPUT } from '@botonic/core'
-import { describe, expect, test } from '@jest/globals'
+import { describe, expect, test } from 'vitest'
 
 import { FlowDoNothing } from '../../src/content-fields/index'
 import { ProcessEnvNodeEnvs } from '../../src/types'
